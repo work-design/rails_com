ecd0468d11222360d7eb2491e4e324159e4b7e55
qinmingyuan <mingyuan0715@foxmail.com>
2017-01-12 20:26:31 +0800

    show model info

--- lib/rails_com.rb
+++ lib/rails_com.rb
@@ -1,6 +1,7 @@
 require 'rails_com/engine'
 require 'rails_com/routes'
 require 'rails_com/models'
+require 'rails_com/model_helper'
 
 module RailsCom
   # Your code goes here...
---
+++ lib/rails_com/model_helper.rb
@@ -0,0 +1,53 @@
+require 'rails/generators'
+require 'generators/factory_girl/model/model_generator'
+module RailsCom::ModelHelper
+
+  def to_factory_girl
+    args = [
+      self.name.underscore
+    ]
+    cols = columns.map { |col| "#{col.name}:#{col.type}" }
+
+    generator = FactoryGirl::Generators::ModelGenerator.new(args + cols, destination_root: Rails.root)
+    generator.invoke_all
+  end
+
+
+  def column_attributes
+    columns.map do |column|
+      [
+        column.name.to_sym,
+        column.default,
+        column.type,
+        column.sql_type,
+        column.null,
+        column.default_function
+      ]
+    end
+  end
+
+
+  def print_table(with_column: false)
+    columns.each do |column|
+      info = with_column ? '#  t.column' : '# '
+      info << " :#{column.name.to_sym}, :#{column.type}"
+      info << ", precision: #{column.precision}" if column.precision
+      info << ", scale: #{column.scale}" if column.scale
+      info << ", limit: #{column.limit}" if column.limit
+      if column.default && column.type == :string
+        info << ", default: '#{column.default}'"
+      elsif column.default
+        info << ", default: #{column.default}"
+      end
+      info << ", null: false" unless column.null
+      info << ", comment: '#{column.comment} type: #{column.sql_type}'" if column.comment
+      puts info
+    end
+
+    nil
+  end
+
+end
+
+
+ActiveRecord::Base.extend RailsCom::ModelHelper
