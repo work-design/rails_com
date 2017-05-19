ee5297f41bfe360a34d103108c02b394fc1607ef
qinmingyuan <mingyuan0715@foxmail.com>
2017-05-19 21:53:39 +0800

    ablity to config qiniu

--- lib/rails_com/sprockets.rb
+++ lib/rails_com/sprockets.rb
@@ -1,2 +1,15 @@
 require 'rails_com/sprockets/non_digest_assets'
 require 'rails_com/sprockets/qiniu_exporter'
+
+
+module Sprockets
+
+  def self.sync
+    config[:sync]
+  end
+
+  def self.sync=(sync)
+    self.config = hash_reassoc(config, :sync) { sync.dup }
+  end
+
+end
