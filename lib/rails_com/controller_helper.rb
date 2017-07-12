6a4323ea0d3b1f00e2b8336b86762c5040a64c6b
qinmingyuan <mingyuan0715@foxmail.com>
2017-07-12 13:53:42 +0800

    add method detect filter

--- lib/rails_com/controller_helper.rb
+++ lib/rails_com/controller_helper.rb
@@ -20,6 +20,21 @@ module RailsCom::ControllerHelper
     end
   end
 
+  def detect_filter(filter)
+    callback = self.__callbacks[:process_action].find { |i| i.filter == filter.to_sym }
+    return false unless callback
+
+    _if = callback.instance_variable_get(:@if).map do |c|
+      c.call(self)
+    end
+
+    _unless = callback.instance_variable_get(:@unless).map do |c|
+      !c.call(self)
+    end
+
+    !(_if + _unless).uniq.include?(false)
+  end
+
 end
 
 ActionController::Base.include RailsCom::ControllerHelper
