2e49837ab07dfc335b43e5f901984f7740d65552
qinmingyuan <mingyuan0715@foxmail.com>
2018-02-08 10:41:58 +0800

    add default url params

--- app/assets/javascripts/rails_com/fetch_xhr_script.js
+++ app/assets/javascripts/rails_com/fetch_xhr_script.js
@@ -1,4 +1,12 @@
 function fetch_xhr_script(url, params){
+  var default_params = {
+    credentials: 'include',
+    headers: {
+      'Accept': 'application/javascript',
+      'X-CSRF-Token': document.head.querySelector("[name=csrf-token]").content
+    }
+  };
+
   fetch(url, params).then(function(response) {
     return response.text()
   }).then(function(text) {
