64e50ab8b815d2f5224ff2dddcda998d7859549b
qinmingyuan <mingyuan0715@foxmail.com>
2018-02-12 10:34:40 +0800

    add ids & remove ids

--- app/assets/javascripts/rails_com/fetch_xhr_script.js
+++ app/assets/javascripts/rails_com/fetch_xhr_script.js
@@ -18,12 +18,36 @@ function fetch_xhr_script(url, params){
   })
 }
 
-function getCheckedIds(name) {
-  var x = 'input[name="' + name + '"]:checked';
-  var ids = [];
-  $(x).each(function(){
-    ids.push($(this).val())
+function listenCheckedIds(name) {
+  var checked = 'input[name="' + name + '"]:checked';
+  var unchecked = 'input[name="' + name + '"][checked!="checked"]';
+
+  window.add_ids = [];
+  window.remove_ids = [];
+  $(checked).change(function(){
+    if(this.checked){
+      var index = window.remove_ids.indexOf(this.value);
+      window.remove_ids.splice(index, 1);
+    } else {
+      window.remove_ids.push(this.value)
+    }
   });
-  ids = ids.join(',');
-  return ids
+  $(unchecked).change(function(){
+    if(this.checked){
+      window.add_ids.push(this.value)
+    } else {
+      var index = window.add_ids.indexOf(this.value);
+      window.add_ids.splice(index, 1);
+    }
+  });
+}
+
+function getAddIds(){
+  add_str = window.add_ids.join(',');
+  return add_str;
+}
+
+function getRemoveIds(){
+  remove_str = window.remove_ids.join(',');
+  return remove_str
 }
