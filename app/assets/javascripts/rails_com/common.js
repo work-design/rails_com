1e76d151d3f6cf2053e668dc1c954a79a0d28f9a
qinmingyuan <mingyuan0715@foxmail.com>
2018-03-01 11:03:08 +0800

    set timezone with turbolinks

--- app/assets/javascripts/rails_com/common.js
+++ app/assets/javascripts/rails_com/common.js
@@ -5,10 +5,28 @@ function timeForLocalized(){
   })
 }
 
-document.addEventListener('DOMContentLoaded', function(){
+function clickCallback(e) {
+  if (e.target.tagName !== 'A') {
+    return;
+  }
+  (new Date).getTimezoneOffset();
+}
+
+//document.addEventListener('click', clickCallback, false);
+
+document.addEventListener('DOMContentLoaded', function() {
   timeForLocalized()
 });
 
-document.addEventListener('turbolinks:load', function(){
+document.addEventListener('turbolinks:load', function() {
   timeForLocalized()
 });
+
+document.addEventListener('turbolinks:request-start', function(event) {
+  var xhr = event.data.xhr;
+  var offset = (new Date).getTimezoneOffset();
+  xhr.setRequestHeader('Utc-Offset', offset);
+});
+
+
+
