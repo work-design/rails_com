112c0b684ea39f7b0873f3bffd3272cb9ca380ff
qinmingyuan <mingyuan0715@foxmail.com>
2018-03-05 16:50:37 +0800

    improve time compute

--- lib/utils/time_helper.rb
+++ lib/utils/time_helper.rb
@@ -1,7 +1,6 @@
 module TimeHelper
   extend self
   
-  # todo consider timezone
   def exact_distance_time(from_time, to_time)
     from_time = from_time.to_datetime
     to_time = to_time.to_datetime
@@ -13,28 +12,19 @@ module TimeHelper
     days = to_time.mday - from_time.mday
     day_seconds = to_time.seconds_since_midnight.to_i - from_time.seconds_since_midnight.to_i
     
-    if [months, days, day_seconds].find { |i| i < 0 }
-      years -= 1
-    end
-
-    if months < 0
-      months = 12 + months
+    if day_seconds < 0
+      days -= 1
+      day_seconds = 86400 + day_seconds
     end
 
     if days < 0
       months -= 1
-    end
-
-    if days < 0
       days = to_time.prev_month.end_of_month.mday + days
     end
-
-    if day_seconds < 0
-      days -= 1
-    end
-
-    if day_seconds < 0
-      day_seconds = 86400 + day_seconds
+    
+    if months < 0
+      years -= 1
+      months = 12 + months
     end
 
     hours, minute_seconds = day_seconds.to_i.divmod(3600)
