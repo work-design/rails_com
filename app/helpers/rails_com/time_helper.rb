d389007e7d7a9ede6dc1c3d7c285625ac047782e
qinmingyuan <mingyuan0715@foxmail.com>
2018-03-21 17:35:52 +0800

    time helper

---
+++ app/helpers/rails_com/time_helper.rb
@@ -0,0 +1,21 @@
+module RailsCom::TimeHelper
+
+  def exact_distance_time(from_time, to_time, options = {})
+    result = TimeHelper.exact_distance_time(from_time, to_time)
+
+    options = {
+      scope: :'datetime.prompts'
+    }.merge!(options)
+
+    I18n.with_options locale: options[:locale], scope: options[:scope] do |locale|
+      str = ''
+      result.each do |k, v|
+        if v > 0
+          str << v.to_s << locale.t(k)
+        end
+      end
+      str
+    end
+  end
+
+end
