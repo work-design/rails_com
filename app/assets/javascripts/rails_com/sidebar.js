975a65167f904a2473d4ba480a3dc22c82db7c9d
qinmingyuan <mingyuan0715@foxmail.com>
2018-03-22 11:48:34 +0800

    select order

--- app/assets/javascripts/rails_com/sidebar.js
+++ app/assets/javascripts/rails_com/sidebar.js
@@ -3,7 +3,7 @@ function sidebarSide1() {
   $('.ui.side .ui.menu').removeClass('accordion');
   $('.ui.side .ui.menu').addClass('icon');
   $('.ui.side .ui.menu .dropdown.icon').remove();
-  $('.ui.side .ui.menu div.item:not(.header)').addClass('ui dropdown');
+  $('.ui.side .ui.menu .item:not(.header)').addClass('ui dropdown');
   $('.ui.dropdown.item').dropdown({on: 'hover'});
   $('.ui.side .ui.menu .item:not(.header)').removeClass('hidden');
 }
@@ -13,10 +13,10 @@ function sidebarSide2() {
   $('.ui.side .ui.menu').addClass('accordion');
   $('.ui.side .ui.menu').removeClass('icon');
   $('.ui.side .ui.menu .item:not(.header)').removeClass('ui dropdown');
+  $('.ui.side .ui.menu .item:not(.header)').dropdown('destroy');
   $('.ui.side .ui.menu .dropdown.icon').remove();
   $('.ui.side .ui.menu .title').append('<i class="dropdown icon"></i>');
   $('.ui.accordion').accordion({selector: {trigger: '.title'}});
-  $('.ui.side .ui.menu .item:not(.header)').dropdown('destroy');
 }
 
 document.addEventListener('turbolinks:load', function(){
