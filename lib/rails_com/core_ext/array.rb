1f0d0870cc0875111eb3e4c812d6e8cdf5fdc6e6
qinmingyuan <mingyuan0715@foxmail.com>
2018-03-28 15:13:06 +0800

    add example for core ext

--- lib/rails_com/core_ext/array.rb
+++ lib/rails_com/core_ext/array.rb
@@ -1,10 +1,16 @@
 class Array
 
+  # arr = [1, 2, 3]
+  # arr.rjust! 5, nil
+  # => [1, 2, 3, nil, nil]
   def rjust!(n, x)
     return self if n < length
     insert(0, *Array.new([0, n-length].max, x))
   end
 
+  # arr = [1, 2, 3]
+  # arr.ljust! 5, nil
+  # => [nil, nil, 1, 2, 3]
   def ljust!(n, x)
     return self if n < length
     fill(x, length...n)
@@ -23,6 +29,8 @@ class Array
   #   { a: 2 },
   #   { b: 2 }
   # ]
+  # raw_data.to_combined_hash
+  # => { a: [1, 2], b: 2 }
   def to_combined_hash
     self.reduce({}) do |memo, index|
       memo.merge(index) { |_, value, default| [value, default] }
@@ -35,6 +43,8 @@ class Array
   #   [:a, 2],
   #   [:b, 2]
   # ]
+  # raw_data.to_combined_h
+  # => { a: [1, 2], b: 2 }
   def to_combined_h
     hash = Hash.new { |hash, key| hash[key] = [] }
     self.each { |x, y| hash[x] << y }
