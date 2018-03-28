df3f0a3679ffa7a5e0feefc6c49685d17cea52c4
qinmingyuan <mingyuan0715@foxmail.com>
2018-03-28 14:54:43 +0800

    add to combined h

--- lib/rails_com/core_ext/array.rb
+++ lib/rails_com/core_ext/array.rb
@@ -4,18 +4,31 @@ class Array
     return self if n < length
     insert(0, *Array.new([0, n-length].max, x))
   end
-  
+
   def ljust!(n, x)
     return self if n < length
     fill(x, length...n)
   end
-  
+
   # arr = [1, 2, 3]
   # arr.mjust!(5, nil)
   # => [1, 2, nil, nil, 3]
   def mjust!(n, x)
     return self if n < length
     insert (length / 2.0).ceil, *Array.new(n - length, x)
+  end
+
+  # def to_combined_h
+  #   self.map! { |a| { a[0] => a[1] } }
+  #   self.reduce({}) do |memo, index|
+  #     memo.merge(index) { |_, value, default| [value, default] }
+  #   end
+  # end
+  # see ruby cook book
+  def to_combined_h
+    hash = Hash.new { |hash, key| hash[key] = [] }
+    self.each { |x, y| hash[x] << y }
+    hash
   end
 
 end
