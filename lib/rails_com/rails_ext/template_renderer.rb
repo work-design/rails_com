b2f245c82507fffd875255d58ed1e69bb43fb165
qinmingyuan <mingyuan0715@foxmail.com>
2018-03-30 09:26:52 +0800

    remove a space

--- lib/rails_com/rails_ext/template_renderer.rb
+++ lib/rails_com/rails_ext/template_renderer.rb
@@ -1,6 +1,6 @@
 module RailsExt
   module TemplateRenderer
-    
+
     # record where the view rendered from, main project or which engine
     # used by view helper methods: js_load, css_load, js_ready
     def render_template(template, layout_name = nil, locals = nil)
@@ -11,7 +11,7 @@ module RailsExt
 
       engine = result.classify.safe_constantize
 
-      @view.instance_variable_set(:@_rendered_from,  engine.root) if engine
+      @view.instance_variable_set(:@_rendered_from, engine.root) if engine
 
       super
     end
