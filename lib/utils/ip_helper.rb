a5d35be7efa9c772a0cb4013714ebd2c9a174c9b
qinmingyuan <mingyuan0715@foxmail.com>
2018-03-30 21:59:55 +0800

    example for import ip

---
+++ lib/utils/ip_helper.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module IpHelper
+  extend self
+
+  def read(name)
+    IpAddress.delete_all
+    IO.foreach(name) do |x|
+      if x.match? /apnic\|CN\|ipv4/
+        r = x.split('|').values_at(3, 4)
+        min = IPAddr.new(r[0]).to_i
+        max = min + r[1].to_i
+        max_ip = IPAddr.new(max, Socket::AF_INET).to_s
+      end
+    end
+  end
+
+  def save
+    IpAddress.create(
+      int_start: min,
+      int_finish: max,
+      ip_start: r[0],
+      ip_finish: max_ip,
+      ip_type: 'ipv4',
+      registry: 'CN'
+    )
+  end
+
+end
