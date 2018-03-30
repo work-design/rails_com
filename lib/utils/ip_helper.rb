a22bb8c2ce51c6bb21c786354e28038744290025
qinmingyuan <mingyuan0715@foxmail.com>
2018-03-30 22:19:42 +0800

    ip helper add links

--- lib/utils/ip_helper.rb
+++ lib/utils/ip_helper.rb
@@ -1,29 +1,21 @@
 # frozen_string_literal: true
 
+# 亚洲最新ip地址
+# http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest
 module IpHelper
   extend self
 
   def read(name)
-    IpAddress.delete_all
     IO.foreach(name) do |x|
       if x.match? /apnic\|CN\|ipv4/
         r = x.split('|').values_at(3, 4)
-        min = IPAddr.new(r[0]).to_i
-        max = min + r[1].to_i - 1
-        max_ip = IPAddr.new(max, Socket::AF_INET).to_s
+        int_min = IPAddr.new(r[0]).to_i
+        int_max = min + r[1].to_i - 1
+        ip_max = IPAddr.new(max, Socket::AF_INET).to_s
+
+        [r[0], ip_max, int_min, int_max]
       end
     end
-  end
-
-  def save
-    IpAddress.create(
-      int_start: min,
-      int_finish: max,
-      ip_start: r[0],
-      ip_finish: max_ip,
-      ip_type: 'ipv4',
-      registry: 'CN'
-    )
   end
 
 end
