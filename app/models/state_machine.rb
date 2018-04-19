8d6dc721777c9e5407724b12abdf1905741ca6c6
qinmingyuan <mingyuan0715@foxmail.com>
2018-04-19 13:33:34 +0800

    add state machine

--- app/models/state_machine.rb
+++ app/models/state_machine.rb
@@ -1,35 +1,49 @@
 module StateMachine
 
   # obj.process_to state: 'xxx'
-  def process_to(options = {})
+  def process_to(options = {}, &block)
     options.each do |column, value|
-      next_state = next_states(column).first
+      next_state = next_states(column, &block).first
 
       if next_state == value.to_s
         assign_attributes(column => value)
-        save!
       else
-        errors.add column, 'Next state is wrong'
-        raise ActiveRecord::Rollback, 'Next state is wrong'
+        error_msg = "Next state is wrong, should be #{next_state}"
+        errors.add column, error_msg
+        raise ActiveRecord::Rollback, error_msg
       end
     end
   end
 
-  def jump_to(options = {})
+  def process_to!(options = {}, &block)
+    self.process_to(options, &block)
+    self.save!
+  end
+
+  def jump_to!(options = {}, &block)
+    self.jump_to(options, &block)
+    self.save!
+  end
+
+  def jump_to(options = {}, &block)
     options.each do |column, value|
-      next_states = next_states(column)
+      next_states = next_states(column, &block)
 
       if next_states.include?(value.to_s)
         assign_attributes(column => value)
-        save!
       else
-        errors.add column, 'Next state is wrong'
-        raise ActiveRecord::Rollback, 'Next state is wrong'
+        error_msg = "Next state is wrong, should be one of #{next_states.join(', ')}"
+        errors.add column, error_msg
+        raise ActiveRecord::Rollback, error_msg
       end
     end
   end
 
-  def next_states(state_name)
+
+  # obj.next_states(:state) do |result|
+  #   result.reject
+  # end
+  def next_states(state_name, &block)
     _states = self.class.send(state_name.to_s.pluralize)
     _states = _states.keys
     _state = self.send(state_name)
@@ -40,7 +54,13 @@ module StateMachine
       i = _states.index(_state)
       next_index = i + 1
     end
-    _states[next_index..(_states.size - 1)]
+    result = _states[next_index..(_states.size - 1)]
+
+    if block_given?
+      yield block.call(result)
+    else
+      result
+    end
   end
 
 end
