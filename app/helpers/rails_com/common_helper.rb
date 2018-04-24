e7482abd67b07dc06ac0752b4b7080e1f0f9f21d
qinmingyuan <mingyuan0715@foxmail.com>
2018-04-24 14:41:19 +0800

    add a common helper

---
+++ app/helpers/rails_com/common_helper.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+module RailsCom::CommonHelper
+
+  def simple_format_hash(hash_text, options = {})
+    wrapper_tag = options.fetch(:wrapper_tag, :p)
+
+    hash_text.map do |k, v|
+      text = k.to_s + ': ' + v.to_s
+      content_tag(wrapper_tag, text)
+    end.join("\n\n").html_safe
+  end
+
+end
