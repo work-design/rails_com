c1e2354652a61d14da8c14f52db4661588fda6bf
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-02 09:39:26 +0800

    upgrade gemfiles

--- lib/rails_com/engine.rb
+++ lib/rails_com/engine.rb
@@ -5,12 +5,21 @@ module RailsCom
       app.config.assets.precompile += ['rails_com_manifest.js']
     end
 
-    initializer 'rails_com.add_generator_templates' do |app|
-      app.config.paths['lib/templates'].push File.expand_path('lib/templates', root)
-    end
+    # initializer 'rails_com.add_generator_templates' do |app|
+    #   app.config.paths['lib/templates'].unshift File.expand_path('lib/templates', root)
+    # end
 
     initializer 'rails_com.add_assets_templates' do |app|
       app.config.assets.paths.push *Dir[File.expand_path('lib/nondigest_assets/*', root)]
+    end
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javasricpts false
+      g.javascript_engine false
+      g.helper false
+      g.jbuilder false
+      g.templates.unshift File.expand_path('lib/templates', root)
     end
 
   end
