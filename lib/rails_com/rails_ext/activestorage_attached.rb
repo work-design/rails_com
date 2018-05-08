0cc15a980a6499282debc1ccd5f577c1e50a8312
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-08 09:55:37 +0800

    user url basename for filename

--- lib/rails_com/rails_ext/activestorage_attached.rb
+++ lib/rails_com/rails_ext/activestorage_attached.rb
@@ -1,11 +1,10 @@
 module ActiveStorage
   class Attached
 
-    def url_sync(url, filename = 'tmp_file_' + self.record.id.to_s)
-      tmp_path = File.expand_path 'tmp/storage_migrate'
-      file_path = File.join tmp_path, filename.to_s
+    def url_sync(url)
+      filename = File.basename URI(url).path
 
-      File.open(file_path, 'w+') do |file|
+      Tempfile.open do |file|
         file.binmode
         HTTParty.get(url, stream_body: true) do |fragment|
           file.write fragment
