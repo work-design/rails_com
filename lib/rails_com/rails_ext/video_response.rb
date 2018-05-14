b97dee795825052395c1712369317459cfc05813
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-14 11:08:53 +0800

    compat with safari

--- lib/rails_com/rails_ext/video_response.rb
+++ lib/rails_com/rails_ext/video_response.rb
@@ -12,16 +12,11 @@ module VideoResponse
   end
 
   def add_range_headers
-    file_begin = 0
     file_size = self.response.body.size
-    file_end = file_size - 1
-
-    if request.headers['Range']
-      match = request.headers['range'].match(/bytes=(\d+)-(\d*)/)
-      if match
-        file_begin = match[1]
-        file_end = match[2].present? ? match[2] : match[1]
-      end
+    match = request.headers['range'].to_s.match(/bytes=(\d+)-(\d*)/)
+    file_begin = match[1]
+    file_end = match[2].presence
+    if file_begin && file_end
       response.header['Content-Range'] = 'bytes ' + file_begin.to_s + '-' + file_end.to_s + '/' + file_size.to_s
       response.header['Content-Length'] = (file_end.to_i - file_begin.to_i + 1).to_s
       response.status = 206
