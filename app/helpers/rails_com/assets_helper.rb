bd066b554abaa5ffda82c54db25d4dc19a2a73d0
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-17 23:30:50 +0800

    assets helper support root options

--- app/helpers/rails_com/assets_helper.rb
+++ app/helpers/rails_com/assets_helper.rb
@@ -5,13 +5,20 @@ module RailsCom::AssetsHelper
     filename ||= "controllers/#{controller_path}/#{action_name}"
     paths = []
 
-    rails_path = Rails.root.join 'app/assets/javascripts', filename
-    paths << rails_path.to_s + '.js'
-    paths << rails_path.to_s + '.js.erb'
-    if @_rendered_from
-      engine_path = @_rendered_from.join 'app/assets/javascripts', filename
-      paths << engine_path.to_s + '.js'
-      paths << engine_path.to_s + '.js.erb'
+    if root
+      root_path = Pathname.new(root).join filename
+      paths << root_path.to_s + '.js'
+      paths << root_path.to_s + '.js.erb'
+    else
+      file_path = Pathname.new("app/assets/javascripts").join filename
+      rails_path = Rails.root.join file_path
+      paths << rails_path.to_s + '.js'
+      paths << rails_path.to_s + '.js.erb'
+      if @_rendered_from
+        engine_path = @_rendered_from.join file_path
+        paths << engine_path.to_s + '.js'
+        paths << engine_path.to_s + '.js.erb'
+      end
     end
 
     if paths.map { |path| File.exist?(path) }.include?(true)
@@ -23,23 +30,29 @@ module RailsCom::AssetsHelper
     filename ||= "controllers/#{controller_path}/#{action_name}"
     paths = []
 
-    rails_path = Rails.root.join 'app/assets/stylesheets', filename
-    paths << rails_path.to_s + '.css'
-    paths << rails_path.to_s + '.css.erb'
-    if @_rendered_from
-      engine_path = @_rendered_from.join 'app/assets/stylesheets', filename
-      paths << engine_path.to_s + '.css'
-      paths << engine_path.to_s + '.css.erb'
+    if root
+      root_path = Pathname.new(root).join filename
+      paths << root_path.to_s + '.css'
+      paths << root_path.to_s + '.css.erb'
+    else
+      file_path = Pathname.new("app/assets/stylesheets").join filename
+      rails_path = Rails.root.join '', file_path
+      paths << rails_path.to_s + '.css'
+      paths << rails_path.to_s + '.css.erb'
+      if @_rendered_from
+        engine_path = @_rendered_from.join 'app/assets/stylesheets', file_path
+        paths << engine_path.to_s + '.css'
+        paths << engine_path.to_s + '.css.erb'
+      end
     end
-    
+
     if paths.map { |path| File.exist?(path) }.include?(true)
       stylesheet_link_tag filename, options
     end
   end
 
-  def js_ready(root: nil, **options)
-    filename ||= "controllers/#{controller_path}/#{action_name}.ready"
-    js_load(filename, root: root, **options)
+  def js_ready(**options)
+    js_load("controllers/#{controller_path}/#{action_name}.ready", **options)
   end
 
 end
