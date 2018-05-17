8cce6cf22e0e4ed6900b832c9e810791c082511b
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-17 22:44:12 +0800

    use join

--- app/helpers/rails_com/assets_helper.rb
+++ app/helpers/rails_com/assets_helper.rb
@@ -4,8 +4,8 @@ module RailsCom::AssetsHelper
   def js_load(filename = nil, root: nil, **options)
     filename ||= "controllers/#{controller_path}/#{action_name}"
     relative_path = 'app/assets/javascripts' + filename.to_s
-    rails_path = Rails.root + relative_path
-    engine_path = @_rendered_from + relative_path
+    rails_path = Rails.root.join relative_path
+    engine_path = @_rendered_from.join relative_path
     result = [
       rails_path.to_s + '.js',
       rails_path.to_s + '.js.erb',
@@ -14,6 +14,7 @@ module RailsCom::AssetsHelper
     ].map do |path|
       File.exist?(path)
     end
+    binding.pry
     if result.include?(true)
       javascript_include_tag filename, options
     end
@@ -22,8 +23,8 @@ module RailsCom::AssetsHelper
   def css_load(filename = nil, root: nil, **options)
     filename ||= "controllers/#{controller_path}/#{action_name}"
     relative_path = 'app/assets/stylesheets' + filename.to_s
-    rails_path = Rails.root + relative_path + '.css'
-    engine_path = @_rendered_from + relative_path + '.css'
+    rails_path = Rails.root.join relative_path + '.css'
+    engine_path = @_rendered_from.join relative_path + '.css'
     if File.exist?(rails_path) || File.exist?(engine_path)
       stylesheet_link_tag filename, options
     end
