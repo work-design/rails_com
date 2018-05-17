ada0aba5889ffa102ff245c33ed5a6dcc1e9e48d
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-17 11:50:27 +0800

    better files folder

--- lib/rails_com.rb
+++ lib/rails_com.rb
@@ -1,11 +1,15 @@
 require 'rails_com/version'
 require 'rails_com/config'
+require 'rails_com/engine'
 
-require 'rails_com/routes'
-require 'rails_com/models'
-require 'rails_com/controllers'
-require 'rails_com/model_helper'
-require 'rails_com/controller_helper'
+# Meta for Rails
+require 'rails_com/meta/routes'
+require 'rails_com/meta/models'
+require 'rails_com/meta/controllers'
+
+# Rails Helper
+require 'rails_com/helpers/model_helper'
+require 'rails_com/helpers/controller_helper'
 
 # Ruby core extension
 require 'rails_com/core_ext/hash'
@@ -26,5 +30,3 @@ require 'rails_com/sprockets/non_digest_assets'
 require 'utils/time_helper'
 require 'utils/uid_helper'
 require 'utils/jobber'
-
-require 'rails_com/engine'
