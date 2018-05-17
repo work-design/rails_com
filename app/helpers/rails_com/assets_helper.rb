0237d557db17c0e18f30eca8964a42ca9fd5b8bb
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-17 23:08:57 +0800

    fix errors

--- app/helpers/rails_com/assets_helper.rb
+++ app/helpers/rails_com/assets_helper.rb
@@ -3,26 +3,36 @@ module RailsCom::AssetsHelper
 
   def js_load(filename = nil, root: nil, **options)
     filename ||= "controllers/#{controller_path}/#{action_name}"
+    paths = []
+
     rails_path = Rails.root.join 'app/assets/javascripts', filename
-    engine_path = @_rendered_from.join 'app/assets/javascripts', filename
-    result = [
-      rails_path.to_s + '.js',
-      rails_path.to_s + '.js.erb',
-      engine_path.to_s + '.js',
-      engine_path.to_s + '.js.erb'
-    ].map do |path|
-      File.exist?(path)
+    paths << rails_path.to_s + '.js'
+    paths << rails_path.to_s + '.js.erb'
+    if @_rendered_from
+      engine_path = @_rendered_from.join 'app/assets/javascripts', filename
+      paths << engine_path.to_s + '.js'
+      paths << engine_path.to_s + '.js.erb'
     end
-    if result.include?(true)
+
+    if paths.map { |path| File.exist?(path) }.include?(true)
       javascript_include_tag filename, options
     end
   end
 
   def css_load(filename = nil, root: nil, **options)
     filename ||= "controllers/#{controller_path}/#{action_name}"
+    paths = []
+
     rails_path = Rails.root.join 'app/assets/stylesheets', filename
-    engine_path = @_rendered_from.join 'app/assets/stylesheets', filename
-    if File.exist?(rails_path.to_s + '.css') || File.exist?(engine_path.to_s + '.css')
+    paths << rails_path.to_s + '.css'
+    paths << rails_path.to_s + '.css.erb'
+    if @_rendered_from
+      engine_path = @_rendered_from.join 'app/assets/stylesheets', filename
+      paths << engine_path.to_s + '.css'
+      paths << engine_path.to_s + '.css.erb'
+    end
+    
+    if paths.map { |path| File.exist?(path) }.include?(true)
       stylesheet_link_tag filename, options
     end
   end
