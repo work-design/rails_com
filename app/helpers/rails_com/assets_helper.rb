8908bb9397739ac26375ae352942244f374f9537
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-17 22:30:59 +0800

    update assets helpers

--- app/helpers/rails_com/assets_helper.rb
+++ app/helpers/rails_com/assets_helper.rb
@@ -3,29 +3,35 @@ module RailsCom::AssetsHelper
 
   def js_load(filename = nil, root: nil, **options)
     filename ||= "controllers/#{controller_path}/#{action_name}"
-    root ||= @_rendered_from || Rails.root
-    path = root + 'app/assets/javascripts' + filename.to_s
-    if File.exist?(path.to_s + '.js') || File.exist?(path.to_s + '.js.erb')
+    relative_path = 'app/assets/javascripts' + filename.to_s
+    rails_path = Rails.root + relative_path
+    engine_path = @_rendered_from + relative_path
+    result = [
+      rails_path.to_s + '.js',
+      rails_path.to_s + '.js.erb',
+      engine_path.to_s + '.js',
+      engine_path.to_s + '.js.erb'
+    ].map do |path|
+      File.exist?(path)
+    end
+    if result.include?(true)
       javascript_include_tag filename, options
     end
   end
 
   def css_load(filename = nil, root: nil, **options)
     filename ||= "controllers/#{controller_path}/#{action_name}"
-    root ||= @_rendered_from || Rails.root
-    path = root + 'app/assets/stylesheets' + filename.to_s
-    if File.exist?(path.to_s + '.css')
+    relative_path = 'app/assets/stylesheets' + filename.to_s
+    rails_path = Rails.root + relative_path + '.css'
+    engine_path = @_rendered_from + relative_path + '.css'
+    if File.exist?(rails_path) || File.exist?(engine_path)
       stylesheet_link_tag filename, options
     end
   end
 
-  def js_ready(filename = nil, root: nil, **options)
+  def js_ready(root: nil, **options)
     filename ||= "controllers/#{controller_path}/#{action_name}.ready"
-    root ||= @_rendered_from || Rails.root
-    path = root + 'app/assets/javascripts' + filename.to_s
-    if File.exist?(path.to_s + '.js') || File.exist?(path.to_s + '.js.erb')
-      javascript_include_tag filename, options
-    end
+    js_load(filename, root: root, **options)
   end
 
 end
