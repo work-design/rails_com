c234f3691ccdc5b1d21163577786615e6d63fa72
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-21 10:18:24 +0800

    format text helper

--- app/helpers/rails_com/common_helper.rb
+++ app/helpers/rails_com/common_helper.rb
@@ -10,4 +10,21 @@ module RailsCom::CommonHelper
     end.join("\n\n").html_safe
   end
 
+  def simple_format(text, html_options = {}, options = {})
+    if text.is_a?(Hash)
+      return simple_format_hash(text, options)
+    end
+
+    if text.is_a?(Array)
+      begin
+        hash_text = text.to_h
+        return simple_format_hash(hash_text, options)
+      rescue TypeError => e
+        return text
+      end
+    end
+
+    super
+  end
+
 end
