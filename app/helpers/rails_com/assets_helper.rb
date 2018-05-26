c20895126b70bd4896b57c88028fada2ff335b1b
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-26 16:11:09 +0800

    support js pack and minify codes

--- app/helpers/rails_com/assets_helper.rb
+++ app/helpers/rails_com/assets_helper.rb
@@ -1,58 +1,56 @@
 # frozen_string_literal: true
 module RailsCom::AssetsHelper
 
-  def js_load(filename = nil, root: nil, **options)
-    filename ||= "controllers/#{controller_path}/#{action_name}"
-    paths = []
-
-    if root
-      root_path = Pathname.new(root).join filename
-      paths << root_path.to_s + '.js'
-      paths << root_path.to_s + '.js.erb'
-    else
-      file_path = Pathname.new("app/assets/javascripts").join filename
-      rails_path = Rails.root.join file_path
-      paths << rails_path.to_s + '.js'
-      paths << rails_path.to_s + '.js.erb'
-      if @_rendered_from
-        engine_path = @_rendered_from.join file_path
-        paths << engine_path.to_s + '.js'
-        paths << engine_path.to_s + '.js.erb'
-      end
-    end
+  def js_load(filename = nil, **options)
+    paths = assets_load_path(filename)
 
     if paths.any? { |path| File.exist?(path) }
       javascript_include_tag filename, options
     end
   end
 
-  def css_load(filename = nil, root: nil, **options)
-    filename ||= "controllers/#{controller_path}/#{action_name}"
-    paths = []
-
-    if root
-      root_path = Pathname.new(root).join filename
-      paths << root_path.to_s + '.css'
-      paths << root_path.to_s + '.css.erb'
-    else
-      file_path = Pathname.new("app/assets/stylesheets").join filename
-      rails_path = Rails.root.join file_path
-      paths << rails_path.to_s + '.css'
-      paths << rails_path.to_s + '.css.erb'
-      if @_rendered_from
-        engine_path = @_rendered_from.join file_path
-        paths << engine_path.to_s + '.css'
-        paths << engine_path.to_s + '.css.erb'
-      end
-    end
+  def css_load(filename = nil, **options)
+    paths = assets_load_path(filename, relative_path: 'app/assets/stylesheets', ext: ['.css', '.css.erb'])
 
     if paths.any? { |path| File.exist?(path) }
       stylesheet_link_tag filename, options
     end
   end
 
+  def js_pack(filename = nil, **options)
+    relative_path = 'app/javascript/packs'
+    paths = assets_load_path(filename, relative_path: relative_path, **options)
+
+    if paths.any? { |path| File.exist?(path) }
+      javascript_pack_tag filename, options
+    end
+  end
+
   def js_ready(**options)
     js_load("controllers/#{controller_path}/#{action_name}.ready", **options)
+  end
+
+  def js_pack_ready(**options)
+    js_pack("controllers/#{controller_path}/#{action_name}.ready", **options)
+  end
+
+  def assets_load_path(filename = nil, relative_path: 'app/assets/javascripts', ext: ['.js', '.js.erb'])
+    filename ||= "controllers/#{controller_path}/#{action_name}"
+    paths = []
+
+    file_path = Pathname.new(relative_path).join filename
+    rails_path = Rails.root.join file_path
+    ext.each do |name|
+      paths << rails_path.to_s + name
+    end
+    if @_rendered_from
+      engine_path = @_rendered_from.join file_path
+      ext.each do |name|
+        paths << engine_path.to_s + name
+      end
+    end
+
+    paths
   end
 
 end
