e95557e1fabb18ce2d0551b4fe93748f6dba9ee2
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-29 11:30:44 +0800

    num helper to rmb

--- lib/rails_com/utils/num_helper.rb
+++ lib/rails_com/utils/num_helper.rb
@@ -1,43 +1,44 @@
 module NumHelper
   NUM = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖']
   DEL = ['', '拾', '佰', '仟']
-  UNIT = ['', '万', '亿', '兆', '京']
-  SUB_UNIT = ['角', '分']
+  SUB_DEL = ['分', '角']
+  UNIT = ['元', '万', '亿', '兆', '京']
   DELIMITER_REGEX = /(\d)(?=(\d\d\d\d)+(?!\d))/
   extend self
 
-  def to_rmb(num, unit: '元')
+  def to_rmb(num)
     left, right = num.to_s(:rounded, precision: 2, strip_insignificant_zeros: true, separator: '.', delimiter: ',', delimiter_pattern: DELIMITER_REGEX).split('.')
 
-    left_str = xxx(left, unit: UNIT)
-    right_str = xxx(right, unit: SUB_UNIT)
+    left_str = to_parts(left, unit: UNIT, del: DEL)
 
     if right
+      right_str = to_parts(right, del: SUB_DEL)
       left_str + right_str
     else
       left_str + '整'
     end
   end
 
-  def xxx(arr, unit: [])
-    han_arr = arr.split(',').reverse.map.with_index do |str, index|
+  def to_parts(num_str, unit: [], del: [])
+    han_arr = num_str.split(',').reverse.map.with_index do |str, index|
       str = str.each_char.map { |i| NUM[i.to_i] }
       xx = ''
-      str.zip(DEL[0..str.size - 1].reverse) do |st, de|
-        if st != '零'
+      str.zip(del[0..str.size - 1].reverse) do |st, de|
+        if st != '零' && de
           xx << st + de
         else
           xx << st unless xx.end_with?('零')
         end
       end
       if xx.chomp!('零')
-        xx << (unit[index]) << '零'
+        xx << (unit[index].to_s) << '零'
       else
-        xx << (unit[index])
+        xx << (unit[index].to_s)
       end
       xx
     end
-    han_arr.reverse.join + unit.to_s
+
+    han_arr.reverse.join
   end
 
 end
