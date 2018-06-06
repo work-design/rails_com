66604451739fde98c55256916da1811cb466d432
qinmingyuan <mingyuan0715@foxmail.com>
2018-06-06 11:35:20 +0800

    revert

--- lib/rails_com/rails_ext/video_response.rb
+++ lib/rails_com/rails_ext/video_response.rb
@@ -16,7 +16,7 @@ module VideoResponse
     match = request.headers['range'].to_s.match(/bytes=(\d+)-(\d*)/)
     match = Array(match)
     file_begin = match[1].presence
-    file_end = match[2].presence || file_size
+    file_end = match[2].presence
     if file_begin && file_end
       response.header['Content-Range'] = 'bytes ' + file_begin.to_s + '-' + file_end.to_s + '/' + file_size.to_s
       response.header['Content-Length'] = (file_end.to_i - file_begin.to_i + 1).to_s
