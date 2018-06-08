74aa897d391f0a3602f6db0693b494513a3ffdea
qinmingyuan <mingyuan0715@foxmail.com>
2018-06-08 09:10:03 +0800

    add transfer

--- lib/rails_com/rails_ext/activestorage_attached.rb
+++ lib/rails_com/rails_ext/activestorage_attached.rb
@@ -1,5 +1,6 @@
 module ActiveStorage
   class Attached
+    include Downloading
 
     def url_sync(url)
       filename = File.basename URI(url).path
@@ -15,6 +16,21 @@ module ActiveStorage
       end
     end
 
+    def transfer_faststart
+      download_blob_to_tempfile do |input|
+        Timefile.open do |file|
+          file.binmode
+          argv = [ffmpeg_path, '-i', input.path, '-codec', 'copy', '-movflags', 'faststart']
+          IO.popen(argv, err: File::NULL) { |out| IO.copy_stream(out, file) }
+          file.rewind
+          self.attach io: file, filename: file.original_filename
+        end
+      end
+    end
+
+    def ffmpeg_path
+      ActiveStorage.paths[:ffmpeg] || 'ffmpeg'
+    end
 
     class One
 
