92ef0afb55a832a29bc5782f1e395b0923c0bfe1
qinmingyuan <mingyuan0715@foxmail.com>
2018-07-10 20:18:16 +0800

    add a helpermethps

--- lib/rails_com/utils/time_helper.rb
+++ lib/rails_com/utils/time_helper.rb
@@ -1,6 +1,6 @@
 module TimeHelper
   extend self
-  
+
   def exact_distance_time(from_time, to_time)
     from_time = from_time.to_datetime
     to_time = to_time.to_datetime
@@ -11,7 +11,7 @@ module TimeHelper
     months = to_time.month - from_time.month
     days = to_time.mday - from_time.mday
     day_seconds = to_time.seconds_since_midnight.to_i - from_time.seconds_since_midnight.to_i
-    
+
     if day_seconds < 0
       days -= 1
       day_seconds = 86400 + day_seconds
@@ -21,7 +21,7 @@ module TimeHelper
       months -= 1
       days = to_time.prev_month.end_of_month.mday + days
     end
-    
+
     if months < 0
       years -= 1
       months = 12 + months
@@ -33,5 +33,26 @@ module TimeHelper
     { year: years, month: months, day: days, hour: hours, minute: minutes, second: seconds }
   end
 
-end
+  def interval(start_at, finish_at, interval_start: '12:30', since: 1.hour)
+    raise 'Must be same day!' if start_at.to_date != finish_at.to_date
+    return 0 if start_at >= finish_at
 
+    hour, min = interval_start.split(':')
+    interval_start_at = start_at.change hour: hour, min: min
+    interval_finish_at = interval_start_at.since(since)
+
+    if start_at < interval_start_at && finish_at >= interval_finish_at
+      seconds = ((finish_at - start_at) - since).to_i
+    elsif start_at < interval_start_at && finish_at > interval_start_at && finish_at < interval_finish_at
+      seconds = interval_start_at - start_at
+    elsif start_at >= interval_start_at && start_at < interval_finish_at && finish_at >= interval_finish_at
+      seconds = finish_at - interval_finish_at
+    elsif start_at >= interval_start_at && start_at < interval_finish_at && finish_at <= interval_finish_at
+      seconds = 0
+    else
+      seconds = finish_at - start_at
+    end
+    seconds
+  end
+
+end
