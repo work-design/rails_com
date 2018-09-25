7a18ad7d33de9e0c56bcbbbcd8b84b6a3aff1845
qinmingyuan <mingyuan0715@foxmail.com>
2018-09-25 22:12:49 +0800

    support forbidden error

--- app/controllers/concerns/rails_common_api.rb
+++ app/controllers/concerns/rails_common_api.rb
@@ -7,14 +7,19 @@ module RailsCommonApi
       render json: { error: { class: exp.class.inspect }, message: exp.message }, status: 500
     end
 
-    rescue_from 'ActionController::UnauthorizedError' do |exp|
-      puts nil, exp.full_message(highlight: true, order: :top)
-      render json: { error: { class: exp.class.inspect }, message: exp.message }, status: 401
-    end
-
     rescue_from 'ActiveRecord::RecordNotFound' do |exp|
       puts nil, exp.full_message(highlight: true, order: :top)
       render json: { error: { class: exp.class.inspect }, message: exp.message }, status: 404
+    end
+
+    rescue_from 'ActionController::ForbiddenError' do |exp|
+      puts nil, exp.full_message(highlight: true, order: :top)
+      render json: { error: { class: exp.class.inspect }, message: exp.message }, status: 403
+    end
+
+    rescue_from 'ActionController::UnauthorizedError' do |exp|
+      puts nil, exp.full_message(highlight: true, order: :top)
+      render json: { error: { class: exp.class.inspect }, message: exp.message }, status: 401
     end
   end
 
--- lib/rails_com/rails_ext/errors.rb
+++ lib/rails_com/rails_ext/errors.rb
@@ -1,3 +1,4 @@
 module ActionController
   class UnauthorizedError < StandardError; end
+  class ForbiddenError < StandardError; end
 end
