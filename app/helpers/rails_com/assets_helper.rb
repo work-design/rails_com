72e6739303a176c31631375b9382c604cdde1ee9
qinmingyuan <mingyuan0715@foxmail.com>
2018-10-12 15:50:17 +0800

    fix assets load error

--- app/helpers/rails_com/assets_helper.rb
+++ app/helpers/rails_com/assets_helper.rb
@@ -52,7 +52,18 @@ module RailsCom::AssetsHelper
     end
 
     if pack_paths.any? { |path| File.exist?(path) }
-      r << stylesheet_pack_tag(pack_filename, options)
+      
+      begin
+        rs = stylesheet_pack_tag(pack_filename, options)
+      rescue
+        rs = nil
+      end
+
+      if rs.is_a?(String)
+        r << rs
+      else
+        r << javascript_pack_tag(pack_filename, options)
+      end
     end
 
     r.join("\n    ").html_safe
