dad1e4bec2c87e026e9339106b54bb8552122e3b
qinmingyuan <mingyuan0715@foxmail.com>
2018-10-22 21:42:24 +0800

    attachment

--- db/migrate/20181012025833_rails_com_init.rb
+++ db/migrate/20181012025833_rails_com_init.rb
@@ -1,7 +1,6 @@
-class CreateActiveStorageBlobDefaults < ActiveRecord::Migration[5.2]
+class RailsComInit < ActiveRecord::Migration[5.2]
   def change
     create_table :active_storage_blob_defaults do |t|
-      t.references :blob
       t.string :record_class
       t.string :name
       t.timestamps
