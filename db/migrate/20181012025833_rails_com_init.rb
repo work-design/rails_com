ffbfa6dacb9158e96d5ad32590c8276acdbedaff
qinmingyuan <mingyuan0715@foxmail.com>
2018-10-26 14:01:41 +0800

    less code

--- db/migrate/20181012025833_rails_com_init.rb
+++ db/migrate/20181012025833_rails_com_init.rb
@@ -1,9 +1,13 @@
 class RailsComInit < ActiveRecord::Migration[5.2]
   def change
+
     create_table :active_storage_blob_defaults do |t|
       t.string :record_class
       t.string :name
       t.timestamps
     end
+
+    change_column_null :active_storage_blobs, :checksum, true
+
   end
 end
