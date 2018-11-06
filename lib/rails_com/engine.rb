13a6f6da4cda04dfae7c04ef42291b0fdbe0bc93
qinmingyuan <mingyuan0715@foxmail.com>
2018-11-06 15:34:56 +0800

    default app config

--- lib/rails_com/engine.rb
+++ lib/rails_com/engine.rb
@@ -11,6 +11,9 @@ class RailsCom::Engine < ::Rails::Engine #:nodoc:
 
   initializer 'rails_com.assets.precompile' do |app|
     app.config.assets.precompile += ['rails_com_manifest.js']
+    app.config.content_security_policy_nonce_generator = -> request {
+      request.headers['Turbolinks-Csp-Nonce'] || SecureRandom.base64(16)
+    }
   end
 
   initializer 'rails_com.add_assets_templates' do |app|
