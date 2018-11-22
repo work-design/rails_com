624f8e213ef950a9e76c405763b296a6d33dd3a1
qinmingyuan <mingyuan0715@foxmail.com>
2018-11-22 10:08:45 +0800

    add dependent moment

--- app/assets/javascripts/rails_com/application.js
+++ app/assets/javascripts/rails_com/application.js
@@ -1,16 +1,7 @@
 //= require rails-ujs
 //= require turbolinks
+//= require ./time_local
 //= require_self
-
-function timeForLocalized(){
-  $('time[data-localized!="true"]').each(function(){
-    if (this.textContent.length > 0) {
-      var format = this.dataset['format'] || 'YYYY-MM-DD HH:mm:ss';
-      this.textContent = moment.utc(this.textContent).local().format(format);
-      this.dataset['localized'] = 'true'
-    }
-  })
-}
 
 function clickCallback(e) {
   if (e.target.tagName !== 'A') {
