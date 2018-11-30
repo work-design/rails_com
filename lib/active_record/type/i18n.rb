41bb5451fd5e3af2b1eb7e5c42e5ef5a6bfa9c40
qinmingyuan <mingyuan0715@foxmail.com>
2018-11-30 18:37:02 +0800

    better way to deal with i18n

--- lib/active_record/type/i18n.rb
+++ lib/active_record/type/i18n.rb
@@ -6,41 +6,11 @@ module ActiveRecord::Type
     end
 
     def deserialize(value)
-      if super.respond_to?(:[])
+      if super && super.respond_to?(:[])
         super[::I18n.locale.to_s]
       else
-        super
+        value
       end
-    end
-
-    def serialize(value, old_value = nil)
-      if old_value
-        begin
-          r = ActiveSupport::JSON.decode(old_value)
-        rescue
-          r = {}
-        end
-      else
-        r = {}
-      end
-
-      begin
-        v = ActiveSupport::JSON.decode(value)
-      rescue
-        v = value
-      end
-
-      binding.pry
-      if v.present? && v.is_a?(Hash)
-        value = v
-      else
-        value = { ::I18n.locale.to_s => v }
-      end
-
-      binding.pry
-      value = r.merge(::I18n.locale.to_s => value)
-
-      super(value)
     end
 
   end
--- lib/rails_com/translation.rb
+++ lib/rails_com/translation.rb
@@ -1,45 +1,46 @@
-module RailsCom::Translation
+module RailsCom::I18n
+  extend ActiveSupport::Concern
 
-  def has_translations(*columns)
+  included do
+    before_save :update_i18n_column
+  end
 
-    columns.each do |column|
-      class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
-
-        def #{column}_i18ns
-          #{column}_before_type_cast
-        end
-
-        def #{column}
-          super[I18n.locale.to_s]
-        end
-
-        def #{column}= value
-          r = method(#{column.inspect}).super_method.call
-          super(r.merge! I18n.locale.to_s => value)
-        end
-
-      RUBY_EVAL
+  def assign_attributes(new_attributes)
+    new_attributes.slice(*i18n_attributes).each do |key, value|
+      new_attributes[key] = { I18n.locale => value }
     end
 
+    super
   end
+
+  def update_i18n_column
+    str = []
+    self.changes.slice(*i18n_attributes).each do |key, value|
+      str << "#{key} = jsonb_set(#{key}, '{#{I18n.locale}}', '\"#{value[1]}\"', true)"
+    end
+    s = str.join(' AND ')
+    self.clear_attribute_changes(i18n_attributes)
+    self.class.connection.execute "UPDATE #{self.class.table_name} SET #{s} WHERE id = #{self.id}"
+  end
+
 
 end
 
-# class ActiveModel::Attribute
-#   class FromUser
-#
-#     # return "{\"en\":\"1\"}"
-#     def type_cast(value)
-#       if type.is_a?(ActiveRecord::Type::I18n) && original_attribute.present?
-#         old_value = self.original_attribute.value_before_type_cast
-#         type.serialize(value, old_value)
-#       else
-#         type.cast(value)
-#       end
-#     end
-#
-#   end
-# end
+module RailsCom::Translation
+
+  # name
+  # * store as jsonb in database;
+  # * read with i18n scope
+  def has_translations(*columns)
+    mattr_accessor :i18n_attributes
+    self.i18n_attributes = columns
+    include RailsCom::I18n
+    columns.each do |column|
+      attribute column, :i18n
+    end
+  end
+
+end
 
 ActiveSupport.on_load :active_record do
   extend RailsCom::Translation
