cc117d77d1a466d181107d6e7e303173dcc6e99f
qinmingyuan <mingyuan0715@foxmail.com>
2018-12-17 16:44:57 +0800

    support data submit

--- app/assets/javascripts/rails_com/footer.js
+++ app/assets/javascripts/rails_com/footer.js
@@ -2,3 +2,8 @@ $('.message .close').on('click', function() {
   $(this).closest('.message').fadeOut();
 });
 $('.ui.toggle.checkbox').checkbox();
+document.querySelectorAll('input[data-submit="true"]').forEach(function (el) {
+  el.addEventListener('change', function () {
+    this.dataset['params'] = this.name + '=' + this.checked;
+  });
+});
