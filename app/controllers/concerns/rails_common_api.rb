518797c185b8d2936bf276cb9613785e5934667f
qinmingyuan <mingyuan0715@foxmail.com>
2018-12-21 15:16:07 +0800

    avoid more render

--- app/controllers/concerns/rails_common_api.rb
+++ app/controllers/concerns/rails_common_api.rb
@@ -4,7 +4,7 @@ module RailsCommonApi
   included do
     rescue_from 'StandardError' do |exp|
       puts nil, exp.full_message(highlight: true, order: :top)
-      render json: { error: { class: exp.class.inspect }, message: exp.message }, status: 500 unless self.respond_body
+      render json: { error: { class: exp.class.inspect }, message: exp.message }, status: 500 unless self.response_body
     end
 
     rescue_from 'ActiveRecord::RecordNotFound' do |exp|
