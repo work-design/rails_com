ce1d31fb56a645f431a733a6f17cc0ec3096a6c8
qinmingyuan <mingyuan0715@foxmail.com>
2019-01-17 16:59:40 +0800

    build insert

--- lib/rails_com/active_record/batch_insert.rb
+++ lib/rails_com/active_record/batch_insert.rb
@@ -3,14 +3,19 @@ module RailsCom::BatchInsert
   # user by  in_batches:
   #  * of
   # Notification.bulk_insert_from_model User, select: { receiver_id: 'id' }, value: { title: 'title', body: 'body' }, default: { }, of: 2
-  def bulk_insert_from_model(model, select:, value: {}, default: { created_at: 'localtimestamp', updated_at: 'localtimestamp' }, **options)
+  def bulk_insert_from_model(model, select: {}, value: {}, default: { created_at: 'localtimestamp', updated_at: 'localtimestamp' }, **options)
+    raise 'select must exists!' if select.blank?
     v = value.values.map { |i| "'#{i}'" }
+    keys = [select.keys, value.keys, default.keys].flatten.join(', ')
+    text = [v, default.values].flatten.join(', ')
 
-    text = "#{v.join(', ')}, #{default.values.join(', ')}"
     enums = model.select(select.values).in_batches(options.slice(:of)).each
     enums.each do |i|
       select_str, where_str = i.to_sql.delete('\"').split(' FROM ')
-      p "INSERT INTO #{table_name} (#{select.keys.join(', ')}, #{value.keys.join(', ')}, #{default.keys.join(', ')}) #{select_str}, #{text} FROM #{where_str}"
+      if text.present?
+        select_str += ", #{text}"
+      end
+      connection.execute("INSERT INTO #{table_name} (#{keys}) #{select_str} FROM #{where_str}")
     end
   end
 
