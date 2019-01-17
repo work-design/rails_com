ec251009cff1d67ec4c21fd623176789f84fb0c9
qinmingyuan <mingyuan0715@foxmail.com>
2019-01-17 16:35:47 +0800

    tested batch insert

--- lib/rails_com/active_record/batch_insert.rb
+++ lib/rails_com/active_record/batch_insert.rb
@@ -3,12 +3,14 @@ module RailsCom::BatchInsert
   # user by  in_batches:
   #  * of
   # Notification.bulk_insert_from_model User, select: { receiver_id: 'id' }, value: { title: 'title', body: 'body' }, default: { }, of: 2
-  def bulk_insert_from_model(model, select:, value: {}, default: { created_at: 'localtimestamp', updated_at: 'localtimestamp' } **options)
+  def bulk_insert_from_model(model, select:, value: {}, default: { created_at: 'localtimestamp', updated_at: 'localtimestamp' }, **options)
     v = value.values.map { |i| "'#{i}'" }
 
-    enums = model.select(Array(select), v, default.values).in_batches(options.slice(:of)).each
+    text = "#{v.join(', ')}, #{default.values.join(', ')}"
+    enums = model.select(select.values).in_batches(options.slice(:of)).each
     enums.each do |i|
-      p "INSERT INTO #{table_name} (#{select.keys.join(', ')}, #{value.keys.join(', ')}, #{default.keys.join(', ')}) #{i.to_sql}"
+      select_str, where_str = i.to_sql.delete('\"').split(' FROM ')
+      p "INSERT INTO #{table_name} (#{select.keys.join(', ')}, #{value.keys.join(', ')}, #{default.keys.join(', ')}) #{select_str}, #{text} FROM #{where_str}"
     end
   end
 
