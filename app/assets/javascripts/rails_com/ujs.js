08bf8b7d3cf8dc2cbd54acec743794c808cc015d
qinmingyuan <mingyuan0715@foxmail.com>
2019-03-26 12:40:41 +0800

    support checkbox with default values

--- app/assets/javascripts/rails_com/ujs.js
+++ app/assets/javascripts/rails_com/ujs.js
@@ -1,6 +1,10 @@
 document.querySelectorAll('input[data-submit="true"]').forEach(function (el) {
   el.addEventListener('change', function () {
-    this.dataset['params'] = this.name + '=' + this.checked;
+    if (this.defaultValue === '') {
+      this.dataset['params'] = this.name + '=' + this.checked;
+    } else if (this.checked) {
+      this.dataset['params'] = this.name + '=' + this.defaultValue;
+    }
   });
 });
 document.querySelectorAll('input[data-form="true"]').forEach(function (el) {
