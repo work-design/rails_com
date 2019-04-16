b0ba2d06b8754c1d08fce69741c84f25976aaba9
qinmingyuan <mingyuan0715@foxmail.com>
2019-04-16 19:26:16 +0800

    remove js support both array or string

--- app/assets/javascripts/rails_com/application.js
+++ app/assets/javascripts/rails_com/application.js
@@ -31,7 +31,11 @@ document.addEventListener('turbolinks:request-start', function(event) {
 });
 
 function remote_js_load(paths) {
-  for (i = 0; i < paths.length; i++) {
-    Rails.ajax({ url: paths[i], type: 'GET', dataType: 'script' })
+  if (Array.isArray(paths)) {
+    for (i = 0; i < paths.length; i++) {
+      Rails.ajax({url: paths[i], type: 'GET', dataType: 'script'})
+    }
+  } else if (typeof(paths) === 'string') {
+    Rails.ajax({url: paths, type: 'GET', dataType: 'script'})
   }
 }
