d83424b32df68ac291dc678d3c1dbc7d1f36c400
qinmingyuan <mingyuan0715@foxmail.com>
2019-04-17 11:57:40 +0800

    fixed combined h use

--- lib/rails_com/active_storage/blob_ext.rb
+++ lib/rails_com/active_storage/blob_ext.rb
@@ -23,9 +23,9 @@ module RailsCom::BlobExt
 
   def private
     return @private if defined?(@private)
-    rts = self.attachments.pluck(:record_type, :name).uniq.to_combined_h
+    rts = self.attachments.pluck(:record_type, :name).uniq.to_array_h.to_combine_h
     ps = Rails.cache.fetch('blob_default/private') do
-      ActiveStorage::BlobDefault.where(private: true).pluck(:record_class, :name).to_combined_h
+      ActiveStorage::BlobDefault.where(private: true).pluck(:record_class, :name).to_array_h.to_combine_h
     end
     @private = ps.slice(*rts.keys).map { |p, v| (Array(rts[p]) - Array(v)).blank? }.uniq == [true]
   end
