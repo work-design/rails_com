21fb4e11c4f92280d67fc98efde795f5625c7dcc
qinmingyuan <mingyuan0715@foxmail.com>
2019-04-17 10:26:31 +0800

    move methods in ac

--- lib/rails_com/core/hash.rb
+++ lib/rails_com/core/hash.rb
@@ -29,40 +29,5 @@ class Hash
     self.merge! other_hash
     self
   end
-
-  def deep_transform_values(&block)
-    _deep_transform_values_in_object(self, &block)
-  end
-
-  def deep_transform_values!(&block)
-    _deep_transform_values_in_object!(self, &block)
-  end
-
-  private
-  def _deep_transform_values_in_object(object, &block)
-    case object
-    when Hash
-      object.each_with_object({}) do |(key, value), result|
-        result[key] = _deep_transform_values_in_object(value, &block)
-      end
-    when Array
-      object.map { |e| _deep_transform_values_in_object(e, &block) }
-    else
-      yield(object)
-    end
-  end
-
-  def _deep_transform_values_in_object!(object, &block)
-    case object
-    when Hash
-      object.each do |key, value|
-        object[key] = _deep_transform_values_in_object!(value, &block)
-      end
-    when Array
-      object.map! { |e| _deep_transform_values_in_object!(e, &block) }
-    else
-      yield(object)
-    end
-  end
-
+  
 end
