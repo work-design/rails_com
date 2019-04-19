45f065b5bbf6f28b94ca2225ac8d65b9dd81088c
qinmingyuan <mingyuan0715@foxmail.com>
2019-04-19 13:14:39 +0800

    support less

--- lib/rails_com/core/date.rb
+++ lib/rails_com/core/date.rb
@@ -1,15 +1,19 @@
 class Date
 
   # Chinese custom after date
-  #   '2018-01-01'.to_date.after(2.month) => '2018-02-31'
-  #   '2018-01-31'.to_date.after(1.month) => '2018-03-02'
-  def contract_duration(*afters)
+  #   '2018-01-01'.to_date.contract_after(2.month) => '2018-02-31'
+  #   '2018-01-31'.to_date.contract_after(1.month) => '2018-02-28'
+  def contract_after(*afters, less: false)
     parts = {}
     # todo use inject or something
     afters.each do |after|
       parts.merge! after.parts
     end
-    ActiveSupport::Duration.new(0, parts).after(self)
+    date = ActiveSupport::Duration.new(0, parts).after(self)
+
+    return date unless less
+
+    day <= date.day ? date - 1 : date
   end
 
   def parts
