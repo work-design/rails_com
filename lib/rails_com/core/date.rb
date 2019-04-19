3a7cd134505b350835e6d029dfafda9599d8b0da
qinmingyuan <mingyuan0715@foxmail.com>
2019-04-19 12:57:00 +0800

    support multi params

--- lib/rails_com/core/date.rb
+++ lib/rails_com/core/date.rb
@@ -3,23 +3,13 @@ class Date
   # Chinese custom after date
   #   '2018-01-01'.to_date.after(2.month) => '2018-02-31'
   #   '2018-01-31'.to_date.after(1.month) => '2018-03-02'
-  def after(other)
-    if ActiveSupport::Duration === other
-      if other.parts.keys == [:months]
-        date = self + other
-        if date.day == self.day
-          r = date - 1.day
-        else
-          r = date + (self.day - date.day - 1).days
-        end
-
-        r
-      else
-        self + other
-      end
-    else
-      self + other
+  def contract_duration(*afters)
+    parts = {}
+    # todo use inject or something
+    afters.each do |after|
+      parts.merge! after.parts
     end
+    ActiveSupport::Duration.new(0, parts).after(self)
   end
 
   def parts
