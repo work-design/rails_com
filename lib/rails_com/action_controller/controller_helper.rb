596efef7add5bbfb855c81de7f80bfca6c23ed5e
qinmingyuan <mingyuan0715@foxmail.com>
2019-04-24 14:46:03 +0800

    add usage

--- lib/rails_com/action_controller/controller_helper.rb
+++ lib/rails_com/action_controller/controller_helper.rb
@@ -18,6 +18,9 @@ module RailsCom::ControllerHelper
 end
 
 module RailsCom::FilterHelper
+  # if whether_filter(:require_login)
+  #   skip_before_action :require_login
+  # end
   def whether_filter(filter)
     self.get_callbacks(:process_action).map(&:filter).include?(filter.to_sym)
   end
