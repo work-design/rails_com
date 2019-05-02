536a3e209a4699eda4a1ad53d76d99d5ea7b808e
qinmingyuan <mingyuan0715@foxmail.com>
2019-05-02 18:47:15 +0800

    add qrcode helper

---
+++ app/models/concerns/qrcode_helper.rb
@@ -0,0 +1,24 @@
+module QrcodeHelper
+  extend self
+
+  def code_file(url)
+    qrcode = RQRCode::QRCode.new(url)
+    png = qrcode.as_png(
+      resize_gte_to: false,
+      resize_exactly_to: false,
+      fill: 'white',
+      color: 'black',
+      size: 300,
+      border_modules: 4,
+      module_px_size: 6,
+      file: nil
+    )
+    tmp = Tempfile.new
+    tmp.binmode
+    tmp.write png.to_s
+    tmp
+  end
+
+end
+
+
