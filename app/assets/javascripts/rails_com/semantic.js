5566ad39f4ee402139135b96c816de8205aa384a
qinmingyuan <mingyuan0715@foxmail.com>
2019-05-25 22:53:45 +0800

    use process

--- app/assets/javascripts/rails_com/semantic.js
+++ app/assets/javascripts/rails_com/semantic.js
@@ -1,2 +1,6 @@
 $('.ui.toggle.checkbox').checkbox();
-$('.ui.progress').progress();
+$('.ui.progress').progress('decrement');
+$('#flash_toast').transition({
+  animation: 'fade right',
+  duration: '3s'
+});
