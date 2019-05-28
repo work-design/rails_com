24e1d98076da1dcb4febe529e825eda2e919dd6d
qinmingyuan <mingyuan0715@foxmail.com>
2019-05-28 19:15:31 +0800

    get controller by name

--- lib/rails_com/meta/controllers.rb
+++ lib/rails_com/meta/controllers.rb
@@ -3,8 +3,14 @@
 module RailsCom::Controllers
   extend self
 
-  def controller controller_path
-    (controller_path + '_controller').classify.constantize
+  def controller(controller_path, action_name)
+    controller_class = (controller_path + '_controller').classify.safe_constantize
+
+    if controller_class
+      controller = controller_class.new
+      controller.action_name = action_name
+      controller
+    end
   end
 
 end
