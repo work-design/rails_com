8529c78dedbf86d2407f3d30e12b383f2828d83b
qinmingyuan <mingyuan0715@foxmail.com>
2019-06-24 22:04:44 +0800

    with assets default

--- app/assets/config/rails_com_manifest.js
+++ app/assets/config/rails_com_manifest.js
@@ -1,4 +1,6 @@
 //= link rails_com/application.js
+//= link phone.js
+//= link phone.css
 
 //= link_tree ../javascripts/controllers .js
 //= link_tree ../stylesheets/controllers .css
---
+++ app/assets/javascripts/phone.js
@@ -0,0 +1,5 @@
+//= require stimulus/dist/stimulus.umd
+//= require weui.js/dist/weui
+//= require default_form/ready
+//= require default_form/valid
+//= require_self
