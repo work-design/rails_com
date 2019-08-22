679d89ecb813dc22f001b074eec3bbac9c093f87
qinmingyuan <mingyuan0715@foxmail.com>
2019-08-22 17:01:05 +0800

    no more repeat packs and assets

--- app/helpers/rails_com/assets_helper.rb
+++ app/helpers/rails_com/assets_helper.rb
@@ -2,32 +2,23 @@
 module RailsCom::AssetsHelper
 
   # Assets path: app/assets/javascripts/controllers
-  # Packs Path: app/javascript/packs/javascripts
   def origin_js_load(**options)
     ext = ['.js', '.js.erb'] + Array(options.delete(:ext))
     suffix = options.delete(:suffix)
 
     asset_filename = "controllers/#{@_rendered_path}"
-    pack_filename = "javascripts/#{@_rendered_path}"
 
     if suffix
       asset_filename = [asset_filename, '-', suffix].join
-      pack_filename = [pack_filename, '-', suffix].join
     end
 
     asset_paths = assets_load_path(asset_filename, relative_path: 'app/assets/javascripts', ext: ext)
-    pack_paths = assets_load_path(pack_filename, relative_path: 'app/javascript/packs', ext: ext)
 
     r = []
     ar = []
     if asset_paths.any? { |path| File.exist?(path) }
-      r << javascript_include_tag(asset_filename, options)
-      ar << javascript_path(asset_filename)
-    end
-
-    if pack_paths.any? { |path| File.exist?(path) }
-      r << javascript_pack_tag(pack_filename, options)
-      ar << asset_pack_path(pack_filename + '.js')
+      r << javascript_pack_tag(asset_filename, options)
+      ar << asset_pack_path(asset_filename)
     end
 
     [r.join("\n    ").html_safe, ar]
@@ -48,40 +39,21 @@ module RailsCom::AssetsHelper
   end
 
   # Assets path: app/assets/stylesheets/controllers
-  # Packs Path: app/javascript/packs/stylesheets
   def css_load(**options)
     ext = ['.css', '.css.erb'] + Array(options.delete(:ext))
     suffix = options.delete(:suffix)
 
     asset_filename = "controllers/#{@_rendered_path}"
-    pack_filename = "stylesheets/#{@_rendered_path}"
 
     if suffix
       asset_filename = [asset_filename, '-', suffix].join
-      pack_filename = [pack_filename, '-', suffix].join
     end
 
     asset_paths = assets_load_path(asset_filename, relative_path: 'app/assets/stylesheets', ext: ext)
-    pack_paths = assets_load_path(pack_filename, relative_path: 'app/javascript/packs', ext: ext)
 
     r = []
     if asset_paths.any? { |path| File.exist?(path) }
       r << stylesheet_link_tag(asset_filename, options)
-    end
-
-    if pack_paths.any? { |path| File.exist?(path) }
-
-      begin
-        rs = stylesheet_pack_tag(pack_filename, options)
-      rescue
-        rs = nil
-      end
-
-      if rs.is_a?(String)
-        r << rs
-      else
-        r << javascript_pack_tag(pack_filename, options)
-      end
     end
 
     r.join("\n    ").html_safe
