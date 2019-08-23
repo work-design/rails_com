ddafec7e1867ef640e9cb11978ea5e48222e345f
qinmingyuan <mingyuan0715@foxmail.com>
2019-08-24 00:03:22 +0800

    add babel loader

--- package/loaders/remote_js_load.js
+++ package/loaders/remote_js_load.js
@@ -2,6 +2,14 @@ module.exports = {
   test: require.resolve('rails_com/package/remote_js_load'),
   use: [
     {
+      loader: 'babel-loader',
+      options: {
+        cacheDirectory: 'tmp/cache/webpacker/babel-loader-node-modules',
+        cacheCompression: false,
+        compact: false
+      }
+    },
+    {
       loader: 'expose-loader',
       options: {
         expose: 'remote_js_loader',
