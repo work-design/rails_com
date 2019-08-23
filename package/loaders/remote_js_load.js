70ba418502d93c5f6f5407f13ae7cf2ec0e7f52d
qinmingyuan <mingyuan0715@foxmail.com>
2019-08-24 00:10:28 +0800

    fix loader oder

--- package/loaders/remote_js_load.js
+++ package/loaders/remote_js_load.js
@@ -2,18 +2,18 @@ module.exports = {
   test: require.resolve('rails_com/package/remote_js_load'),
   use: [
     {
+      loader: 'expose-loader',
+      options: {
+        expose: 'remote_js_loader',
+        exportKey: 'default'
+      }
+    },
+    {
       loader: 'babel-loader',
       options: {
         cacheDirectory: 'tmp/cache/webpacker/babel-loader-node-modules',
         cacheCompression: false,
         compact: false
-      }
-    },
-    {
-      loader: 'expose-loader',
-      options: {
-        expose: 'remote_js_loader',
-        exportKey: 'default'
       }
     }
   ]
