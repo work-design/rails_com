34d8e429ce33db0f55c48b65dc2da8396d5864e4
qinmingyuan <mingyuan0715@foxmail.com>
2019-08-26 16:09:24 +0800

    avoid add repeat content

--- lib/rails_com/webpacker/yaml_helper.rb
+++ lib/rails_com/webpacker/yaml_helper.rb
@@ -1,4 +1,3 @@
-
 # temp ignore config/webpacker changes
 # git update-index --assume-unchanged config/webpacker.yml
 # check it works
@@ -7,36 +6,38 @@
 # git update-index --no-assume-unchanged config/webpacker.yml
 module Webpacker
   class YamlHelper
-    attr_reader :yaml, :io
 
     def initialize(path: 'config/webpacker.yml', export: 'config/webpacker.yml')
       real_path = Rails.root + path
       real_export = Rails.root + export
       
       @yaml = YAML.parse_stream File.read(real_path)
+      @content = @yaml.children[0].children[0].children
+      @parsed = @yaml.to_ruby[0]
       @io = File.new(real_export, 'a+')
     end
     
     def dump
-      io.truncate(0)
-      @yaml.yaml io
-      io.fsync
+      @io.truncate(0)
+      @yaml.yaml @io
+      @io.fsync
+      @io.close
     end
     
     def append(env = 'default', key, value)
-      a = yaml.children[0].children[0].children
-      a_index = a.find_index { |i| i.scalar? && i.value == env }
+      return if Array(@parsed.dig(env, key)).include? value
+      env_index = @content.find_index { |i| i.scalar? && i.value == env }
 
-      b = a[a_index + 1].children
-      b_index = b.find_index { |i| i.scalar? && i.value == key }
+      env_content = @content[env_index + 1].children
+      key_index = env_content.find_index { |i| i.scalar? && i.value == key }
       
-      c = b[b_index + 1]
-      if c.sequence?
-        c.style = 1  # block style
-        c.children << Psych::Nodes::Scalar.new(value)
+      value_content = env_content[key_index + 1]
+      if value_content.sequence?
+        value_content.style = 1  # block style
+        value_content.children << Psych::Nodes::Scalar.new(value)
       end
-      
-      c
+
+      value_content
     end
     
   end
