760c48a3357096112a99c473a2204c3674e1164f
qinmingyuan <mingyuan0715@foxmail.com>
2019-08-27 17:43:52 +0800

    common methods

--- app/assets/javascripts/rails_com/checkbox.js
+++ app/assets/javascripts/rails_com/checkbox.js
@@ -8,23 +8,38 @@ class CheckController extends Controller {
     console.log('Check Controller works!')
   }
 
-  // data-action="check#toggle"
-  toggle(event) {
-    let checkbox = event.target
-    let changed = checkbox.checked === checkbox.defaultChecked
+  doToggle(checkbox, changed) {
     if (changed && checkbox.checked) {
       checkbox.dataset.add_target('check.added')
     } else if (changed && !checkbox.checked) {
       checkbox.dataset.add_target('check.moved')
+    } else {
+      checkbox.dataset.remove_target('check.added')
+      checkbox.dataset.remove_target('check.moved')
     }
   }
 
+  // data-action="check#toggle"
+  toggle(event) {
+    let checkbox = event.target
+    let changed = checkbox.checked !== checkbox.defaultChecked
+    this.doToggle(checkbox, changed)
+  }
+
   addedIds() {
-    this.addedTargets
+    let ids = []
+    this.addedTargets.forEach((item) => {
+      ids.push(item.value)
+    })
+    return ids
   }
 
   movedIds() {
-    this.movedTargets
+    let ids = []
+    this.movedTargets.forEach((item) => {
+      ids.push(item.value)
+    })
+    return ids
   }
 
   // data-action="check#toggleAll"
@@ -32,15 +47,11 @@ class CheckController extends Controller {
   toggleAll(event) {
     let element = event.target
     let checkboxes = document.getElementsByName(this.data.get('name'));
-    
+
     for (let checkbox of checkboxes) {
       checkbox.checked = element.checked
-      let changed = checkbox.checked === checkbox.defaultChecked
-      if (changed && checkbox.checked) {
-        checkbox.dataset.add_target('check.added')
-      } else if (changed && !checkbox.checked) {
-        checkbox.dataset.add_target('check.moved')
-      }
+      let changed = checkbox.checked !== checkbox.defaultChecked
+      this.doToggle(checkbox, changed)
     }
   }
 }
