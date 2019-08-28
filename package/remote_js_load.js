b212e48e4fbd6935abeb7391ef58057ce5092d04
qinmingyuan <mingyuan0715@foxmail.com>
2019-08-28 16:14:24 +0800

    fix remote js load

--- package/remote_js_load.js
+++ package/remote_js_load.js
@@ -2,10 +2,10 @@ import Rails from '@rails/ujs'
 
 export default function remote_js_load(paths) {
   if (Array.isArray(paths)) {
-    for (i = 0; i < paths.length; i++) {
+    for (let i = 0; i < paths.length; i++) {
       Rails.ajax({url: paths[i], type: 'GET', dataType: 'script'})
     }
-  } else if (typeof(paths) === 'string') {
+  } else if (typeof(paths) === 'string' && paths.length > 0) {
     Rails.ajax({url: paths, type: 'GET', dataType: 'script'})
   }
 }
