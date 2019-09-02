e1d7d92e018afa89cb7636fb790cca63116223f7
qinmingyuan <mingyuan0715@foxmail.com>
2019-09-02 09:58:17 +0800

    test run

--- test/controllers/com/admin/cache_lists_controller_test.rb
+++ test/controllers/com/admin/cache_lists_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class Com::Admin::CacheListsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @com_admin_cache_list = create com_admin_cache_lists
+    @cache_list = create com_admin_cache_lists
   end
 
   test 'index ok' do
@@ -17,30 +17,30 @@ class Com::Admin::CacheListsControllerTest < ActionDispatch::IntegrationTest
 
   test 'create ok' do
     assert_difference('CacheList.count') do
-      post admin_cache_lists_url, params: { #{singular_table_name}: { #{attributes_string} } }
+      post admin_cache_lists_url, params: { cache_list: { key: 'xx' } }
     end
 
     assert_redirected_to com_admin_cache_list_url(CacheList.last)
   end
 
   test 'show ok' do
-    get admin_cache_list_url(@com_admin_cache_list)
+    get admin_cache_list_url(@cache_list)
     assert_response :success
   end
 
   test 'edit ok' do
-    get edit_admin_cache_list_url(@com_admin_cache_list)
+    get edit_admin_cache_list_url(@cache_list)
     assert_response :success
   end
 
   test 'update ok' do
-    patch admin_cache_list_url(@com_admin_cache_list), params: { #{singular_table_name}: { #{attributes_string} } }
-    assert_redirected_to com_admin_cache_list_url(@#{singular_table_name})
+    patch admin_cache_list_url(@cache_list), params: { cache_list: { key: 'xx' } }
+    assert_redirected_to com_admin_cache_list_url(@cache_list)
   end
 
   test 'destroy ok' do
     assert_difference('CacheList.count', -1) do
-      delete admin_cache_list_url(@com_admin_cache_list)
+      delete admin_cache_list_url(@cache_list)
     end
 
     assert_redirected_to admin_cache_lists_url
