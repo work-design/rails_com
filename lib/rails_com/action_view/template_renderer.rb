7bb8cc5a85902c72400f65cb432dfb6d4b7828c9
qinmingyuan <mingyuan0715@foxmail.com>
2019-10-17 11:40:06 +0800

    fix lookup context formats

--- lib/rails_com/action_view/template_renderer.rb
+++ lib/rails_com/action_view/template_renderer.rb
@@ -4,8 +4,10 @@ module RailsCom
   module TemplateRenderer
     
     def render(context, options)
+      _formats = [context.request.format.symbol].presence || @lookup_context.formats[0..0]
+      
       # todo better implement
-      @lookup_context.send :_set_detail, :formats, @lookup_context.formats[0..0]
+      @lookup_context.send :_set_detail, :formats, _formats
       context.instance_variable_set(:@_rendered_template, options[:template])
       super
     end
