34478846611e2c74fd011e7ce4a88989af4ab4ca
qinmingyuan <mingyuan0715@foxmail.com>
2019-10-26 20:49:36 +0800

    simple config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
 ENV['RAILS_ENV'] = 'test'
-require_relative '../test/dummy/config/environment'
+require_relative 'dummy/config/environment'
 require 'rails/test_help'
 require 'minitest/mock'
 require 'factory_bot'
 
 ActiveRecord::Migrator.migrations_paths = [
-  File.expand_path('../test/dummy/db/migrate', __dir__)
+  File.expand_path('dummy/db/migrate', __dir__)
 ]
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 FactoryBot.find_definitions
