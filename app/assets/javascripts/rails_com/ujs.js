b6e1a71c7551b3f5b2dee027901643e5a3d51364
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-09 17:30:48 +0800

    style improve

--- app/assets/javascripts/rails_com/ujs.js
+++ app/assets/javascripts/rails_com/ujs.js
@@ -1,22 +1,22 @@
 document.querySelectorAll('input[data-submit="true"], select[data-submit="true"]').forEach(function(el) {
-  el.addEventListener('change', function () {
+  el.addEventListener('change', function() {
     if (this.dataset['params']) {
       this.dataset['params'] += '&'
     } else {
       this.dataset['params'] = ''
     }
-    var str;
+    let str
     if (this.defaultValue === '') {
-      str = this.name + '=' + this.checked;
+      str = this.name + '=' + this.checked
     } else {
-      str = this.name + '=' + this.defaultValue;
+      str = this.name + '=' + this.defaultValue
     }
     this.dataset['params'] += str
-  });
-});
+  })
+})
 
 document.querySelectorAll('input[data-form="true"], select[data-form="true"]').forEach(function(el) {
-  el.addEventListener('change', function () {
+  el.addEventListener('change', function() {
     Rails.fire(this.form, 'submit')
   })
 })
