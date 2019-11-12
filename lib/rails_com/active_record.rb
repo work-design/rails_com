8fe018ce3476877f98f39aa9b8a5644d71fbc6f2
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-12 19:54:57 +0800

    extend

--- lib/rails_com/active_record.rb
+++ lib/rails_com/active_record.rb
@@ -3,7 +3,6 @@
 module RailsCom::ActiveRecord; end
 require 'rails_com/active_record/i18n'
 require 'rails_com/active_record/translation'
-require 'rails_com/active_record/model_helper'
-require 'rails_com/active_record/pg_coder'
 require 'rails_com/active_record/extend'
+require 'rails_com/active_record/pg_coder'
 require 'rails_com/active_record/include'
