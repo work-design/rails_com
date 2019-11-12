d2ac3433b404a5cdc7925229ec044eeed7f01e8a
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-12 18:12:22 +0800

    add module define

--- lib/rails_com/active_record.rb
+++ lib/rails_com/active_record.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+module RailsCom::ActiveRecord; end
 require 'rails_com/active_record/i18n'
 require 'rails_com/active_record/translation'
 require 'rails_com/active_record/model_helper'
