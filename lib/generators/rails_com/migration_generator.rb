921267fc1e91a22b2756a9af5b7fc9d162dd231c
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-27 22:10:03 +0800

    add column works

--- lib/generators/rails_com/migration_generator.rb
+++ lib/generators/rails_com/migration_generator.rb
@@ -3,21 +3,31 @@ require 'rails/generators/active_record'
 
 class RailsCom::MigrationGenerator < ActiveRecord::Generators::Base
   source_root File.expand_path('templates', __dir__)
-  attr_reader :model_name, :model_class
+  attr_reader :model_name, :model_class, :todo_attributes
   
   def create_migration_file
     set_local_assigns!
     check_model_exist?
-    migration_template @migration_template, File.join(db_migrate_path, "#{file_name}.rb")
     binding.pry
+    migration_template @migration_template, File.join(db_migrate_path, "#{file_name}.rb")
   end
   
   private
   def set_local_assigns!
+    @todo_attributes = model_class.new_attributes
+    set_inject_options
+    binding.pry
     if table_not_exist?
       @migration_template = 'create_table_migration.rb'
+    else
+      @migration_template = 'add_migration.rb'
     end
-    @migration_template = 'add_migration.rb'
+  end
+  
+  def set_inject_options
+    @todo_attributes.map! do |attribute|
+      attribute.merge! inject_options: attribute.slice(:limit, :precision, :scale).inject('') { |s, h| s << ", #{h[0]}: #{h[1]}" }
+    end
   end
   
   def table_not_exist?
