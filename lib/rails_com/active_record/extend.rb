71053b5dbd5c28b5d36a8c30b6e3210a1d9a28f6
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-27 14:06:42 +0800

    moved sql table

--- lib/rails_com/active_record/extend.rb
+++ lib/rails_com/active_record/extend.rb
@@ -38,58 +38,6 @@ module RailsCom::ActiveRecord::Extend
     r.create_migration_file
   end
 
-  def sql_table(except: [], only: [], pure: true)
-    if only.size > 0
-      _columns = columns.select { |column| only.include?(column.name) }
-    else
-      _columns = columns.reject { |column| except.include?(column.name) }
-    end
-
-    if pure
-      sql = ""
-    else
-      sql = "CREATE TABLE `#{self.table_name}` (\n"
-    end
-
-    _columns.each do |column|
-      sql << "  `#{column.name}` #{column.sql_type}"
-      sql << " COLLATE #{column.collation}" if column.collation
-      sql << " NOT NULL" unless column.null
-      if column.default
-        sql << " DEFAULT '#{column.default}',\n"
-      elsif column.default.nil? && column.null
-        sql << " DEFAULT NULL,\n"
-      else
-        sql << ",\n"
-      end
-    end
-
-    sql << "  PRIMARY KEY (`#{self.primary_key}`)"
-
-    _indexes = connection.indexes(self.table_name).reject { |index| (index.columns & _columns.map { |col| col.name }).blank? }
-    if _indexes.present?
-      sql << ",\n"
-    else
-      sql << "\n"
-    end
-    _indexes.each_with_index do |obj, index|
-      sql << "  KEY `#{obj.name}` ("
-      sql << obj.columns.map { |col| "`#{col}`" }.join(',')
-
-      if index + 1 == _indexes.size
-        sql << ")\n"
-      else
-        sql << "),\n"
-      end
-    end
-
-    if pure
-      sql
-    else
-      sql << ")"
-    end
-  end
-
 end
 
 ActiveSupport.on_load :active_record do
