18e8ddacfe50ef0ec55a604dcc473242eef165b9
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-27 23:21:49 +0800

    support belogns to

--- lib/generators/rails_com/migration_generator.rb
+++ lib/generators/rails_com/migration_generator.rb
@@ -3,10 +3,11 @@ require 'rails/generators/active_record'
 
 class RailsCom::MigrationGenerator < ActiveRecord::Generators::Base
   source_root File.expand_path('templates', __dir__)
-  attr_reader :model_name, :model_class, :todo_attributes
+  attr_reader :model_name, :model_class, :todo_attributes, :todo_references
   
   def create_migration_file
     check_model_exist?
+    set_belongs_to_reflections!
     set_local_assigns!
     migration_template @migration_template, File.join(db_migrate_path, "#{file_name}.rb")
   end
@@ -20,6 +21,14 @@ class RailsCom::MigrationGenerator < ActiveRecord::Generators::Base
       @migration_template = 'create_table_migration.rb'
     else
       @migration_template = 'add_migration.rb'
+    end
+  end
+  
+  def set_belongs_to_reflections!
+    @todo_references = model_class.reflections.values.select { |reflection| reflection.belongs_to? }.map do |ref|
+      {
+        name: ref.name
+      }
     end
   end
   
