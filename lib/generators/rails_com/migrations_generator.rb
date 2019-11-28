4ad70da642c5287c623a6f895eab6b2720431387
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-28 16:17:53 +0800

    migrations works

--- lib/generators/rails_com/migrations_generator.rb
+++ lib/generators/rails_com/migrations_generator.rb
@@ -2,17 +2,21 @@ require 'rails/generators/active_record/migration'
 
 class RailsCom::MigrationsGenerator < Rails::Generators::Base
   include ActiveRecord::Generators::Migration
+  source_root File.expand_path('templates', __dir__)
   attr_reader :tables
   
   def create_migration_file
     set_local_assigns!
+    file_name = 'rails_com_migration'
     migration_template 'migration.rb', File.join(db_migrate_path, "#{file_name}.rb")
   end
   
   private
   def set_local_assigns!
     Zeitwerk::Loader.eager_load_all
-    @tables = ApplicationRecord.subclasses.map do |record_class|
+    @tables = ApplicationRecord.subclasses
+    @tables.reject! { |k| k.abstract_class? }
+    @tables.map! do |record_class|
       [record_class.table_name, RailsCom::MigrationAttributes.new(record_class).to_hash]
     end
   end
