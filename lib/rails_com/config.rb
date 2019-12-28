aadb7223a4a8ef0f44ae785aae4540379c88f634
qinmingyuan <mingyuan0715@foxmail.com>
2019-12-28 14:14:26 +0800

    moved an unused config

--- lib/rails_com/config.rb
+++ lib/rails_com/config.rb
@@ -8,7 +8,6 @@ module RailsCom #:nodoc:
   configure do |config|
     config.app_controller = 'ApplicationController'
     config.admin_controller = 'AdminController'
-    config.exception_log = true
     config.custom_webpacker = true
     config.github_hmac_key = 'must_change_this'
     config.host = 'localhost:3000'
