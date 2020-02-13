e7c231db79efe64a54a6d4c1eb91f2eea1feac71
qinmingyuan <mingyuan0715@foxmail.com>
2020-02-13 22:44:12 +0800

    weui ext

--- lib/rails_com/all.rb
+++ lib/rails_com/all.rb
@@ -4,18 +4,18 @@
 
 require 'rails'
 
-%w(
-  active_record/railtie
-  active_storage/engine
-  action_controller/railtie
-  action_view/railtie
-  action_mailer/railtie
-  active_job/railtie
-  action_cable/engine
-  action_mailbox/engine
-  action_text/engine
-  rails/test_unit/railtie
-).each do |railtie|
+[
+  'active_record/railtie',
+  'active_storage/engine',
+  'action_controller/railtie',
+  'action_view/railtie',
+  'action_mailer/railtie',
+  'active_job/railtie',
+  'action_cable/engine',
+  'action_mailbox/engine',
+  'action_text/engine',
+  'rails/test_unit/railtie',
+].each do |railtie|
   begin
     require railtie
   rescue LoadError
--- lib/tasks/rails_com_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :rails_com do
-#   # Task goes here
-# end
