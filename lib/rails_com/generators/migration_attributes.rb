8a73855a8aff9353dc3e6d8ca7ea8cec5c1db79e
qinmingyuan <mingyuan0715@foxmail.com>
2020-02-17 15:15:46 +0800

    simple wepacker yml

--- lib/rails_com/generators/migration_attributes.rb
+++ lib/rails_com/generators/migration_attributes.rb
@@ -1,6 +1,7 @@
 class RailsCom::MigrationAttributes
+  attr_accessor :table_exists
   attr_reader :record_class, :new_attributes, :custom_attributes, :new_references
-  
+
   def initialize(record_class)
     @record_class = record_class
     @table_exists = record_class.table_exists?
@@ -10,7 +11,7 @@ class RailsCom::MigrationAttributes
     set_custom_attributes
     set_indexes
   end
-  
+
   def to_hash
     r = instance_values.slice('table_exists', 'timestamps', 'indexes', 'new_references', 'new_attributes', 'custom_attributes')
     r.symbolize_keys!
@@ -19,18 +20,18 @@ class RailsCom::MigrationAttributes
   def table_name
     @table_name = record_class.table_name
   end
-  
+
   def set_new_references
     @new_references = record_class.reflections.values.select { |reflection| reflection.belongs_to? }
     @new_references.reject! { |reflection| record_class.attributes_to_define_after_schema_loads.keys.include?(reflection.foreign_key.to_s) }
-    @new_references.reject! { |reflection| record_class.table_exists? && record_class.column_names.include?(reflection.foreign_key.to_s) }
+    @new_references.reject! { |reflection| @table_exists && record_class.column_names.include?(reflection.foreign_key.to_s) }
     @new_references.map! do |ref|
       r = { name: ref.name }
       r.merge! polymorphic: true if ref.polymorphic?
       r.merge! reference_options: reference_options(r)
     end
   end
-  
+
   def set_new_attributes
     @new_attributes = record_class.new_attributes
     @new_attributes.map! do |attribute|
@@ -40,7 +41,7 @@ class RailsCom::MigrationAttributes
       attribute.merge! attribute_options: attribute_options(attribute)
     end
   end
-  
+
   def set_custom_attributes
     if @table_exists
       @custom_attributes = record_class.custom_attributes
@@ -51,7 +52,7 @@ class RailsCom::MigrationAttributes
       @custom_attributes = []
     end
   end
-  
+
   def set_indexes
     @indexes = record_class.indexes_to_define_after_schema_loads
     @indexes.map! do |index|
@@ -62,11 +63,11 @@ class RailsCom::MigrationAttributes
   def reference_options(reference)
     reference.slice(:polymorphic).inject('') { |s, h| s << ", #{h[0]}: #{h[1].inspect}" }
   end
-  
+
   def attribute_options(attribute)
     attribute.slice(:limit, :precision, :scale, :comment, :default, :null, :index, :array, :size).inject('') { |s, h| s << ", #{h[0]}: #{h[1].inspect}" }
   end
-  
+
   def index_options(index)
     index.slice(:unique, :name).inject('') { |s, h| s << ", #{h[0]}: #{h[1].inspect}" }
   end
