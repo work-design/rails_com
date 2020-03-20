325f77ec6b5690771568d51b24cd6eb4a378b8a2
qinmingyuan <mingyuan0715@foxmail.com>
2020-03-21 01:58:37 +0800

    unify method to wether filter

--- lib/rails_com/action_controller/include.rb
+++ lib/rails_com/action_controller/include.rb
@@ -1,28 +1,28 @@
 module RailsCom::ActionController
   module Include
 
-    def detect_filter(filter)
+    def whether_filter(filter)
       callback = self.__callbacks[:process_action].find { |i| i.filter == filter.to_sym }
       return false unless callback
-  
+
       _if = callback.instance_variable_get(:@if).map do |c|
         c.call(self)
       end
-  
+
       _unless = callback.instance_variable_get(:@unless).map do |c|
         !c.call(self)
       end
-  
+
       !(_if + _unless).uniq.include?(false)
     end
-  
+
     def valid_ivars
       _except = _protected_ivars.to_a + [
         :@marked_for_same_origin_verification
       ]
       self.instance_variables - _except
     end
-    
+
   end
 end
 
