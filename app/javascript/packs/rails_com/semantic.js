fa8a7ee7eea694eff224ae4a9022b95b58968c84
qinmingyuan <mingyuan0715@foxmail.com>
2020-03-29 18:17:13 +0800

    sumit for search form

--- app/javascript/packs/rails_com/semantic.js
+++ app/javascript/packs/rails_com/semantic.js
@@ -1,23 +1,18 @@
+document.querySelectorAll('form[method="get"]').forEach(function(el) {
+  el.addEventListener('submit', function() {
+    for (var i = 0; i < this.elements.length; i++) {
+      if ( this[i].value.length === 0 ) {
+        this[i].disabled = true
+      }
 
-document.querySelectorAll('form[method="get"]').submit(function(e) {
-  for (var i = 0; i < this.elements.length; i++) {
-    if ( this[i].value.length === 0 ) {
-      this[i].disabled = true;
+      if ( this[i].name === 'commit' ) {
+        this[i].disabled = true
+      }
+
+      if ( this[i].name === 'utf8' ) {
+        this[i].disabled = true
+      }
     }
-
-    if ( this[i].name === 'commit' ) {
-      this[i].disabled = true;
-    }
-
-    if ( this[i].name === 'utf8' ) {
-      this[i].disabled = true;
-    }
-  }
-})
-
-document.querySelectorAll('input[data-form="true"], select[data-form="true"]').forEach(function(el) {
-  el.addEventListener('change', function() {
-    Rails.fire(this.form, 'submit')
   })
 })
 
