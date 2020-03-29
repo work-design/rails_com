790292d59fc7211b509e6dd06dc593e69fad67a0
qinmingyuan <mingyuan0715@foxmail.com>
2020-03-29 16:37:30 +0800

    use more simple modal

--- app/javascript/packs/rails_com/semantic.js
+++ app/javascript/packs/rails_com/semantic.js
@@ -1,15 +1,3 @@
-$('.ui.checkbox').checkbox()
-$('.ui.progress').progress({
-  duration: 3000
-})
-$('#flash_toast').transition({
-  animation: 'fade',
-  duration: '3s'
-})
-
-$('.message .close').on('click', function() {
-  $(this).closest('.message').fadeOut();
-})
 
 $('form[method="get"]').submit(function(e) {
   for (var i = 0; i < this.elements.length; i++) {
