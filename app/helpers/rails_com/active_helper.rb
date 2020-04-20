0242bc2ea347ffbed55f9d189c33b1b67194fc0b
qinmingyuan <mingyuan0715@foxmail.com>
2020-04-20 23:53:49 +0800

    active asset is gild the lily

--- app/helpers/rails_com/active_helper.rb
+++ app/helpers/rails_com/active_helper.rb
@@ -1,18 +1,6 @@
 # frozen_string_literal: true
 module RailsCom::ActiveHelper
 
-  #
-  # return by assert return
-  #   active_assert('notice' == 'notice', expected: 'ui info message', unexpected: 'ui negative message')
-  #   #=> 'ui info message'
-  def active_assert(assert, expected: 'item active', unexpected: 'item')
-    if assert
-      expected
-    else
-      unexpected
-    end
-  end
-
   # return value by each keys which is true
   #   active_asserts(active: true, item: false)
   #   #=> 'active'
@@ -37,49 +25,49 @@ module RailsCom::ActiveHelper
   #   active_helper 'work/employee': ['index', 'show']
   # params:
   #   active_helper controller: 'users', action: 'show', id: 371
-  def active_helper(paths: [], controllers: [], modules: [], active_class: 'navbar-item is-active', item_class: 'navbar-item', **options)
+  def active_helper(paths: [], controllers: [], modules: [], active: 'navbar-item is-active', item: 'navbar-item', **options)
     check_parameters = options.delete(:check_parameters)
 
     if paths.present?
       Array(paths).each do |path|
-        return active_class if current_page?(path, check_parameters: check_parameters)
+        return active if current_page?(path, check_parameters: check_parameters)
       end
     end
 
     if controllers.present?
-      return active_class if (Array(controllers) & [controller_name, controller_path]).size > 0
+      return active if (Array(controllers) & [controller_name, controller_path]).size > 0
     end
 
     if modules.present?
       this_modules = controller_path.split('/')
       this_modules.pop
       _this_modules = this_modules.map.with_index { |_, index| this_modules[0, index + 1].join('/') }
-      return active_class if (Array(modules) & _this_modules).size > 0
+      return active if (Array(modules) & _this_modules).size > 0
     end
 
-    return active_class if options.present? && current_page?(options)
+    return active if options.present? && current_page?(options)
 
     options.select { |k, _| [controller_name, controller_path].include?(k.to_s) }.each do |_, value|
-      return active_class if Array(value).include?(action_name)
+      return active if Array(value).include?(action_name)
     end
 
-    item_class
+    item
   end
 
   # return value by params
   #   active_params state: 'xxx', organ_id: 1
-  def active_params(active_class: 'navbar-item is-active', item_class: 'navbar-item', **options)
+  def active_params(active: 'navbar-item is-active', item: 'navbar-item', **options)
     options.compact.each do |k, v|
       if params[k].to_s == v.to_s
-        return active_class
+        return active
       end
 
       if !params.key?(k) && session[k].to_s == v.to_s
-        return active_class
+        return active
       end
     end
 
-    item_class
+    item
   end
 
   def filter_params(options = {})
