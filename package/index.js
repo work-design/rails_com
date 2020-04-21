a62aa0841d53359ab01254f85e3e77eab96772a1
qinmingyuan <mingyuan0715@foxmail.com>
2020-04-21 22:09:31 +0800

    ignore partial scss file

--- package/index.js
+++ package/index.js
@@ -4,12 +4,12 @@ const extname = require('path-complete-extname')
 const config = require('@rails/webpacker/package/config')
 
 const paths = () => {
-  const { extensions } = config
+  const { extensions, ignore } = config
   let glob = extensions.length === 1 ? `**/*${extensions[0]}` : `**/*{${extensions.join(',')}}`
   let result = {}
 
   config.resolved_paths.forEach((rootPath) => {
-    const ab_paths = sync(join(rootPath, glob))
+    const ab_paths = sync(join(rootPath, glob), { ignore: ignore })
 
     ab_paths.forEach((path) => {
       const namespace = relative(join(rootPath), dirname(path))
