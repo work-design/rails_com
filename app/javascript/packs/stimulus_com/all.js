697a4e6efd04d06976aba48c0253404635e7b9d2
qinmingyuan <mingyuan0715@foxmail.com>
2020-05-14 19:36:31 +0800

    sortable js

--- app/javascript/packs/stimulus_com/all.js
+++ app/javascript/packs/stimulus_com/all.js
@@ -4,5 +4,8 @@ import './menu'
 import './picture'
 import './input'
 
+// dependent on sortable
+import './sortable'
+
 // Dependent on Bulma CSS
 import './modal'
---
+++ app/javascript/packs/stimulus_com/sortable.js
@@ -0,0 +1,32 @@
+import { Controller } from 'stimulus'
+import Sortable from 'sortablejs'
+
+class SortableController extends Controller {
+
+  reload(element) {
+    Sortable.create(element, {
+      onEnd: function(evt) {
+        if (evt.oldIndex === evt.newIndex) {
+          return
+        }
+        let url = element.dataset['src'] + evt.item.dataset['id'] + '/reorder'
+        let body = new FormData()
+        this.toArray().forEach(el => {
+          body.append('sort_array[]', el)
+        })
+        body.append('old_index', evt.oldIndex)
+        body.append('new_index', evt.newIndex)
+
+        Rails.ajax({ url: url, type: 'PATCH', dataType: 'script', data: body })
+      }
+    })
+  }
+
+  connect() {
+    console.log('Sortable Controller works!')
+    this.reload(this.element)
+  }
+
+}
+
+application.register('sortable', SortableController)
