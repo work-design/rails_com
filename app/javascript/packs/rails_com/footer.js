1c8604d0f7cc2c1542b7fa9997997b66bd9f72e1
qinmingyuan <mingyuan0715@foxmail.com>
2020-05-20 23:25:08 +0800

    search form with turbolinks;

--- app/javascript/packs/rails_com/footer.js
+++ app/javascript/packs/rails_com/footer.js
@@ -1,16 +1,15 @@
-document.querySelectorAll('form[method="get"]').forEach(function(el) {
-  el.addEventListener('submit', function() {
-    for (let i = 0; i < this.elements.length; i++) {
-      if ( this[i].value.length === 0 ) {
-        this[i].disabled = true
-      }
+document.querySelectorAll('form[method="get"]').forEach(el => {
+  el.addEventListener('submit', event => {
+    event.preventDefault()
+    let url = new URL(location)
+    let form = new FormData(event.target)
 
-      if ( this[i].name === 'commit' ) {
-        this[i].disabled = true
+    for (let el of form.entries()) {
+      if (el[1].length > 0) {
+        url.searchParams.set(el[0], el[1])
       }
     }
+
+    Turbolinks.visit(url.href)
   })
 })
-
-
-
