7f18a195abc5857a72e033d34e95d1bccd7ff5f9
qinmingyuan <mingyuan0715@foxmail.com>
2020-05-21 19:26:20 +0800

    use common rails com

---
+++ app/javascript/packs/rails_com/application.js
@@ -0,0 +1,42 @@
+import Rails from '@rails/ujs'
+import Turbolinks from 'qinmingyuan_turbolinks'
+import 'rails_com/package/remote_js_load'
+
+Rails.start()
+Turbolinks.start()
+
+const timeForLocalized = () => {
+  document.querySelectorAll('time:not([data-localized="true"])').forEach(el => {
+    if (el.textContent.length > 0) {
+      let format = el.dataset['format'] || 'YYYY-MM-DD HH:mm'
+      el.textContent = moment.utc(el.textContent).local().format(format)
+      el.dataset['localized'] = 'true'
+    }
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  timeForLocalized()
+})
+document.addEventListener('turbolinks:load', function() {
+  timeForLocalized()
+})
+document.addEventListener('turbolinks:visit', function() {
+  timeForLocalized()
+})
+document.addEventListener('ajax:success', function() {
+  timeForLocalized()
+})
+
+document.addEventListener('ajax:beforeSend', function(event) {
+  let xhr = event.detail[0]
+  let offset = (new Date).getTimezoneOffset()
+  xhr.setRequestHeader('Utc-Offset', offset)
+  xhr.setRequestHeader('X-Csp-Nonce', Rails.cspNonce())
+})
+document.addEventListener('turbolinks:request-start', function(event) {
+  let xhr = event.data.xhr
+  let offset = (new Date).getTimezoneOffset()
+  xhr.setRequestHeader('Utc-Offset', offset)
+  xhr.setRequestHeader('X-Csp-Nonce', Rails.cspNonce())
+})
