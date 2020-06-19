b92071f71ec65ca542721603622b41aac5565c2f
qinmingyuan <mingyuan0715@foxmail.com>
2020-06-19 19:38:57 +0800

    support remove

--- app/javascript/packs/stimulus_com/choice.js
+++ app/javascript/packs/stimulus_com/choice.js
@@ -5,7 +5,9 @@ class ChoiceController extends Controller {
 
   reload(element) {
     new Choices(element, {
-      noChoicesText: '无可选项'
+      noChoicesText: '无可选项',
+      itemSelectText: '点击选择',
+      removeItemButton: true
     })
   }
 
