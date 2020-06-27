b21ae2edac5fc6b3b09f078a5bf7ec181ab78491
qinmingyuan <mingyuan0715@foxmail.com>
2020-06-27 18:30:32 +0800

    slide support high speed

--- app/javascript/packs/stimulus_com/slide.js
+++ app/javascript/packs/stimulus_com/slide.js
@@ -23,6 +23,7 @@ class SlideController extends Controller {
       x: touch.pageX,
       y: touch.pageY
     }
+    this.startTime = new Date().getTime() // 毫秒，千分之一秒
   }
 
   // data-action="touchmove->slide#move touchstart->slide#start"
@@ -61,31 +62,45 @@ class SlideController extends Controller {
     if (event.changedTouches.length > 1 || event.scale && event.scale !== 1) {
       return
     }
+    let endTime = new Date().getTime()
     let offset = this.offset(event.changedTouches[0])
     let pad = Math.abs(offset.x)
     let isScrolling = pad < Math.abs(offset.y) ? 1 : 0  // 1 上下滚动，0 左右滑动
     if (isScrolling !== 0) {
       return
     }
-    
+
     let isMore = pad > this.element.clientWidth / 2 ? 1 : 0  // 滑动距离是否超过元素宽度一半
-    let next = ele.nextElementSibling
-    if (next && isMore) {
-      ele.style.right = this.element.clientWidth + 'px'
-      ele.style.transitionProperty = 'right'
-      ele.style.transitionDuration = '1s'
-      next.style.left = 0
-      next.style.transitionProperty = 'left'
-      next.style.transitionDuration = '1s'
+    let speed = pad / (endTime - this.startTime)  // 手势速度
+    console.log('手势速度', speed)  // 大于 0.1
+
+    if (isMore || speed > 0.1) {
+      if (offset.x < 0) {
+        let next = ele.nextElementSibling
+        if (next) {
+          next.style.left = 0
+          next.style.transitionProperty = 'left'
+          next.style.transitionDuration = '1s'
+        }
+        ele.style.right = this.element.clientWidth + 'px'
+        ele.style.transitionProperty = 'right'
+        ele.style.transitionDuration = '1s'
+      } else if (offset.x > 0) {
+        let prev = ele.previousElementSibling
+        if (prev) {
+          prev.style.right = 0
+          prev.style.transitionProperty = 'right'
+          prev.style.transitionDuration = '1s'
+        }
+        ele.style.left = this.element.clientWidth + 'px'
+        ele.style.transitionProperty = 'left'
+        ele.style.transitionDuration = '1s'
+      }
     } else if (isMore === 0) {
       ele.style.right = 0
       ele.style.transitionProperty = 'right'
       ele.style.transitionDuration = '1s'
     }
-  }
-
-  get index() {
-    this.data.get('index')
   }
 
   get startPos() {
@@ -98,6 +113,14 @@ class SlideController extends Controller {
 
   set startPos(pos) {
     this.data.set('startPos', [pos.x, pos.y].join(','))
+  }
+
+  get startTime() {
+    return this.data.get('startTime')
+  }
+
+  set startTime(time) {
+    this.data.set('startTime', time)
   }
 
 }
