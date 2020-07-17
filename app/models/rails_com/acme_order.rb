90f340d218714ce978e1e61b92c2fb70b354b44b
qinmingyuan <mingyuan0715@foxmail.com>
2020-07-17 16:40:25 +0800

    request cert

--- app/models/rails_com/acme_order.rb
+++ app/models/rails_com/acme_order.rb
@@ -1,3 +1,4 @@
+require 'acme-client'
 module RailsCom::AcmeOrder
   extend ActiveSupport::Concern
 
@@ -7,8 +8,12 @@ module RailsCom::AcmeOrder
     attribute :file_content, :string
     attribute :record_name, :string
     attribute :record_content, :string
+    attribute :domain, :string
 
     belongs_to :acme_account
+
+    has_one_attached :private_pem
+    has_one_attached :cert_key
   end
 
   def order
@@ -25,14 +30,54 @@ module RailsCom::AcmeOrder
     [identifier]
   end
 
-  def http_challenge
+  def csr
+    return @csr if defined? @csr
+    @csr = Acme::Client::CertificateRequest.new(subject: { common_name: identifier })
+    Tempfile.open do |file|
+      file.binmode
+      file.write @csr.private_key.to_pem
+      file.rewind
+      self.private_pem.attach io: file, filename: "#{identifier}.pem"
+    end
+    @csr
+  end
 
+  def cert
+    order.finalize(csr: csr)
+    while order.status == 'processing'
+      sleep(1)
+      order.reload
+    end
+    r = order.certificate
+    Tempfile.open do |file|
+      file.binmode
+      file.write r
+      file.rewind
+      self.cert_key.attach io: file, filename: "#{identifier}.key"
+    end
+    r
+  end
+
+  def dns_resolv
+    Resolv::DNS.open do |dns|
+      records = dns.getresources dns_host, Resolv::DNS::Resource::IN::TXT
+      records.empty? ? nil : records.map(&:data).join(' ')
+    end
+  end
+
+  def dns_valid?
+    dns_resolv == record_content && authorization.dns.request_validation
+  end
+
+  def dns_host
+    "#{record_name}.#{domain}"
   end
 
   def dns_challenge
     dns = authorization.dns
     self.record_name = dns.record_name
     self.record_content = dns.record_content
+    self.domain = authorization.domain
     self.save
     self
   end
