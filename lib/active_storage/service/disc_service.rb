bf68f42ed37ca3ec96c65449d4d42ade7c7b9a62
qinmingyuan <mingyuan0715@foxmail.com>
2020-07-22 17:11:33 +0800

    also copy with filename path

--- lib/active_storage/service/disc_service.rb
+++ lib/active_storage/service/disc_service.rb
@@ -3,7 +3,19 @@ module ActiveStorage
   class Service
     class DiscService < DiskService
 
-      def path_for(key)
+      def upload(key, io, checksum: nil, **metadata)
+        path = metadata[:filename].to_s
+        IO.copy_stream(io, make_filename_path_for(path)) if path.present?
+        io.rewind
+
+        super
+      end
+
+      def make_filename_path_for(key)
+        filename_path_for(key).tap { |path| FileUtils.mkdir_p File.dirname(path) }
+      end
+
+      def filename_path_for(key)
         File.join root, key
       end
 
