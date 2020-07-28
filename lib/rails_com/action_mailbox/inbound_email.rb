3e2118bd791d489c5394b43a6f234d4d506f74e7
qinmingyuan <mingyuan0715@foxmail.com>
2020-07-28 11:02:41 +0800

    attribute use prepend

--- lib/rails_com/action_mailbox/inbound_email.rb
+++ lib/rails_com/action_mailbox/inbound_email.rb
@@ -1,8 +1,8 @@
 module RailsCom::ActionMailbox::InboundEmail
   extend ActiveSupport::Concern
-  
+
   included do
-    attribute :status, :integer, default: 0, null: false
+    attribute :status, :integer, default: 0
     attribute :message_id, :string, null: false
     attribute :message_checksum, :string, null: false
     index [ :message_id, :message_checksum ], name: 'index_action_mailbox_inbound_emails_uniqueness', unique: true
@@ -11,5 +11,5 @@ module RailsCom::ActionMailbox::InboundEmail
 end
 
 ActiveSupport.on_load :action_mailbox_inbound_email do
-  include RailsCom::ActionMailbox::InboundEmail
+  prepend RailsCom::ActionMailbox::InboundEmail  # attribute 定义会覆盖掉 enum
 end
