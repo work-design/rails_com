32c4069df8167401386338750de3d2c232b64a8a
qinmingyuan <mingyuan0715@foxmail.com>
2020-07-28 14:47:06 +0800

    no attributes for enum column

--- lib/rails_com/active_storage.rb
+++ lib/rails_com/active_storage.rb
@@ -4,7 +4,8 @@ require 'rails_com/active_storage/activestorage_attached'
 require 'rails_com/active_storage/attached_macros'
 require 'rails_com/active_storage/attachment_transfer'
 require 'rails_com/active_storage/attachment_prepend'
-require 'rails_com/active_storage/video_response'
 require 'rails_com/active_storage/blob_prepend'
+require 'rails_com/active_storage/variant_include'
+require 'rails_com/active_storage/video_response'
 
 ActiveStorage.mattr_accessor :service_urls_expire_in, default: 5.minutes
---
+++ lib/rails_com/active_storage/variant_include.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module RailsCom::VariantInclude
+  extend ActiveSupport::Concern
+
+  included do
+    attribute :variation_digest, :string, null: false
+  end
+
+end
+
+ActiveSupport.on_load(:active_storage_blob) do
+  ActiveStorage::VariantRecord.include RailsCom::VariantInclude
+end
