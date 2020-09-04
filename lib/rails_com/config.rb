c9484f32f7fca6356dd547b349740a4176703ec9
MingyuanQin <mingyuan0715@foxmail.com>
2020-09-04 16:50:26 +0800

    acme log

--- lib/rails_com/config.rb
+++ lib/rails_com/config.rb
@@ -11,7 +11,7 @@ module RailsCom #:nodoc:
     config.default_error_message = '服务端发生错误'
     config.host = 'localhost:3000'
     config.subdomain = nil
-    config.acme_url = 'https://acme-staging-v02.api.letsencrypt.org/directory'
+    config.acme_url = 'https://acme-v02.api.letsencrypt.org/directory'
     config.enum_key = ->(o, attribute){ "#{o.i18n_scope}.enum.#{o.base_class.model_name.i18n_key}.#{attribute}" }
     config.help_key = ->(o, attribute){ "#{o.i18n_scope}.help.#{o.base_class.model_name.i18n_key}.#{attribute}" }
   end
