b486bf92bd82d688adfda6b1401a166e43f77082
Quo yi <beitaz@icloud.com>
2020-09-09 09:42:31 +0800

    修复未登录可直接访问 panel 错误

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -1,5 +1,6 @@
 unless defined? PanelController
   class PanelController < ApplicationController
+  	before_action :require_login if defined? RailsAuth
     after_action :set_flash, only: %i[update create destroy]
   end
 end
