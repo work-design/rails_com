1f74cfec8ec9d54cbce473f100562060ad566132
qinmingyuan <mingyuan0715@foxmail.com>
2020-09-18 17:32:09 +0800

    extra form

---
+++ app/models/rails_com_ext/extra.rb
@@ -0,0 +1,18 @@
+require 'rails_com/utils/setting'
+
+module RailsComExt::Extra
+  extend ActiveSupport::Concern
+
+  included do
+    attribute :parameters, :json, default: {}
+  end
+
+  def form_parameters
+    r = Hash(parameters).map { |k, v| { key: k, value: v } }
+    if r.blank?
+      r = [{ key: nil, value: nil }]
+    end
+    RailsCom::Settings.new(r)
+  end
+
+end
