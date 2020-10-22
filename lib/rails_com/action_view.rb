74336f16511e9bbd146fcf7bb5a24e938a9955de
qinmingyuan <mingyuan0715@foxmail.com>
2020-10-22 11:17:35 +0800

    Revert "no more support action under"
    This reverts commit f8a2e2b19b57c3d669730a4c4faa0aa014f79c48.

--- lib/rails_com/action_view.rb
+++ lib/rails_com/action_view.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
-
+#
+require 'rails_com/action_view/partial_renderer'
 require 'rails_com/action_view/template_renderer'
 require 'rails_com/action_view/translation_helper'
--- lib/rails_com/action_view/template_renderer.rb
+++ lib/rails_com/action_view/template_renderer.rb
@@ -14,6 +14,16 @@ module RailsCom
 
       # todo better implement
       @lookup_context.send :_set_detail, :formats, _formats
+
+      # 支持在views/:controller 目录下，用 _action 开头的子目录进一步分组，会优先查找该目录下文件
+      context_prefix = @lookup_context.prefixes[0]
+      action = (request&.params || {})['action']
+      if context_prefix && context_prefix.split('/')[-1].start_with?('_') && action
+        @lookup_context.prefixes[0] = [@lookup_context.prefixes[1], "_#{action}"].join('/')
+      elsif context_prefix && action
+        @lookup_context.prefixes.prepend [context_prefix, "_#{action}"].join('/')
+      end
+
       context.instance_variable_set(:@_rendered_template, options[:template])
       super
     end
