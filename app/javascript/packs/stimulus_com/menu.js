241d11fe8dad6b1e3d9a5f32d6040e93d828c96b
lewislinlin <lewislinlin@gmail.com>
2020-11-05 17:32:26 +0800

    fix menu.js for menu-list

--- app/javascript/packs/stimulus_com/menu.js
+++ app/javascript/packs/stimulus_com/menu.js
@@ -3,12 +3,36 @@ import { Controller } from 'stimulus'
 // data-controller="menu"
 class MenuController extends Controller {
 
+  static values = {
+    openClass: String,
+    closeClass: String,
+  }
+
   connect() {
     console.debug('Menu Controller works!')
   }
 
   toggle() {
     this.element.classList.toggle('is-active')
+  }
+
+  expand(event) {
+    let expander = event.currentTarget
+    if (expander.nextElementSibling !== null) {
+      expander.nextElementSibling.classList.toggle('is-hidden')
+    }
+    if (expander.lastElementChild !== null) {
+      expander.lastElementChild.classList.toggle(this.closeClass)
+      expander.lastElementChild.classList.toggle(this.openClass)
+    }
+  }
+
+  get closeClass () {
+    return this.openClassValue || 'fa-angle-left'
+  }
+
+  get openClass () {
+    return this.closeClassValue || 'fa-angle-down'
   }
 
 }
