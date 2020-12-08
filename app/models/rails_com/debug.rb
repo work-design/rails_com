ac3d65a7fa79f603386f94b5af562551d257818f
qinmingyuan <mingyuan0715@foxmail.com>
2020-12-08 23:35:34 +0800

    add more debug info

--- app/models/rails_com/debug.rb
+++ app/models/rails_com/debug.rb
@@ -19,51 +19,51 @@ module RailsCom::Debug
   end
 
   def debug_before_validation
-    logger.debug "  \e[35m~~~~~~~~~~ before_validation ~~~~~~~~~~\e[0m"
+    logger.debug "  \e[35m~~~~~~~~~~ #{self.class.name}/#{self.object_id} before_validation ~~~~~~~~~~\e[0m"
   end
 
   def debug_after_validation
-    logger.debug "  \e[35m~~~~~~~~~~ after_validation ~~~~~~~~~~\e[0m"
+    logger.debug "  \e[35m~~~~~~~~~~ #{self.class.name}/#{self.object_id} after_validation ~~~~~~~~~~\e[0m"
   end
 
   def debug_before_save
-    logger.debug "  \e[35m~~~~~~~~~~ before_save ~~~~~~~~~~\e[0m"
+    logger.debug "  \e[35m~~~~~~~~~~ #{self.class.name}/#{self.object_id} before_save ~~~~~~~~~~\e[0m"
   end
 
   def debug_around_save
-    logger.debug "  \e[35m~~~~~~~~~~ around_save ~~~~~~~~~~\e[0m"
+    logger.debug "  \e[35m~~~~~~~~~~ #{self.class.name}/#{self.object_id} around_save ~~~~~~~~~~\e[0m"
   end
 
   def debug_before_create
-    logger.debug "  \e[35m~~~~~~~~~~ before_create ~~~~~~~~~~\e[0m"
+    logger.debug "  \e[35m~~~~~~~~~~ #{self.class.name}/#{self.object_id} before_create ~~~~~~~~~~\e[0m"
   end
 
   def debug_before_update
-    logger.debug "  \e[35m~~~~~~~~~~ before_update ~~~~~~~~~~\e[0m"
+    logger.debug "  \e[35m~~~~~~~~~~ #{self.class.name}/#{self.object_id} before_update ~~~~~~~~~~\e[0m"
   end
 
   def debug_around_create
-    logger.debug "  \e[35m~~~~~~~~~~ around_create ~~~~~~~~~~\e[0m"
+    logger.debug "  \e[35m~~~~~~~~~~ #{self.class.name}/#{self.object_id} around_create ~~~~~~~~~~\e[0m"
   end
 
   def debug_around_update
-    logger.debug "  \e[35m~~~~~~~~~~ around_update ~~~~~~~~~~\e[0m"
+    logger.debug "  \e[35m~~~~~~~~~~ #{self.class.name}/#{self.object_id} around_update ~~~~~~~~~~\e[0m"
   end
 
   def debug_after_create
-    logger.debug "  \e[35m~~~~~~~~~~ after_create ~~~~~~~~~~\e[0m"
+    logger.debug "  \e[35m~~~~~~~~~~ #{self.class.name}/#{self.object_id} after_create ~~~~~~~~~~\e[0m"
   end
 
   def debug_after_update
-    logger.debug "  \e[35m~~~~~~~~~~ after_update ~~~~~~~~~~\e[0m"
+    logger.debug "  \e[35m~~~~~~~~~~ #{self.class.name}/#{self.object_id} after_update ~~~~~~~~~~\e[0m"
   end
 
   def debug_after_save
-    logger.debug "  \e[35m~~~~~~~~~~ after_save ~~~~~~~~~~\e[0m"
+    logger.debug "  \e[35m~~~~~~~~~~ #{self.class.name}/#{self.object_id} after_save ~~~~~~~~~~\e[0m"
   end
 
   def debug_after_commit
-    logger.debug "  \e[35m~~~~~~~~~~ after_commit ~~~~~~~~~~\e[0m"
+    logger.debug "  \e[35m~~~~~~~~~~ #{self.class.name}/#{self.object_id} after_commit ~~~~~~~~~~\e[0m"
   end
 
 end
