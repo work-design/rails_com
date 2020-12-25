389a9b726212c9f5266c386cf43c0699e84eb6b2
qinmingyuan <mingyuan0715@foxmail.com>
2020-12-25 19:33:17 +0800

    upgrade webpacker

--- package/index.js
+++ package/index.js
@@ -1,15 +1,14 @@
 const { basename, dirname, join, relative, resolve } = require('path')
 const { sync } = require('glob')
 const extname = require('path-complete-extname')
-const config = require('@rails/webpacker/package/config')
+const { config } = require('@rails/webpacker')
 
 const paths = () => {
-  const { extensions, ignore } = config
-  let glob = extensions.length === 1 ? `**/*${extensions[0]}` : `**/*{${extensions.join(',')}}`
+  let glob = config.extensions.length === 1 ? `**/*${config.extensions[0]}` : `**/*{${config.extensions.join(',')}}`
   let result = {}
 
   config.additional_paths.forEach((rootPath) => {
-    const ab_paths = sync(join(rootPath, glob), { ignore: ignore })
+    const ab_paths = sync(join(rootPath, glob), { ignore: config.ignore })
 
     ab_paths.forEach((path) => {
       const namespace = relative(join(rootPath), dirname(path))
