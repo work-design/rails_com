68bca9b7437cb81a133d34d063374dd52cdf3623
qinmingyuan <mingyuan0715@foxmail.com>
2020-12-25 20:32:34 +0800

    use new api

--- package/index.js
+++ package/index.js
@@ -1,16 +1,13 @@
 const { basename, dirname, join, relative, resolve } = require('path')
-const { sync } = require('glob')
+const { sync: globSync } = require('glob')
 const extname = require('path-complete-extname')
 const { config } = require('@rails/webpacker')
 
 const paths = () => {
-  let glob = config.extensions.length === 1 ? `**/*${config.extensions[0]}` : `**/*{${config.extensions.join(',')}}`
   let result = {}
 
   config.additional_paths.forEach((rootPath) => {
-    const ab_paths = sync(join(rootPath, glob), { ignore: config.ignore })
-
-    ab_paths.forEach((path) => {
+    globSync(`${rootPath}/**/*.*`, { ignore: config.ignore }).forEach((path) => {
       const namespace = relative(join(rootPath), dirname(path))
       const name = join(namespace, basename(path, extname(path)))
       result[name] = resolve(path)
