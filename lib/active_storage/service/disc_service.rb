6e6d117f39949f7cdefdb02820d6e57ae32c2ba1
qinmingyuan <mingyuan0715@foxmail.com>
2021-02-18 22:22:46 +0800

    download by filename

--- lib/active_storage/service/disc_service.rb
+++ lib/active_storage/service/disc_service.rb
@@ -12,6 +12,22 @@ module ActiveStorage
         end
       end
 
+      def download(key, &block)
+        path = ActiveStorage::Blob.find_by(key: key)&.filename.to_s
+
+        if block_given?
+          instrument :streaming_download, key: key do
+            stream path, &block
+          end
+        else
+          instrument :download, key: key do
+            File.binread path_for(path)
+          rescue Errno::ENOENT
+            raise ActiveStorage::FileNotFoundError
+          end
+        end
+      end
+
       def make_path_for(path)
         path_for(path).tap { |p| FileUtils.mkdir_p File.dirname(p) }
       end
