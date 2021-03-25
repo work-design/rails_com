3c0c5999fa663e2e96303d3ff2c45a7fbf8f2a16
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-25 10:07:03 +0800

    modal

---
+++ app/helpers/rails_com/frame_helper.rb
@@ -0,0 +1,13 @@
+module RailsCom
+  module FrameHelper
+
+    def turbo_frame_tagged(id, src: nil, target: nil, **attributes, &block)
+      if request.headers['Turbo-Frame'].blank? && block_given?
+        yield block
+      else
+        turbo_frame_tag(id, src: src, target: target, **attributes, &block)
+      end
+    end
+
+  end
+end
