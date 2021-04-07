1bc1b415b1bd2e3143fd38ff53c7eb7790a6351a
qinmingyuan <mingyuan0715@foxmail.com>
2021-04-07 12:30:45 +0800

    migration

--- lib/generators/rails_com/migration_generator.rb
+++ lib/generators/rails_com/migration_generator.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
+# bin/rails g rails_com:migration User
 require 'rails/generators/active_record'
 
 class RailsCom::MigrationGenerator < ActiveRecord::Generators::Base
   source_root File.expand_path('templates', __dir__)
   attr_reader :tables, :record_class
-  
+
   def create_migration_file
     check_model_exist?
     set_local_assigns!
     migration_template 'migration.rb', File.join(db_migrate_path, "#{file_name}.rb")
   end
-  
+
   private
   def set_local_assigns!
     unless record_class.table_exists?
@@ -20,7 +21,7 @@ class RailsCom::MigrationGenerator < ActiveRecord::Generators::Base
       record_class.table_name => RailsCom::MigrationAttributes.new(record_class).to_hash
     }
   end
-  
+
   def check_model_exist?
     @record_class = file_name.classify.safe_constantize
     unless record_class
@@ -30,5 +31,5 @@ class RailsCom::MigrationGenerator < ActiveRecord::Generators::Base
       abort "#{record_class.name} is not an ActiveRecord Object!"
     end
   end
-  
+
 end
--- lib/generators/rails_com/migrations_generator.rb
+++ lib/generators/rails_com/migrations_generator.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
+# bin/rails g rails_com:migrations
 require 'rails/generators/active_record/migration'
 
 class RailsCom::MigrationsGenerator < Rails::Generators::Base
   include ActiveRecord::Generators::Migration
   source_root File.expand_path('templates', __dir__)
   attr_reader :tables
-  
+
   def create_migration_file
     set_local_assigns!
     file_name = 'rails_com_migration'
     migration_template 'migration.rb', File.join(db_migrate_path, "#{file_name}.rb")
   end
-  
+
   private
   def set_local_assigns!
     Zeitwerk::Loader.eager_load_all
