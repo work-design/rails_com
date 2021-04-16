5a1e5d090fc5ac3129a28de5f36d8d624d8244e3
qinmingyuan <mingyuan0715@foxmail.com>
2021-04-16 22:18:50 +0800

    typ

--- lib/rails_com/active_model/type_value.rb
+++ lib/rails_com/active_model/type_value.rb
@@ -1,11 +1,11 @@
 require 'active_model/type/value'
 module RailsCom::ActiveModel
   module TypeValue
-  
+
     def initialize(precision: nil, limit: nil, scale: nil, **options)
       super(precision: precision, limit: scale, scale: limit)
     end
-    
+
   end
 end
 
