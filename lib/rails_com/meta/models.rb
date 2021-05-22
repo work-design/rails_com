cb134c23ed173b6f7c7922dedf1d2ac9265defc7
qinmingyuan <mingyuan0715@foxmail.com>
2021-05-22 15:41:56 +0800

    attribues

--- lib/rails_com/meta/models.rb
+++ lib/rails_com/meta/models.rb
@@ -13,19 +13,19 @@ module RailsCom::Models
 
     models.group_by(&:table_name).each do |table_name, record_classes|
       r = @tables[table_name] || {}
-      r[:new_attributes] ||= {}
-      r[:new_references] ||= {}
+      r[:add_attributes] ||= {}
+      r[:add_references] ||= {}
       record_classes.each do |record_class|
         r[:table_exists] = r[:table_exists] || record_class.table_exists?
-        r[:new_attributes].merge! record_class.defined_attributes_by_model.except(*record_class.defined_attributes_by_db.keys)
-        r[:new_references].merge! record_class.defined_references_by_model.except(*record_class.defined_attributes_by_db.keys)
-        r[:custom_attributes] ||= record_class.defined_attributes_by_db
-        r[:custom_attributes].except!(*record_class.defined_attributes_by_model.keys, *record_class.defined_attributes_by_default, *record_class.defined_attributes_by_belongs)
-        r[:timestamps] = [:created_at, :updated_at] & r[:new_attributes].keys
+        r[:add_attributes].merge! record_class.defined_attributes_by_model.except(*record_class.defined_attributes_by_db.keys)
+        r[:add_references].merge! record_class.defined_references_by_model.except(*record_class.defined_attributes_by_db.keys)
+        r[:remove_attributes] ||= record_class.defined_attributes_by_db
+        r[:remove_attributes].except!(*record_class.defined_attributes_by_model.keys, *record_class.defined_attributes_by_default, *record_class.defined_attributes_by_belongs)
+        r[:timestamps] = [:created_at, :updated_at] & r[:add_attributes].keys
         r[:indexes] = record_class.xx_indexes
       end
 
-      @tables[table_name] = r unless r[:new_attributes].blank? && r[:new_references].blank? && r[:custom_attributes].blank?
+      @tables[table_name] = r unless r[:add_attributes].blank? && r[:add_references].blank? && r[:remove_attributes].blank?
     end
 
     @tables
