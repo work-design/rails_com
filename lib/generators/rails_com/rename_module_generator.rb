43d271cf0440f80754f70fb0b0ac2be185be61fc
MingyuanQin <mingyuan0715@foxmail.com>
2021-05-22 19:34:10 +0800

    generate migration

--- lib/generators/rails_com/rename_module_generator.rb
+++ lib/generators/rails_com/rename_module_generator.rb
@@ -1,24 +1,16 @@
 # frozen_string_literal: true
 # bin/rails g rails_com:rename_module new old
-require 'rails/generators/active_record'
+require 'rails/generators/active_record/migration'
 
-class RailsCom::RenameModuleGenerator < ActiveRecord::Generators::Base
+class RailsCom::RenameModuleGenerator < Rails::Generators::Base
+  include ActiveRecord::Generators::Migration
   source_root File.expand_path('templates', __dir__)
-  attr_reader :tables, :record_class
+  attr_reader :tables
 
   def create_migration_file
-    binding.pry
-    check_model_exist?
-    set_local_assigns!
-    migration_template 'change_module.rb', File.join(db_migrate_path, "#{file_name}.rb")
-  end
-
-  private
-  def set_local_assigns!
-    unless record_class.table_exists?
-      @file_name = "create_#{file_name}"
-    end
-    @tables = RailsCom::Models.tables_hash.slice(record_class.table_name)
+    @tables = RailsCom::Models.modules_hash
+    file_name = 'rails_com_rename_module'
+    migration_template 'rename_module.rb', File.join(db_migrate_path, "#{file_name}.rb")
   end
 
   def check_model_exist?
