1731aa31ed942471701ca29c194a419bf80501a4
qinmingyuan <mingyuan0715@foxmail.com>
2021-05-27 20:00:31 +0800

    just use references

--- lib/rails_com/action_text/rich_text.rb
+++ lib/rails_com/action_text/rich_text.rb
@@ -4,8 +4,6 @@ module RailsCom::ActionText::RichText
   included do
     attribute :name, :string, null: false
     attribute :body, :text, size: :long
-    attribute :record_type, :string, null: false
-    attribute :record_id, :integer, null: false
     index [ :record_type, :record_id, :name ], name: 'index_action_text_rich_texts_uniqueness', unique: true
   end
 
