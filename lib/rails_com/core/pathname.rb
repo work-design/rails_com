27bb1accb277e52c1cdd65053d891abe78f8f693
qinmingyuan <mingyuan0715@foxmail.com>
2021-06-09 11:52:06 +0800

    add without extname

--- lib/rails_com/core/pathname.rb
+++ lib/rails_com/core/pathname.rb
@@ -1,9 +1,15 @@
 class Pathname
-  
+
   # like present? and presence
   # if exist? return self
   def existence
     self if exist?
   end
-  
+
+  # xx.html.erb => xx
+  def without_extname
+    suffix = basename.to_s.split('.')[0]
+    dirname.join(suffix)
+  end
+
 end
