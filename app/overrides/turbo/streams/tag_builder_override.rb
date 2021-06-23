c28613d1f5dcbd1647d30ed379445a96d76dbf83
qinmingyuan <mingyuan0715@foxmail.com>
2021-06-23 13:00:52 +0800

    moved override

--- app/overrides/turbo/streams/tag_builder_override.rb
+++
@@ -1,7 +0,0 @@
-Turbo::Streams::TagBuilder.class_eval do
-
-  def after(target, content = nil, **rendering, &block)
-    action :after, target, content, **rendering, &block
-  end
-
-end if defined? Turbo::Streams::TagBuilder
