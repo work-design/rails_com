4985c5d7a7ccd10903d3a6beae0f3eb8c77b88fa
qinmingyuan <mingyuan0715@foxmail.com>
2021-07-24 20:40:31 +0800

    turbo stream

--- test/controllers/com/panel/acme_orders_controller_test.rb
+++ test/controllers/com/panel/acme_orders_controller_test.rb
@@ -3,52 +3,58 @@ class Com::Panel::AcmeOrdersControllerTest < ActionDispatch::IntegrationTest
 
   setup do
     begin
-      acme_account = Com::AcmeAccount.create email: 'test3@work.design'
+      @acme_account = Com::AcmeAccount.create email: 'test3@work.design'
+      @acme_order = Com::AcmeOrder.create com_acme_orders(:one).attributes.slice('id')
     rescue
-      acme_account.stub :account, 1 do
-
-      end
+      @acme_order = com_acme_orders(:one)
+      @acme_account = @acme_order.acme_account
     end
-
-    @acme_order = Com::AcmeOrder.create com_acme_orders(:one).attributes.slice('id')
   end
 
   test 'index ok' do
-    get url_for(controller: 'com/panel/acme_orders', acme_account_id: @acme_order.acme_account_id)
+    get url_for(controller: 'com/panel/acme_orders', acme_account_id: @acme_account.id)
     assert_response :success
   end
 
   test 'new ok' do
-    get url_for(controller: 'com/panel/acme_orders', action: 'new')
+    get url_for(controller: 'com/panel/acme_orders', action: 'new', acme_account_id: @acme_account.id)
     assert_response :success
   end
 
   test 'create ok' do
     assert_difference('Com::AcmeOrder.count') do
-      post panel_acme_orders_url, params: { }
+      post(
+        url_for(controller: 'com/panel/acme_orders', action: 'create', acme_account_id: @acme_account.id),
+        params: { },
+        as: :turbo_stream
+      )
     end
 
     assert_response :success
   end
 
   test 'show ok' do
-    get panel_acme_order_url(@acme_order)
+    get url_for(controller: 'com/panel/acme_orders', action: 'show', acme_account_id: @acme_account.id, id: @acme_order.id)
     assert_response :success
   end
 
   test 'edit ok' do
-    get edit_panel_acme_order_url(@acme_order)
+    get url_for(controller: 'com/panel/acme_orders', action: 'edit', acme_account_id: @acme_account.id, id: @acme_order.id)
     assert_response :success
   end
 
   test 'update ok' do
-    patch panel_acme_order_url(@acme_order), params: { }
+    patch(
+      url_for(controller: 'com/panel/acme_orders', action: 'update', acme_account_id: @acme_account.id, id: @acme_order.id),
+      params: { },
+      as: :turbo_stream
+    )
     assert_response :success
   end
 
   test 'destroy ok' do
     assert_difference('Com::AcmeOrder.count', -1) do
-      delete panel_acme_order_url(@acme_order)
+      delete url_for(controller: 'com/panel/acme_orders', action: 'destroy', acme_account_id: @acme_account.id, id: @acme_order.id), as: :turbo_stream
     end
 
     assert_response :success
