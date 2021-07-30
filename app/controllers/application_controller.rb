3ba74f38a17b286d7c133ee7800fbaa0a6f4b2a2
qinmingyuan <mingyuan0715@foxmail.com>
2021-07-30 13:25:19 +0800

    defulat intege auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include RailsCom::Application
+  include Auth::Controller::Application if defined? RailsAuth
 
   protect_from_forgery with: :exception, unless: -> { json_format? }
 
