189212bab062c646d3de61e8a137c83912b3833b
qinmingyuan <mingyuan0715@foxmail.com>
2021-08-26 17:00:51 +0800

    avoid a series bug

--- lib/rails_com/active_record.rb
+++ lib/rails_com/active_record.rb
@@ -3,9 +3,10 @@
 module RailsCom::ActiveRecord; end
 require 'rails_com/active_record/i18n'
 require 'rails_com/active_record/enum'
+require 'rails_com/active_record/taxon'
 require 'rails_com/active_record/translation'
 require 'rails_com/active_record/extend'
 require 'rails_com/active_record/include'
 
 # enum has an attribute define, which get default value form this
-ActiveRecord::Type.instance_variable_set :@default_value, ActiveModel::Type::String.new
+#ActiveRecord::Type.instance_variable_set :@default_value, ActiveModel::Type::String.new
