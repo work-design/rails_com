ddad594a0d80674d96522c29a7ab353bf75e551c
qinmingyuan <mingyuan0715@foxmail.com>
2021-09-06 20:58:04 +0800

    enum

--- lib/rails_com.rb
+++ lib/rails_com.rb
@@ -7,7 +7,6 @@ require 'rails_com/action_controller'
 require 'rails_com/action_view'
 require 'rails_com/action_text'
 require 'rails_com/action_mailbox'
-require 'rails_com/active_record'
 require 'rails_com/active_storage'
 require 'rails_com/quiet_logs'
 
