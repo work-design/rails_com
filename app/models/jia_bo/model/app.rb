2438d93f708d4b5ba9790b877de6ca075c76979f
qinmingyuan <mingyuan0715@foxmail.com>
2021-11-01 19:24:16 +0800

    add print api

--- app/models/jia_bo/model/app.rb
+++ app/models/jia_bo/model/app.rb
@@ -1,6 +1,5 @@
 module JiaBo
   module Model::App
-    BASE_URL = 'https://api.poscom.cn/apisc'
     extend ActiveSupport::Concern
 
     included do
@@ -8,6 +7,7 @@ module JiaBo
       attribute :api_key, :string
       attribute :devices_count, :integer, default: 0
       attribute :templates_count, :integer, default: 0
+      attribute :base_url, :string, default: 'https://api.poscom.cn/apisc'
 
       has_many :devices, dependent: :destroy_async
       has_many :templates, dependent: :destroy_async
@@ -18,18 +18,23 @@ module JiaBo
         reqTime: (Time.now.to_f * 1000).round.to_s,
         memberCode: member_code
       }
-      joined_str = [p[:memberCode], p[:reqTime], api_key].join
+      joined_str = yield p
+
       p.merge! securityCode: Digest::MD5.hexdigest(joined_str)
     end
 
     def list_templates
+      params = common_params do |p|
+        [p[:memberCode], p[:reqTime], api_key].join
+      end
+
       r = HTTPX.with(debug: STDERR, debug_level: 2).post(
-        BASE_URL + '/listTemplate',
+        base_url + '/listTemplate',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
           Accept: 'application/json'
         },
-        params: common_params
+        params: params
       )
 
       if r.status == 200
@@ -49,13 +54,17 @@ module JiaBo
     end
 
     def list_devices
+      params = common_params do |p|
+        [p[:memberCode], p[:reqTime], api_key].join
+      end
+
       r = HTTPX.with(debug: STDERR, debug_level: 2).post(
-        BASE_URL + '/listDevice',
+        base_url + '/listDevice',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
           Accept: 'application/json'
         },
-        params: common_params
+        params: params
       )
 
       if r.status == 200
--- app/models/jia_bo/model/device.rb
+++ app/models/jia_bo/model/device.rb
@@ -10,5 +10,33 @@ module JiaBo
       belongs_to :app, counter_cache: true
     end
 
+    def templet_print(msg_no:, template_id:, data: {})
+      params = app.common_params do |p|
+        [p[:memberCode], device_id, msg_no, p[:reqTime], app.api_key].join
+      end
+      params.merge!(
+        msgNo: msg_no,
+        deviceID: device_id,
+        charset: 4,
+        templetID: template_id,
+        tData: data
+      )
+
+      r = HTTPX.with(debug: STDERR, debug_level: 2).post(
+        app.base_url + '/templetPrint',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Accept: 'application/json'
+        },
+        params: params
+      )
+
+      if r.status == 200
+        JSON.parse(r.to_s)
+      else
+        r
+      end
+    end
+
   end
 end
