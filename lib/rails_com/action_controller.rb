7440ab53ff90c47403e8dc3c78ad6d6c0410f863
qinmingyuan <mingyuan0715@foxmail.com>
2021-11-03 14:11:19 +0800

    moved to rails extend

--- lib/rails_com/action_controller.rb
+++ lib/rails_com/action_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 module RailsCom::ActionController; end
-require 'rails_com/action_controller/extend'
-require 'rails_com/action_controller/include'
 require 'rails_com/action_controller/errors'
 require 'rails_com/action_controller/parameters'
 require 'rails_com/action_controller/public_exceptions'
