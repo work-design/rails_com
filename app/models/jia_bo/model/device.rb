e3f0f4331d42b48d291224c76169b838f265c488
qinmingyuan <mingyuan0715@foxmail.com>
2021-12-13 01:06:51 +0800

    device

--- app/models/jia_bo/model/device.rb
+++ app/models/jia_bo/model/device.rb
@@ -10,7 +10,7 @@ module JiaBo
       belongs_to :app, counter_cache: true
     end
 
-    def templet_print(msg_no:, template_id:, data: {}, reprint: 0, multi: 0)
+    def templet_print(template_id:, msg_no: nil, data: {}, reprint: 0, multi: 0)
       params = app.common_params do |p|
         [p[:memberCode], device_id, msg_no, p[:reqTime], app.api_key].join
       end
@@ -18,11 +18,11 @@ module JiaBo
         deviceID: device_id,
         templetID: template_id,
         charset: 1,
-        msgNo: msg_no,
         tData: data.to_json,
         reprint: reprint,
         multi: multi
       )
+      params.merge! msgNo: msg_no if msg_no.present?
 
       r = HTTPX.with(debug: STDERR, debug_level: 2).post(
         app.base_url + '/templetPrint',
