9124284146ee5dafac98585abb56e677b9f980eb
qinmingyuan <mingyuan0715@foxmail.com>
2022-01-14 15:21:14 +0800

    use capture not yield block

--- app/helpers/rails_com/frame_helper.rb
+++ app/helpers/rails_com/frame_helper.rb
@@ -3,7 +3,7 @@ module RailsCom
 
     def turbo_frame_tagged(id, src: nil, target: nil, **attributes, &block)
       if request.headers['Turbo-Frame'].blank? && block_given?
-        yield block
+        capture(&block)
       else
         turbo_frame_tag(id, src: src, target: target, **attributes, &block)
       end
