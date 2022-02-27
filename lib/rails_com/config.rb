7a7e2a79f93b13d894ca5cf424fc6f96c2236f4b
qinmingyuan <mingyuan0715@foxmail.com>
2022-02-27 09:39:22 +0800

    add rails role config

--- lib/rails_com/config.rb
+++ lib/rails_com/config.rb
@@ -20,3 +20,14 @@ module RailsCom #:nodoc:
   end
 
 end
+
+module RailsRole
+  include ActiveSupport::Configurable
+
+  configure do |config|
+    config.default_admin_accounts = []
+    config.debug = false
+    config.default_return_path = '/board'
+  end
+
+end
