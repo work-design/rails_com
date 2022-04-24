28baea42f20d92835e6a7b7f50b566bdc1cc411d
qinmingyuan <mingyuan0715@foxmail.com>
2022-04-24 23:24:51 +0800

    reorder

--- app/controllers/com/controller/admin.rb
+++ app/controllers/com/controller/admin.rb
@@ -61,6 +61,21 @@ module Com
       model.move_lower
     end
 
+    def reorder
+      model = model_object
+      sort_array = params[:sort_array].select { |i| i.integer? }
+
+      if params[:new_index] > params[:old_index]
+        prev_one = model.class.find(sort_array[params[:new_index].to_i - 1])
+        model.insert_at prev_one.position
+      else
+        next_ones = model.class.find(sort_array[(params[:new_index].to_i + 1)..params[:old_index].to_i])
+        next_ones.each do |next_one|
+          next_one.insert_at model.position
+        end
+      end
+    end
+
     def destroy
       model = model_object
       model.destroy
