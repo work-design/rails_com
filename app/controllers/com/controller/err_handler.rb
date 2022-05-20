9f0594ada3d0aa8c1fb762e43fb84444958b63d9
qinmingyuan <mingyuan0715@foxmail.com>
2022-05-20 18:00:49 +0800

    rescue with disposable auth fail

--- app/controllers/com/controller/err_handler.rb
+++ app/controllers/com/controller/err_handler.rb
@@ -5,6 +5,7 @@ module Com
     included do
       rescue_from ActiveRecord::RecordInvalid, with: :record_not_save
       rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+      rescue_from Com::DisposableTokenError, with: :disposable_auth_fail
     end
 
     def record_not_save(exception)
@@ -13,6 +14,10 @@ module Com
 
     def record_not_found(exception)
       render 'err', locals: { exception: exception, message: exception.message }, status: :not_found
+    end
+
+    def disposable_auth_fail(exception)
+      render 'disposable_auth_fail', layout: 'raw'
     end
 
   end
---
+++ app/errors/com/disposable_token_error.rb
@@ -0,0 +1,4 @@
+module Com
+  class DisposableTokenError < StandardError
+  end
+end
