71f46f102fe12f65c75435fac1810921783678c4
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-11 23:29:34 +0800

    should related to job

--- app/models/job/ext/jobbed.rb
+++ app/models/job/ext/jobbed.rb
@@ -5,7 +5,7 @@ module Job
     included do
       attribute :job_id, :string
 
-      belongs_to :job, class_name: 'GoodJob::Execution', foreign_key: :job_id, primary_key: :active_job_id, optional: true
+      belongs_to :job, class_name: 'GoodJob::ActiveJobJob', foreign_key: :job_id, primary_key: :active_job_id, optional: true
     end
 
   end
