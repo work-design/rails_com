eafd77d0727b1b3cbc4a1ade0e75adab84a32c83
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-27 09:27:48 +0800

    rename to good::job

--- app/models/job/ext/jobbed.rb
+++ app/models/job/ext/jobbed.rb
@@ -5,7 +5,7 @@ module Job
     included do
       attribute :job_id, :string
 
-      has_one :job, class_name: 'GoodJob::ActiveJobJob', primary_key: :job_id, foreign_key: :active_job_id
+      has_one :job, class_name: 'GoodJob::Job', primary_key: :job_id, foreign_key: :active_job_id
       has_one :execution, ->{ order(created_at: :asc) }, class_name: 'GoodJob::Execution', primary_key: :job_id, foreign_key: :active_job_id
       has_many :executions, class_name: 'GoodJob::Execution', primary_key: :job_id, foreign_key: :active_job_id
     end
