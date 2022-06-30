aee5f0534d0e06265e15640076605041a45ebda3
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-30 22:17:14 +0800

    is it china mobile

--- lib/rails_com/utils/regexp_util.rb
+++ lib/rails_com/utils/regexp_util.rb
@@ -9,4 +9,8 @@ module RegexpUtil
     /(?<=#{prefix}).*(?=#{suffix})/
   end
 
+  def china_mobile?(tel)
+    tel.to_s.match? /^1(3[0-9]|4[01456879]|5[0-35-9]|6[2567]|7[0-8]|8[0-9]|9[0-35-9])\d{8}$/
+  end
+
 end
