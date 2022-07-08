0f4b3cc502901d441486afe84e375db6764c1fe8
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-08 17:46:55 +0800

    fix test

--- test/controllers/com/panel/acme_identifiers_controller_test.rb
+++ test/controllers/com/panel/acme_identifiers_controller_test.rb
@@ -7,23 +7,23 @@ module Com
     end
 
     test 'index ok' do
-      get url_for(controller: 'com/panel/acme_identifiers', acme_order_id: @acme_identifier.acme_order_id)
+      get url_for(controller: 'com/panel/acme_identifiers', acme_account_id: @acme_identifier.acme_order.acme_account_id, acme_order_id: @acme_identifier.acme_order_id)
       assert_response :success
     end
 
     test 'show ok' do
-      get url_for(controller: 'com/panel/acme_identifiers', action: 'show', acme_order_id: @acme_identifier.acme_order_id, id: @acme_identifier.id)
+      get url_for(controller: 'com/panel/acme_identifiers', action: 'show', acme_account_id: @acme_identifier.acme_order.acme_account_id, acme_order_id: @acme_identifier.acme_order_id, id: @acme_identifier.id)
       assert_response :success
     end
 
     test 'edit ok' do
-      get url_for(controller: 'com/panel/acme_identifiers', action: 'edit', acme_order_id: @acme_identifier.acme_order_id, id: @acme_identifier.id)
+      get url_for(controller: 'com/panel/acme_identifiers', action: 'edit', acme_account_id: @acme_identifier.acme_order.acme_account_id, acme_order_id: @acme_identifier.acme_order_id, id: @acme_identifier.id)
       assert_response :success
     end
 
     test 'update ok' do
       patch(
-        url_for(controller: 'com/panel/acme_identifiers', action: 'update', acme_order_id: @acme_identifier.acme_order_id, id: @acme_identifier.id),
+        url_for(controller: 'com/panel/acme_identifiers', action: 'update', acme_account_id: @acme_identifier.acme_order.acme_account_id, acme_order_id: @acme_identifier.acme_order_id, id: @acme_identifier.id),
         params: { },
         as: :turbo_stream
       )
@@ -33,7 +33,7 @@ module Com
     test 'destroy ok' do
       assert_difference('Com::AcmeIdentifier.count', -1) do
         delete(
-          url_for(controller: 'com/panel/acme_identifiers', action: 'destroy', acme_order_id: @acme_identifier.acme_order_id, id: @acme_identifier.id),
+          url_for(controller: 'com/panel/acme_identifiers', action: 'destroy', acme_account_id: @acme_identifier.acme_order.acme_account_id, acme_order_id: @acme_identifier.acme_order_id, id: @acme_identifier.id),
           as: :turbo_stream
         )
       end
