d075e4ac13ae7181974679e4c5ec090c8b1b565d
qinmingyuan <mingyuan0715@foxmail.com>
2022-12-07 17:11:01 +0800

    print with cpsl

--- app/models/jia_bo/model/device.rb
+++ app/models/jia_bo/model/device.rb
@@ -15,27 +15,25 @@ module JiaBo
       after_update :set_default, if: -> { default? && saved_change_to_default? }
     end
 
-    def templet_print(template_id:, msg_no: nil, data: {}, reprint: 0, multi: 0)
+    def print(msg_no: nil, data: nil, reprint: 0, multi: 0)
       params = app.common_params do |p|
         [p[:memberCode], device_id, msg_no, p[:reqTime], app.api_key].join
       end
       params.merge!(
         deviceID: device_id,
-        templetID: template_id,
+        mode: 2,
         charset: 1,
-        tData: data.to_json,
+        msgDetail: data,
         reprint: reprint,
         multi: multi
       )
       params.merge! msgNo: msg_no if msg_no.present?
 
-      r = HTTPX.with(debug: STDERR, debug_level: 2).post(
-        app.base_url + '/templetPrint',
-        form: params
-      )
+      r = HTTPX.with(origin: 'https://api.poscom.cn/apisc/', debug: STDERR, debug_level: 2).post('sendMsg', form: params)
 
       if r.status == 200
-        JSON.parse(r.to_s)
+        r
+        #JSON.parse(r.to_s)
       else
         r
       end
