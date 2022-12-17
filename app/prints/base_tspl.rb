2643d869294e293efd63452587ade7c418266501
qinmingyuan <mingyuan0715@foxmail.com>
2022-12-17 22:13:08 +0800

    linek height

--- app/prints/base_tspl.rb
+++ app/prints/base_tspl.rb
@@ -17,7 +17,9 @@ class BaseTspl
   end
 
   def render
-    (size + @qrcodes + @texts + footer).join("\n")
+    r = (size + @qrcodes + @texts + footer).join("\n")
+    puts r
+    r
   end
 
   def footer
@@ -38,7 +40,7 @@ class BaseTspl
   end
 
   # TSS16, 字体为：16x16
-  def text(data, font: 'TSS24.BF2', line_height: FONTS[font] * 1.5, scale: 1, x:, y: @lines * line_height, line_add: true)
+  def text(data, font: 'TSS24.BF2', line_height: FONTS[font] * 1.5, scale: 1, x:, y: @lines * line_height * scale, line_add: true)
     @texts << "TEXT #{x},#{y},\"#{font}\",0,#{scale},#{scale},\"#{data}\""
     @lines += 1 if line_add
   end
