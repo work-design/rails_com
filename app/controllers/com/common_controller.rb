6049b1d68e9259094a2cdac46ae7aae548457840
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-27 14:12:49 +0800

    use base url

--- app/controllers/com/common_controller.rb
+++ app/controllers/com/common_controller.rb
@@ -68,7 +68,7 @@ module Com
     end
 
     def cors_preflight_check
-      response.headers['Access-Control-Allow-Origin'] = request.host
+      response.headers['Access-Control-Allow-Origin'] = request.base_url
       response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
       response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-Csrf-Token, X-User-Token, X-User-Email'
       response.headers['Access-Control-Max-Age'] = '1728000'
