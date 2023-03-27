4ee54f22fd7a4faa5bd0a3a89ae0752443b1356d
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-27 12:46:00 +0800

    support csrf token

--- app/controllers/com/common_controller.rb
+++ app/controllers/com/common_controller.rb
@@ -70,7 +70,7 @@ module Com
     def cors_preflight_check
       response.headers['Access-Control-Allow-Origin'] = '*'
       response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-      response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+      response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-Csrf-Token, X-User-Token, X-User-Email'
       response.headers['Access-Control-Max-Age'] = '1728000'
 
       head :no_content
