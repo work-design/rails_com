65dec7b3bd8fc9b9b2ff7d3b787ddd3c11ca1c3f
qinmingyuan <mingyuan0715@foxmail.com>
2023-05-05 20:24:23 +0800

    add support cors

--- app/controllers/com/controller/application.rb
+++ app/controllers/com/controller/application.rb
@@ -142,6 +142,14 @@ module Com
       end
     end
 
+    def support_cors
+      response.headers['Access-Control-Allow-Origin'] = request.origin.presence || '*'
+      response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+      response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-Csrf-Token, X-User-Token, X-User-Email'
+      response.headers['Access-Control-Max-Age'] = '1728000'
+      response.headers['Access-Control-Allow-Credentials'] = true
+    end
+
     def default_params
       {}
     end
