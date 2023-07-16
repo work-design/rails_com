5f7dea38487c2c665b970c72dd056069afe13e13
qinmingyuan <mingyuan0715@foxmail.com>
2023-07-16 22:52:15 +0800

    the rails user logic

--- app/controllers/roled/controller/admin.rb
+++ app/controllers/roled/controller/admin.rb
@@ -2,8 +2,14 @@ module Roled
   module Controller::Admin
 
     def rails_role_user
-      logger.debug "\e[35m  Role User: Member  \e[0m"
-      defined?(current_member) && current_member
+      return @rails_role_user if defined? @rails_role_user
+      if defined?(current_member) && current_member
+        @rails_role_user = current_member
+      else
+        @rails_role_user = current_user
+      end
+      logger.debug "\e[35m  Role User: #{@rails_role_user.class_name}/#{@rails_role_user.id}  \e[0m"
+      @rails_role_user
     end
 
   end
