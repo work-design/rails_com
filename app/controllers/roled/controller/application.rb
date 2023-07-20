3edb50a97fbf1b84a739093d2e84cfbf8713a862
qinmingyuan <mingyuan0715@foxmail.com>
2023-07-20 22:36:08 +0800

    role user

--- app/controllers/roled/controller/application.rb
+++ app/controllers/roled/controller/application.rb
@@ -39,8 +39,10 @@ module Roled
     end
 
     def rails_role_user
-      logger.debug "\e[35m  Role User: User  \e[0m"
-      defined?(current_user) && current_user
+      return @rails_role_user if defined? @rails_role_user
+      @rails_role_user = defined?(current_user) && current_user
+      logger.debug "\e[35m  Role User: #{@rails_role_user.class_name}/#{@rails_role_user.id}  \e[0m"
+      @rails_role_user
     end
 
   end
