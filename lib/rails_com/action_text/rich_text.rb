f89ecdb2a4a05802ed2c18fe8dfbc13c8c8cd239
qinmingyuan <mingyuan0715@foxmail.com>
2023-10-24 15:11:41 +0800

    siz

--- lib/rails_com/action_text/rich_text.rb
+++ lib/rails_com/action_text/rich_text.rb
@@ -4,7 +4,7 @@ module RailsCom::ActionText
 
     included do
       attribute :name, :string, null: false
-      attribute :body, :text, size: :long
+      attribute :body, :text
       index [:record_type, :record_id, :name], name: 'index_action_text_rich_texts_uniqueness', unique: true
     end
 
