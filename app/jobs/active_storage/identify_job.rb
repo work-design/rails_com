d1258d302917e502064e997b99a849196900ca55
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-04 13:21:09 +0800

    moved files to extend

--- app/jobs/active_storage/identify_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class ActiveStorage::IdentifyJob < ActiveStorage::BaseJob
-
-  def perform(blob)
-    blob.identify
-  end
-
-end
--- lib/rails_com/active_storage.rb
+++ lib/rails_com/active_storage.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-require 'rails_com/active_storage/activestorage_attached'
 require 'rails_com/active_storage/attached_macros'
-require 'rails_com/active_storage/attachment_transfer'
-require 'rails_com/active_storage/attachment_prepend'
-require 'rails_com/active_storage/video_response'
 
 ActiveStorage.mattr_accessor :service_urls_expire_in, default: 5.minutes
