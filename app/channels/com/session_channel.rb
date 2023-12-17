60ae4a7da1dbfabb02cefb1fd3441bba43571763
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-17 14:28:34 +0800

    support online logic

--- app/channels/com/session_channel.rb
+++ app/channels/com/session_channel.rb
@@ -3,6 +3,7 @@ module Com
 
     def subscribed
       if defined?(Auth::AuthorizedToken) && verified_receiver.is_a?(Auth::AuthorizedToken)
+        verified_receiver.update online: true
         stream_from "com:session:#{verified_receiver.identity}"
       else
         stream_from "com:session:#{verified_receiver}"
@@ -10,6 +11,9 @@ module Com
     end
 
     def unsubscribed
+      if defined?(Auth::AuthorizedToken) && verified_receiver.is_a?(Auth::AuthorizedToken)
+        verified_receiver.update online: false
+      end
     end
 
   end
