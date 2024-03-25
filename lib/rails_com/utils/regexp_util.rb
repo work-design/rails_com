ce5a812fed9841b650923163ca6adb8ce5b4bd48
qinmingyuan <mingyuan0715@foxmail.com>
2024-03-25 15:06:23 +0800

    the regex logs for linkly

--- lib/rails_com/utils/regexp_util.rb
+++ lib/rails_com/utils/regexp_util.rb
@@ -5,20 +5,33 @@ module RegexpUtil
   # * zero or more times
   # greedy: 贪婪模式，尽可能多次的匹配
   # non greedy：非贪婪模式，尽可能少的
-  def between(prefix, suffix, quantifier: '*', greedy: true)
+  def between(prefix, suffix, quantifier: '*', greedy: false)
     /(?<=#{prefix}).#{quantifier}#{greedy ? '' : '?'}(?=#{suffix})/
   end
 
   def least_between(prefix, suffix)
-    between(prefix, suffix, quantifier: '+', greedy: true)
+    between(prefix, suffix, quantifier: '+', greedy: false)
   end
 
   def more_between(prefix, suffix)
-    between(prefix, suffix, quantifier: '*', greedy: false)
+    between(prefix, suffix, quantifier: '*', greedy: true)
+  end
+
+  def rails_log()
+    {
+      method: ['Started ', ' '],
+      path: [' "', '" ']
+    }
   end
 
   def xx(**options)
+    options.map do |key, value|
+      "#{value[0]}(?<#{key}>[^#{value[1]}]*)"
+    end
 
+
+
+    /^ #{} $/
   end
 
   def china_mobile?(tel)
