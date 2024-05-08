2ea6e9239939983132b5ddbee857f81abdc21e2f
qinmingyuan <mingyuan0715@foxmail.com>
2024-05-08 21:58:36 +0800

    require member or user

--- app/controllers/com/controller/panel.rb
+++ app/controllers/com/controller/panel.rb
@@ -3,6 +3,10 @@ module Com
     extend ActiveSupport::Concern
     include Controller::Curd
 
+    def require_member_or_user
+      require_user unless current_member
+    end
+
     def index
       instance_variable_set "@#{pluralize_model_name}", model_klass.order(id: :asc).page(params[:page])
     end
