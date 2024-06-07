162cabf190a2c944006e44baa8c64eeccbb56f19
qinmingyuan <mingyuan0715@foxmail.com>
2024-06-08 00:24:44 +0800

    shared method

--- app/apis/concerns/common_api.rb
+++ app/apis/concerns/common_api.rb
@@ -54,10 +54,10 @@ module CommonApi
 
   protected
   def with_access_token(params = {}, tries = 2)
-    app.refresh_access_token unless app.access_token_valid?
-    yield params.merge!(access_token: app.access_token)
+    @app.refresh_access_token unless @app.access_token_valid?
+    yield params.merge!(access_token: @app.access_token)
   rescue AccessTokenExpiredError
-    app.refresh_access_token
+    @app.refresh_access_token
     retry unless (tries -= 1).zero?
   end
 
