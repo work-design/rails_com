ce1f3f9abf72a3a1fb2fa0f073fa7dc553f05a60
qinmingyuan <mingyuan0715@foxmail.com>
2024-07-08 21:37:40 +0800

    d

--- app/apis/concerns/common_api.rb
+++ app/apis/concerns/common_api.rb
@@ -19,7 +19,7 @@ module CommonApi
 
   def get(path, params: {}, headers: {}, origin: nil, debug: nil)
     with_options = { origin: origin }
-    with_options.merge! debug: STDERR, debug_level: 2 if debug
+    with_options.merge! debug: STDERR, debug_level: 3 if debug
 
     with_access_token(params: params, headers: headers) do
       response = @client.with_headers(headers).with(with_options).get(path, params: params)
