c1060a31dedf22d85eb13bab93a7951edbf23640
qinmingyuan <mingyuan0715@foxmail.com>
2024-07-18 10:50:47 +0800

    support extname

--- lib/rails_com/utils/url_util.rb
+++ lib/rails_com/utils/url_util.rb
@@ -3,7 +3,8 @@ module UrlUtil
   extend self
 
   def file_from_url(url)
-    file = Tempfile.new
+    extname = ".#{File.basename(URI(url).path).split('.')[-1]}"
+    file = Tempfile.new ['', extname]
     file.binmode
 
     res = HTTPX.plugin(:follow_redirects).get(url)
