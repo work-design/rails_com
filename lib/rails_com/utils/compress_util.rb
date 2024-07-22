65334cc10eb7ca44eb2fbf9172598474d4f96e6a
qinmingyuan <mingyuan0715@foxmail.com>
2024-07-22 15:42:41 +0800

    compress util

--- lib/rails_com/utils/compress_util.rb
+++ lib/rails_com/utils/compress_util.rb
@@ -3,12 +3,14 @@
 module CompressUtil
   extend self
 
-  def involve(path)
+  def involve(path, copy: 'copy')
     variation = ActiveStorage::Variation.new(resize_to_limit: [1000, 1000], saver: { quality: 90 })
+    copy_dir = path.dirname.join(copy)
+    copy_dir.exist? || copy_dir.mkdir
     path.each_child do |entry|
       if entry.file?
         output = variation.send(:transformer).send(:process, entry, format: :jpg)
-        FileUtils.copy_file(output, path.dirname.join('copy' ,entry.basename))
+        FileUtils.copy_file(output, copy_dir(entry.basename))
       end
     end
   end
