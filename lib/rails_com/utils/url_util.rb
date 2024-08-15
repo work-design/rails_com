9c23ac831eee698475723fc9d8a10421043fdc26
qinmingyuan <mingyuan0715@foxmail.com>
2024-08-15 20:32:24 +0800

    wirhte in root

--- lib/rails_com/utils/url_util.rb
+++ lib/rails_com/utils/url_util.rb
@@ -3,17 +3,17 @@ module UrlUtil
   extend self
 
   def file_from_url(url)
-    file = Tempfile.new
-    file.binmode
+    file = Rails.root.join('tmp/files', SecureRandom.alphanumeric)
+    file.delete if file.file?
+    file.dirname.exist? || file.dirname.mkpath
 
     begin
       res = HTTPX.plugin(:follow_redirects).get(url)
       res.body.each do |fragment|
-        file.write fragment
+        file.binwrite fragment
       end if res.error.nil?
     rescue => e
     end
-    file.rewind
     file
   end
 end
