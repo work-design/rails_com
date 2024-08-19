476c8be1c86090f049c95d003e606896624c3713
qinmingyuan <mingyuan0715@foxmail.com>
2024-08-19 11:54:36 +0800

    jobbed

--- app/models/job/ext/jobbed.rb
+++ app/models/job/ext/jobbed.rb
@@ -5,9 +5,8 @@ module Job
     included do
       attribute :job_id, :string
 
-      has_one :job, class_name: 'GoodJob::Job', primary_key: :job_id, foreign_key: :active_job_id
-      has_one :execution, ->{ order(created_at: :asc) }, class_name: 'GoodJob::Execution', primary_key: :job_id, foreign_key: :active_job_id
-      has_many :executions, class_name: 'GoodJob::Execution', primary_key: :job_id, foreign_key: :active_job_id
+      has_one :job, class_name: 'SolidQueue::Job', primary_key: :job_id, foreign_key: :active_job_id
+      has_many :jobs, class_name: 'SolidQueue::Job', primary_key: :job_id, foreign_key: :active_job_id
     end
 
   end
