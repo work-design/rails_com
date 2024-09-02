a4d0cb124ae6f84d86a68ff0a10514e414d94ffa
qinmingyuan <mingyuan0715@foxmail.com>
2024-09-02 16:23:34 +0800

    user

--- app/controllers/my/home_controller.rb
+++ app/controllers/my/home_controller.rb
@@ -1,5 +1,8 @@
 module My
   class HomeController < BaseController
+    before_action :set_card_templates, :set_wallet_template, only: [:index]
+    before_action :set_lawful_wallet, only: [:index]
+    before_action :set_cart, only: [:index]
 
     def index
     end
--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,5 +1,6 @@
 class MyController < ApplicationController
   include Com::Controller::Admin
+  include Trade::Controller::My
   before_action :require_user
 
 end
