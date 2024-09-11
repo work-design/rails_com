8e993f74f74283222c0e0dbeda278c698f079951
qinmingyuan <mingyuan0715@foxmail.com>
2024-09-11 16:11:30 +0800

    support solid job done

--- lib/rails_com/solid_queue/include.rb
+++ lib/rails_com/solid_queue/include.rb
@@ -5,11 +5,25 @@ module RailsCom::SolidQueue
     included do
       scope :todo, -> { default_where('scheduled_at-gte': Time.current) }
       scope :lost, -> { default_where('scheduled_at-lt': Time.current) }
+
+      has_many :jobbeds, class_name: 'Qingflow::Application', primary_key: :active_job_id, foreign_key: :job_id
+
+      after_save_commit :job_done, if: -> { saved_change_to_finished_at? }
+    end
+
+    def job_done
+      broadcast_action_to(
+        self,
+        action: :replace,
+        target: 'sync_all',
+        partial: 'job/job_done',
+        locals: { model: self }
+      )
     end
 
   end
 end
 
-ActiveSupport.on_load :solid_queue_record do
-  SolidQueue::Job.include RailsCom::SolidQueue::Include
+ActiveSupport.on_load :solid_queue_job do
+  include RailsCom::SolidQueue::Include
 end
