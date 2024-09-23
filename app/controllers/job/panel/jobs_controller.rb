c152f482025ce88863dd511fe38865fecb4e57b9
qinmingyuan <mingyuan0715@foxmail.com>
2024-09-23 18:03:54 +0800

    suppot desc

--- app/controllers/job/panel/jobs_controller.rb
+++ app/controllers/job/panel/jobs_controller.rb
@@ -102,7 +102,7 @@ module Job
 
     def q_params
       q = { queue_name: params[:queue_id] }
-      q.merge! params.permit(:class_name)
+      q.merge! params.permit(:class_name, 'finished_at-desc')
       q
     end
 
