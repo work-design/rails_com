faf0d6f30fb00d633463ebfbb1f17bdc7cbf01e6
qinmingyuan <mingyuan0715@foxmail.com>
2024-09-24 21:15:31 +0800

    at all

--- app/models/com/model/detector_bot/detector_feishu.rb
+++ app/models/com/model/detector_bot/detector_feishu.rb
@@ -2,6 +2,7 @@ module Com
   module Model::DetectorBot::DetectorFeishu
 
     def send_message(err)
+      add_at
       set_content(err)
       res = HTTPX.post(hook_url, json: body)
       res.json
@@ -9,11 +10,43 @@ module Com
 
     def body
       {
-        msg_type: 'text',
+        msg_type: 'post',
         content: {
-          text: content
+          post: {
+            zh_cn: {
+              title: '请求存在故障',
+              content: content
+            }
+          }
         }
       }
+    end
+
+    def init_ivar
+      @content = []
+    end
+
+    def add_at
+      @content << [
+        {
+          tag: 'at',
+          user_id: 'all',
+          user_name: '弟兄们'
+        }
+      ]
+    end
+
+    def add_column(title, content)
+      @content << [
+        {
+          tag: 'text',
+          text: title
+        },
+        {
+          tag: 'text',
+          text: content
+        }
+      ]
     end
 
   end
