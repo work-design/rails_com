6c61c31b71d0f657c801082fb86f72210eebdbf1
qinmingyuan <mingyuan0715@foxmail.com>
2024-09-24 17:52:56 +0800

    d

--- app/controllers/com/panel/detectors_controller.rb
+++ app/controllers/com/panel/detectors_controller.rb
@@ -1,5 +1,32 @@
 module Com
   class Panel::DetectorsController < Panel::BaseController
+    before_action :set_detector, only: [:show, :edit, :update, :destroy, :actions]
+    before_action :set_new_detector, only: [:new, :create]
+    
+    def new
+      @detector.detector_bots.build
+    end
+
+    def edit
+      @detector.detector_bots.build if @detector.detector_bots.blank?
+    end
+
+    private
+    def set_detector
+      @detector = Detector.find params[:id]
+    end
+
+    def set_new_detector
+      @detector = Detector.new(detector_params)
+    end
+
+    def detector_params
+      params.fetch(:detector, {}).permit(
+        :name,
+        :url,
+        detector_bots_attributes: {}
+      )
+    end
 
   end
 end
