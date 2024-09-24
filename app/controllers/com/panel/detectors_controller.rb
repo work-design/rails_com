b76a492c8906f2d9885b7779405f97ff305324ac
qinmingyuan <mingyuan0715@foxmail.com>
2024-09-24 20:45:32 +0800

    dj

--- app/controllers/com/panel/detectors_controller.rb
+++ app/controllers/com/panel/detectors_controller.rb
@@ -2,7 +2,7 @@ module Com
   class Panel::DetectorsController < Panel::BaseController
     before_action :set_detector, only: [:show, :edit, :update, :destroy, :actions]
     before_action :set_new_detector, only: [:new, :create]
-    
+
     def new
       @detector.detector_bots.build
     end
