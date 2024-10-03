e987037b83ef7f3962eb877df021760eff630882
qinmingyuan <mingyuan0715@foxmail.com>
2024-10-03 21:25:17 +0800

    share

--- lib/rails_com/utils/url_util.rb
+++ lib/rails_com/utils/url_util.rb
@@ -5,18 +5,7 @@ module UrlUtil
   def file_from_url(url, filename: SecureRandom.alphanumeric)
     file_path = Rails.root.join('tmp/files', filename)
     file_path.dirname.exist? || file_path.dirname.mkpath
-    file = File.new(file_path, 'w')
-    file.binmode
-
-    begin
-      res = HTTPX.plugin(:follow_redirects).get(url)
-      res.body.each do |fragment|
-        file.write fragment
-      end if res.error.nil?
-    rescue => e
-    end
-    file.rewind
-    file
+    xx(file_path, url)
   end
 
   def filepath_from_url(url, filename: SecureRandom.alphanumeric)
