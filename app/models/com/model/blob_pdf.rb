ce429d5105a0deef70090a3907913cf19d5cb6d6
qinmingyuan <mingyuan0715@foxmail.com>
2024-10-08 22:02:05 +0800

    d

--- app/models/com/model/blob_pdf.rb
+++ app/models/com/model/blob_pdf.rb
@@ -13,10 +13,10 @@ module Com
 
     def process_file
       pdf_path = UrlUtil.filepath_from_url url
-      page_count = CompressUtil.pdf_pages(pdf_path)
-      return if page_count.to_i > 10 || page_count == 0
-      file = CompressUtil.pdf_to_jpg(pdf_path)
-      self.file.attach(io: file, filename: pdf_path.basename)
+      if Marcel::MimeType.for(pdf_path) == 'application/pdf' && CompressUtil.pdf_pages(pdf_path).to_i < 10
+        file = CompressUtil.pdf_to_jpg(pdf_path)
+        self.file.attach(io: file, filename: pdf_path.basename)
+      end
     end
 
     def file_url
