fb7b915a1f66b76ef3d9af28b4862308643552df
qinmingyuan <mingyuan0715@foxmail.com>
2024-10-14 16:22:51 +0800

    subscritpions

---
+++ app/controllers/com/panel/pg_stat_subscriptions_controller.rb
@@ -0,0 +1,15 @@
+module Com
+  class Panel::PgStatSubscriptionsController < Panel::BaseController
+    before_action :set_pg_subscription
+
+    def index
+      @pg_stat_subscriptions = @pg_subscription.pg_stat_subscriptions
+    end
+
+    private
+    def set_pg_subscription
+      @pg_subscription = PgSubscripcation.find params[:pg_subscription_id]
+    end
+
+  end
+end
