6fef06102483191b8b5a1462be2507c7d3e915ca
qinmingyuan <mingyuan0715@foxmail.com>
2024-10-25 17:13:08 +0800

    zip

--- lib/rails_com/utils/zip_generator.rb
+++ lib/rails_com/utils/zip_generator.rb
@@ -14,43 +14,27 @@ require 'zip'
 #   zf.write()
 class ZipGenerator
   # Initialize with the directory to zip and the location of the output archive.
-  def initialize(input_dir, output_file)
+  def initialize(input_dir = Rails.root.join('tmp', 'zip'), out_name = 'xx.zip')
     @input_dir = input_dir
-    @output_file = output_file
-  end
+    @input_dir.exist? || @input_dir.mkdir
+    output_file = @input_dir.join(out_name)
 
-  # Zip the input directory.
-  def write
-    entries = Dir.entries(@input_dir) - %w[. ..]
-
-    ::Zip::File.open(@output_file, create: true) do |zipfile|
-      write_entries entries, '', zipfile
+    ::Zip::File.open(output_file, create: true) do |zipfile|
+      recursively_write(@input_dir, zipfile)
     end
   end
 
-  private
-
-  # A helper method to make the recursion work.
-  def write_entries(entries, path, zipfile)
-    entries.each do |e|
-      zipfile_path = path == '' ? e : File.join(path, e)
-      disk_file_path = File.join(@input_dir, zipfile_path)
-
-      if File.directory? disk_file_path
-        recursively_deflate_directory(disk_file_path, zipfile, zipfile_path)
-      else
-        put_into_archive(disk_file_path, zipfile, zipfile_path)
+  def recursively_write(disk_file_path, zipfile)
+    if File.directory? disk_file_path
+      disk_file_path.children.each do |file_path|
+        recursively_write(file_path, zipfile)
+      end
+    else
+      unless disk_file_path.basename.to_s == '.DS_Store'
+        zipfile.add(disk_file_path.relative_path_from(@input_dir).to_s, disk_file_path)
       end
     end
   end
 
-  def recursively_deflate_directory(disk_file_path, zipfile, zipfile_path)
-    zipfile.mkdir zipfile_path
-    subdir = Dir.entries(disk_file_path) - %w[. ..]
-    write_entries subdir, zipfile_path, zipfile
-  end
 
-  def put_into_archive(disk_file_path, zipfile, zipfile_path)
-    zipfile.add(zipfile_path, disk_file_path)
-  end
 end
