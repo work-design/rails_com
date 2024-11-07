e498bac07f032f1ccfbe946a6a2d2db9305775df
qinmingyuan <mingyuan0715@foxmail.com>
2024-11-07 10:33:54 +0800

    simple esc

--- app/prints/base_esc.rb
+++ app/prints/base_esc.rb
@@ -3,29 +3,24 @@ class BaseEsc
 
   def initialize
     # ensure only supported sequences are generated
-    @data = "".force_encoding("ASCII-8BIT")
-    @data << EscHelper.sequence(Escpos::HW_INIT)
+    @data = ''.force_encoding('ASCII-8BIT')
+    @data << sequence(Escpos::HW_INIT)
   end
 
   def write(data)
-    escpos_data = data.respond_to?(:to_escpos) ? data.to_escpos : data
-    @data << escpos_data.force_encoding("ASCII-8BIT")
+    @data << data.force_encoding('ASCII-8BIT')
   end
 
   def partial_cut!
-    @data << EscHelper.sequence(Escpos::PAPER_PARTIAL_CUT)
+    @data << sequence(Escpos::PAPER_PARTIAL_CUT)
   end
 
   def cut!
-    @data << EscHelper.sequence(Escpos::PAPER_FULL_CUT)
+    @data << sequence(Escpos::PAPER_FULL_CUT)
   end
 
   def render
     to_escpos.bytes.map {|i| i.to_s(16) }.join('')
-  end
-
-  def to_escpos
-    @data
   end
 
   # Encodes UTF-8 string to encoding acceptable for the printer
