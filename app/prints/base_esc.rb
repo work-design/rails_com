413ebc51226b9c25ea3828b12f9f3fc598daccaf
qinmingyuan <mingyuan0715@foxmail.com>
2024-11-07 17:18:33 +0800

    base esc

--- app/prints/base_esc.rb
+++ app/prints/base_esc.rb
@@ -67,41 +67,41 @@ class BaseEsc
   CP_CP874                     = [ 0x47 ]
 
   # Text formating
-  TXT_NORMAL                   = [ 0x1b, 0x21, 0x00 ]        # Normal text
-  TXT_2HEIGHT                  = [ 0x1b, 0x21, 0x10 ]        # Double height text
-  TXT_2WIDTH                   = [ 0x1b, 0x21, 0x20 ]        # Double width text
-  TXT_4SQUARE                  = [ 0x1b, 0x21, 0x30 ]        # Quad area text
-  TXT_UNDERL_OFF               = [ 0x1b, 0x2d, 0x00 ]        # Underline font OFF
-  TXT_UNDERL_ON                = [ 0x1b, 0x2d, 0x01 ]        # Underline font 1
-  TXT_UNDERL2_ON               = [ 0x1b, 0x2d, 0x02 ]        # Underline font 2
-  TXT_BOLD_OFF                 = [ 0x1b, 0x45, 0x00 ]        # Bold font OFF
-  TXT_BOLD_ON                  = [ 0x1b, 0x45, 0x01 ]        # Bold font ON
-  TXT_FONT_A                   = [ 0x1b, 0x4d, 0x00 ]        # Font type A
-  TXT_FONT_B                   = [ 0x1b, 0x4d, 0x01 ]        # Font type B
-  TXT_ALIGN_LT                 = [ 0x1b, 0x61, 0x00 ]        # Left justification
-  TXT_ALIGN_CT                 = [ 0x1b, 0x61, 0x01 ]        # Centering
-  TXT_ALIGN_RT                 = [ 0x1b, 0x61, 0x02 ]        # Right justification
-  TXT_INVERT_ON                = [ 0x1d, 0x42, 0x01 ]        # Inverted color text
-  TXT_INVERT_OFF               = [ 0x1d, 0x42, 0x00 ]        # Inverted color text
-  TXT_COLOR_BLACK              = [ 0x1b, 0x72, 0x00 ]        # Default Color
-  TXT_COLOR_RED                = [ 0x1b, 0x72, 0x01 ]        # Alternative Color (Usually Red)
+  TXT_NORMAL = [ 0x1b, 0x21, 0x00 ]        # Normal text
+  TXT_2HEIGHT = [ 0x1b, 0x21, 0x10 ]        # Double height text
+  TXT_2WIDTH = [ 0x1b, 0x21, 0x20 ]        # Double width text
+  TXT_4SQUARE = [ 0x1b, 0x21, 0x30 ]        # Quad area text
+  TXT_UNDERL_OFF = [ 0x1b, 0x2d, 0x00 ]        # Underline font OFF
+  TXT_UNDERL_ON = [ 0x1b, 0x2d, 0x01 ]        # Underline font 1
+  TXT_UNDERL2_ON = [ 0x1b, 0x2d, 0x02 ]        # Underline font 2
+  TXT_BOLD_OFF = [ 0x1b, 0x45, 0x00 ]        # Bold font OFF
+  TXT_BOLD_ON = [ 0x1b, 0x45, 0x01 ]        # Bold font ON
+  TXT_FONT_A = [ 0x1b, 0x4d, 0x00 ]        # Font type A
+  TXT_FONT_B = [ 0x1b, 0x4d, 0x01 ]        # Font type B
+  TXT_ALIGN_LT = [ 0x1b, 0x61, 0x00 ]        # Left justification
+  TXT_ALIGN_CT = [ 0x1b, 0x61, 0x01 ]        # Centering
+  TXT_ALIGN_RT = [ 0x1b, 0x61, 0x02 ]        # Right justification
+  TXT_INVERT_ON = [ 0x1d, 0x42, 0x01 ]        # Inverted color text
+  TXT_INVERT_OFF = [ 0x1d, 0x42, 0x00 ]        # Inverted color text
+  TXT_COLOR_BLACK = [ 0x1b, 0x72, 0x00 ]        # Default Color
+  TXT_COLOR_RED = [ 0x1b, 0x72, 0x01 ]        # Alternative Color (Usually Red)
 
   # Barcodes
-  BARCODE_TXT_OFF             = [ 0x1d, 0x48, 0x00 ]         # HRI barcode chars OFF
-  BARCODE_TXT_ABV             = [ 0x1d, 0x48, 0x01 ]         # HRI barcode chars above
-  BARCODE_TXT_BLW             = [ 0x1d, 0x48, 0x02 ]         # HRI barcode chars below
-  BARCODE_TXT_BTH             = [ 0x1d, 0x48, 0x03 ]         # HRI barcode chars both above and below
-  BARCODE_FONT_A              = [ 0x1d, 0x66, 0x00 ]         # Font type A for HRI barcode chars
-  BARCODE_FONT_B              = [ 0x1d, 0x66, 0x01 ]         # Font type B for HRI barcode chars
-  BARCODE_HEIGHT              = [ 0x1d, 0x68 ]               # Barcode Height (1 - 255)
-  BARCODE_WIDTH               = [ 0x1d, 0x77 ]               # Barcode Width (2 - 6)
-  BARCODE_UPC_A               = [ 0x1d, 0x6b, 0x00 ]         # Barcode type UPC-A
-  BARCODE_UPC_E               = [ 0x1d, 0x6b, 0x01 ]         # Barcode type UPC-E
-  BARCODE_EAN13               = [ 0x1d, 0x6b, 0x02 ]         # Barcode type EAN13
-  BARCODE_EAN8                = [ 0x1d, 0x6b, 0x03 ]         # Barcode type EAN8
-  BARCODE_CODE39              = [ 0x1d, 0x6b, 0x04 ]         # Barcode type CODE39
-  BARCODE_ITF                 = [ 0x1d, 0x6b, 0x05 ]         # Barcode type ITF
-  BARCODE_NW7                 = [ 0x1d, 0x6b, 0x06 ]         # Barcode type NW7
+  BARCODE_TXT_OFF = [ 0x1d, 0x48, 0x00 ]         # HRI barcode chars OFF
+  BARCODE_TXT_ABV = [ 0x1d, 0x48, 0x01 ]         # HRI barcode chars above
+  BARCODE_TXT_BLW = [ 0x1d, 0x48, 0x02 ]         # HRI barcode chars below
+  BARCODE_TXT_BTH = [ 0x1d, 0x48, 0x03 ]         # HRI barcode chars both above and below
+  BARCODE_FONT_A = [ 0x1d, 0x66, 0x00 ]         # Font type A for HRI barcode chars
+  BARCODE_FONT_B = [ 0x1d, 0x66, 0x01 ]         # Font type B for HRI barcode chars
+  BARCODE_HEIGHT = [ 0x1d, 0x68 ]               # Barcode Height (1 - 255)
+  BARCODE_WIDTH = [ 0x1d, 0x77 ]               # Barcode Width (2 - 6)
+  BARCODE_UPC_A = [ 0x1d, 0x6b, 0x00 ]         # Barcode type UPC-A
+  BARCODE_UPC_E = [ 0x1d, 0x6b, 0x01 ]         # Barcode type UPC-E
+  BARCODE_EAN13 = [ 0x1d, 0x6b, 0x02 ]         # Barcode type EAN13
+  BARCODE_EAN8 = [ 0x1d, 0x6b, 0x03 ]         # Barcode type EAN8
+  BARCODE_CODE39 = [ 0x1d, 0x6b, 0x04 ]         # Barcode type CODE39
+  BARCODE_ITF = [ 0x1d, 0x6b, 0x05 ]         # Barcode type ITF
+  BARCODE_NW7 = [ 0x1d, 0x6b, 0x06 ]         # Barcode type NW7
 
   attr_reader :data
   def initialize
