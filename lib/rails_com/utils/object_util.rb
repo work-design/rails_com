9a8bf0fc39e08828c46338a212c2c256680008d8
qinmingyuan <mingyuan0715@foxmail.com>
2024-11-12 13:27:23 +0800

    membory total

--- lib/rails_com/utils/object_util.rb
+++ lib/rails_com/utils/object_util.rb
@@ -1,10 +1,14 @@
 module ObjectUtil
   extend self
 
-  def memory_usage(klass)
+  def memory_usage(klass = Object)
     Rails.logger.debug "\e[35m  #{klass.name} Memory Usage: #{ObjectSpace.memsize_of_all(klass).to_fs(:human_size)}  \e[0m"
     present_ids = ObjectSpace.each_object(klass).map { |i| i.__id__ }
     Rails.logger.debug "\e[35m  Present #{klass.name} Object: #{present_ids}, Count: #{present_ids.size}  \e[0m"
+  end
+
+  def memory_total
+    Rails.logger.debug "\e[35m  Total Memory Usage: #{ObjectSpace.memsize_of_all.to_fs(:human_size)}  \e[0m"
   end
 
   def present_objects(klass, ids)
