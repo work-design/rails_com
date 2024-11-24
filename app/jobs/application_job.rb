c08d463cca4a5ff9b87cc67f1961b2215540a88c
qinmingyuan <mingyuan0715@foxmail.com>
2024-11-24 23:52:00 +0800

    show backtrace

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -8,6 +8,7 @@ class ApplicationJob < ActiveJob::Base
           job_id: job.job_id,
           queue_name: job.queue_name,
           error: error.message,
+          backtrace: error.backtrace.join("\n"),
           arg: job.arguments.to_s
         }
       )
