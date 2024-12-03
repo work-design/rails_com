360e7c8580591e44ad0628539ac76aa12a02a0dd
qinmingyuan <mingyuan0715@foxmail.com>
2024-12-03 15:17:51 +0800

    qignflow

--- lib/rails_com/action_controller/prepend.rb
+++ lib/rails_com/action_controller/prepend.rb
@@ -6,21 +6,24 @@ module RailsCom::ActionController
       # 支持在 views/:controller 目录下以 _:action 开头的子目录进一步分组，会优先查找该目录下文件
       # 支持在 views/:controller 目录下以 _base 开头的通用分组子目录
       pres = ["#{controller_path}/_#{params[:action]}"]
-      names = ["#{controller_path}"]
+      bases = ["#{controller_path}/_base"]
+      names = [controller_path]
       namespaces = [params[:namespace]]
 
       super_class = self.class.superclass
       super_actions = RailsCom::Routes.controllers[super_class.controller_path]
       # 同名 controller, 向上级追溯
-      while super_actions&.key?(params[:action])
-        pres.append "#{super_class.controller_path}/_#{params[:action]}"
-        names.append "#{super_class.controller_path}"
+      while super_actions.present?
+        pres.append "#{super_class.controller_path}/_#{params[:action]}" if super_actions.key?(params[:action])
+        bases.append "#{super_class.controller_path}/_base"
+        names.append super_class.controller_path
         namespaces.append super_actions.dig(params[:action], :namespace)
+
         super_class = super_class.superclass
         super_actions = RailsCom::Routes.controllers[super_class.controller_path]
       end
 
-      pres.concat names.map(&->(i){ "#{i}/_base" })
+      pres.concat bases
       # 可以在 controller 中定义 _prefixes 方法
       # super do |pres|
       #   pres + ['xx']
