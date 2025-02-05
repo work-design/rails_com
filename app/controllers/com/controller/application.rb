7c27bcb63ff3238ff1cceca2eb8336449bd84134
qinmingyuan <mingyuan0715@foxmail.com>
2025-02-05 16:21:50 +0800

    clean code

--- app/controllers/com/controller/application.rb
+++ app/controllers/com/controller/application.rb
@@ -13,7 +13,7 @@ module Com
     included do
       layout -> { "frame/#{proper_layout}" if turbo_frame_body? }
       before_action :set_locale, :set_timezone, :set_variant
-      before_action :set_roled_tabs, if: -> { logger.debug "--------------------#{request.variant}"; request.variant.any?(:phone) }
+      before_action :set_roled_tabs, if: -> { request.variant.any?(:phone) }
       helper_method :current_title, :current_organ_name, :current_state, :current_filters, :default_params, :turbo_frame_request_id, :tab_item_items
       after_action :set_state, if: -> { request.variant.any? :phone }
     end
