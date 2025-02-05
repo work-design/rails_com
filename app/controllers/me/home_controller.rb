fa30ac9365422b365eef7688b900fb55100bb509
qinmingyuan <mingyuan0715@foxmail.com>
2025-02-05 14:09:55 +0800

    mehome

--- app/controllers/me/home_controller.rb
+++ app/controllers/me/home_controller.rb
@@ -1,6 +1,5 @@
 module Me
   class HomeController < BaseController
-    before_action :set_roled_tabs
     skip_before_action :require_role, only: [:index] if whether_filter :require_role
 
     def index
