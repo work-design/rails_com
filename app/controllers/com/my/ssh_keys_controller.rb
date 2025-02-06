b932b1634c37b207a22e418e26b5f982e212f91b
qinmingyuan <mingyuan0715@foxmail.com>
2025-02-07 01:21:44 +0800

    form

--- app/controllers/com/my/ssh_keys_controller.rb
+++ app/controllers/com/my/ssh_keys_controller.rb
@@ -3,15 +3,20 @@ module Com
     before_action :set_new_ssh_key, only: [:new, :create]
 
     def index
-      @ssh_keys = SshKey.where(user_id: current_user.id)
+      @ssh_keys = SshKey.where(user_id: current_user.id).page(params[:page])
     end
 
     private
     def set_new_ssh_key
-      params.fetch(:ssh_key, {}).permit(
+      @ssh_key = SshKey.new(ssh_key_params)
+    end
+
+    def ssh_key_params
+      p = params.fetch(:ssh_key, {}).permit(
         :host,
         :domain
       )
+      p.merge! user_id: current_user.id
     end
 
   end
