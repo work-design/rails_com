167884e83f2fedf819bacbf6d03a3d40d5f17eb1
qinmingyuan <mingyuan0715@foxmail.com>
2025-02-07 01:15:05 +0800

    d

--- app/controllers/com/my/ssh_keys_controller.rb
+++ app/controllers/com/my/ssh_keys_controller.rb
@@ -1,5 +1,10 @@
 module Com
   class My::SshKeysController < My::BaseController
+    before_action :set_new_ssh_key, only: [:new, :create]
+
+    def index
+      @ssh_keys = SshKey.where(user_id: current_user.id)
+    end
 
     private
     def set_new_ssh_key
