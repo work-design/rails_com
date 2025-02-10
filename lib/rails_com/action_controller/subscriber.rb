9bf140bb6b117c6985a46144f782f1343580b5b5
qinmingyuan <mingyuan0715@foxmail.com>
2025-02-10 16:15:34 +0800

    simple codes

--- lib/rails_com/action_controller/subscriber.rb
+++ lib/rails_com/action_controller/subscriber.rb
@@ -6,14 +6,15 @@ module RailsCom::ActionController
       payload = event.payload
       raw_headers = payload.fetch(:headers, {})
       real_headers = Com::Err.request_headers(raw_headers)
+      sessions = raw_headers['rack.session'].to_h
       cookies = payload[:request].cookies.except(Rails.configuration.session_options[:key])
       ancestors = payload[:request].controller_class.ancestors.yield_self(&->(i){ i.slice(0...(i.index(ActionController::Base) || i.index(ActionController::API))) })
 
-      debug "\e[33m  Headers:\e[0m { #{real_headers.map(&->(k,v){ "\e[37m#{k}:\e[0m \e[90m#{v}\e[0m" }).join(', ')} }"
-      debug "\e[33m  Sessions:\e[0m { #{raw_headers['rack.session'].to_h.map(&->(k,v){ "\e[37m#{k}:\e[0m \e[90m#{v}\e[0m" }).join(', ')} }" unless raw_headers['rack.session'].blank?
-      debug "\e[33m  Cookies:\e[0m { #{cookies.map(&->(k, v){ "\e[37m#{k}:\e[0m \e[90m#{v}\e[0m" }).join(', ')} }" unless cookies.blank?
-      debug "\e[33m  Ancestors:\e[0m [ #{ancestors.map(&->(i){ i.is_a?(Class) ? "\e[37m#{i}\e[0m" : "\e[90m#{i}\e[0m" }).join(', ')} ]"
-      debug "\e[33m  Prefixes:\e[0m #{event.payload[:request].controller_instance.send(:_prefixes)}"
+      debug "\e[33m  Headers:\e[0m {#{real_headers.map(&->(k,v){ "\e[37m#{k}:\e[0m \e[90m#{v}\e[0m" }).join(', ')}}"
+      debug "\e[33m  Sessions:\e[0m {#{sessions.map(&->(k,v){ "\e[37m#{k}:\e[0m \e[90m#{v}\e[0m" }).join(', ')}}" unless sessions.blank?
+      debug "\e[33m  Cookies:\e[0m {#{cookies.map(&->(k, v){ "\e[37m#{k}:\e[0m \e[90m#{v}\e[0m" }).join(', ')}}" unless cookies.blank?
+      debug "\e[33m  Ancestors:\e[0m [#{ancestors.map(&->(i){ i.is_a?(Class) ? "\e[37m#{i}\e[0m" : "\e[90m#{i}\e[0m" }).join(', ')}]"
+      debug "\e[33m  Prefixes:\e[0m #{payload[:request].controller_instance.send(:_prefixes)}"
     end
 
     def process_action(event)
