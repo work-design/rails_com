b1a3d59b9151e7e5cd6f316860ce74960e8f2385
qinmingyuan <mingyuan0715@foxmail.com>
2025-02-28 16:19:49 +0800

    add more columns

--- app/models/com/model/statistic_day.rb
+++ app/models/com/model/statistic_day.rb
@@ -6,9 +6,17 @@ module Com
       attribute :year, :string
       attribute :month, :string
       attribute :day, :string
+      attribute :year_month, :string, index: true
+      attribute :date, :date
       attribute :value, :decimal
 
       belongs_to :statistic
+
+      before_validation :init_year_month, if: -> { (changes.keys & ['year', 'month']).present? }
+    end
+
+    def init_year_month
+      self.year_month = "#{year}-#{month.to_s.rjust(2, '0')}"
     end
 
   end
--- app/models/com/model/statistic_month.rb
+++ app/models/com/model/statistic_month.rb
@@ -5,13 +5,16 @@ module Com
     included do
       attribute :year, :string
       attribute :month, :string
+      attribute :year_month, :string, index: true
       attribute :value, :decimal
 
       belongs_to :statistic
+
+      before_validation :init_year_month, if: -> { (changes.keys & ['year', 'month']).present? }
     end
 
-    def year_month
-      "#{year}-#{month.to_s.rjust(2, '0')}"
+    def init_year_month
+      self.year_month = "#{year}-#{month.to_s.rjust(2, '0')}"
     end
 
   end
