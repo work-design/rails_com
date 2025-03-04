ebb11522b7980cb84e2393e3dec783d1eb999e8b
qinmingyuan <mingyuan0715@foxmail.com>
2025-03-04 11:26:25 +0800

    jobs

--- app/jobs/com/statistical_month_job.rb
+++ app/jobs/com/statistical_month_job.rb
@@ -1,5 +1,6 @@
 module Com
   class StatisticalMonthJob < ApplicationJob
+    queue_as :statistic
 
     def perform(statistical, year_month)
       month = Date.new(*year_month.split('-').map(&:to_i))
