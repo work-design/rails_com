afd0b6247ff58595f255b287c934e9dcd7f72be3
qinmingyuan <mingyuan0715@foxmail.com>
2025-03-07 11:22:53 +0800

    statis

--- app/controllers/com/panel/statistic_days_controller.rb
+++ app/controllers/com/panel/statistic_days_controller.rb
@@ -1,10 +1,12 @@
 module Com
-  class Panel::StatisticMonthsController < Panel::BaseController
+  class Panel::StatisticDaysController < Panel::BaseController
     before_action :set_statistic
 
     def index
+      today = Date.today
       @statistic_days = @statistic.statistic_days.where(date: Date.today.beginning_of_month..).order(date: :desc)
-      @statistic_months = @statistic.statistic_months.order(year_month: :desc).page(params[:page]).per(params[:per])
+      @statistic_months = @statistic.statistic_months.where(year: today.year, month: 1..today.month - 1).order(year_month: :desc)
+      @statistic_years = @statistic.statistic_years.order(year: :desc)
     end
 
     private
