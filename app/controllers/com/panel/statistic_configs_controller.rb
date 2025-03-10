5e5dd895e38fefbf8faba7361224e7e486328279
qinmingyuan <mingyuan0715@foxmail.com>
2025-03-10 20:03:36 +0800

    index

--- app/controllers/com/panel/statistic_configs_controller.rb
+++ app/controllers/com/panel/statistic_configs_controller.rb
@@ -1,5 +1,16 @@
 module Com
   class Panel::StatisticConfigsController < Panel::BaseController
 
+    private
+    def statistic_config_params
+      params.fetch(:statistic_config, {}).permit(
+        :statistical_type,
+        :statistical_id,
+        :note,
+        :begin_on,
+        :end_on,
+        keys: []
+      )
+    end
   end
 end
