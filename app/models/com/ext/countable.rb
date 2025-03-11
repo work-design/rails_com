5e3e3c6da5b6a19ee382206644e4524c8f013050
qinmingyuan <mingyuan0715@foxmail.com>
2025-03-11 11:56:51 +0800

    use inject

--- app/models/com/ext/countable.rb
+++ app/models/com/ext/countable.rb
@@ -12,15 +12,14 @@ module Com
       extras = extra.product_with_key
       all_counter = counters.where(extra: extras)
 
-      CounterMonth.where(counter_id: all_counter.pluck(:id)).group_by(&:counter).each_with_object(0) do |(counter, counter_months), total|
+      CounterMonth.where(counter_id: all_counter.pluck(:id)).group_by(&:counter).inject(0) do |total, (counter, counter_months)|
         counter_months.each do |counter_month|
           total = total + counter_month.count
         end
 
         # 加上今天的实时数据
         today_value = today_count(counter)
-        total = total + today_value
-        total
+        total + today_value
       end
     end
 
