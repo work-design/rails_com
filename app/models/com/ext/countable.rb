55d502f1e21a7c1639b65677d5e050fc2c49a110
qinmingyuan <mingyuan0715@foxmail.com>
2025-03-11 13:14:03 +0800

    counter

--- app/models/com/ext/countable.rb
+++ app/models/com/ext/countable.rb
@@ -23,6 +23,21 @@ module Com
       end
     end
 
+    def read_counter_year(extra: {}, year: Date.today.year)
+      extras = extra.product_with_key
+      all_counter = counters.where(extra: extras)
+
+      CounterMonth.where(counter_id: all_counter.pluck(:id), year: year).group_by(&:counter).each_with_object({}) do |(counter, counter_months), count_h|
+        counter_months.each do |counter_month|
+          count_h[counter_month.year_month] = count_h[counter_month.year_month] + counter_month.count
+        end
+
+        # 加上今天的实时数据
+        today_value = today_count(counter)
+        count_h["#{Date.today.to_fs(:year_and_month)}"] = count_h["#{Date.today.to_fs(:year_and_month)}"] + today_value
+      end
+    end
+
     def generate_all_counters(extra_hash: {})
       extra_hash.product_with_key.each do |extra|
         counter = counters.find_or_initialize_by(extra: extra)
