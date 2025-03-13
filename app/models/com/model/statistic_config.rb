3067b6652bbb9f14599a3a1a8b52c70901b159c9
qinmingyuan <mingyuan0715@foxmail.com>
2025-03-13 21:14:19 +0800

    show id

--- app/models/com/model/statistic_config.rb
+++ app/models/com/model/statistic_config.rb
@@ -10,14 +10,14 @@ module Com
       attribute :note, :string
       attribute :keys, :json
       attribute :today, :date
-      attribute :today_begin, :big_integer
+      attribute :today_begin_id, :big_integer
 
       has_many :counters, primary_key: :statistical_type, foreign_key: :countable_type
     end
 
     def compute_today_begin!
       id = statistical_type.constatize.where(created_at: ...Date.today.beginning_of_day.to_fs(:human)).order(id: :desc).first.id
-      self.today_begin = id
+      self.today_begin_id = id
       self.today = Date.today
       self.save
     end
