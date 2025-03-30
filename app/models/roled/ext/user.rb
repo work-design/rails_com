a5e4c81f334ecc6acc1e4d3d6ff1459038978824
qinmingyuan <mingyuan0715@foxmail.com>
2025-03-30 22:37:33 +0800

    admin method

--- app/models/roled/ext/user.rb
+++ app/models/roled/ext/user.rb
@@ -11,14 +11,8 @@ module Roled
       Role.joins(:role_types).where(role_types: { who_type: 'Auth::User' }).visible
     end
 
-    def old_admin?
-      if respond_to?(:account_identities) && (RailsCom.config.default_admin_accounts & account_identities).length > 0
-        true
-      elsif respond_to?(:identity) && RailsCom.config.default_admin_accounts.include?(identity)
-        true
-      elsif method(:admin?).super_method
-        super
-      end
+    def admin?
+      id == 1 || super
     end
 
   end
