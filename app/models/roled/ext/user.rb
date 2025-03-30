67537889053acf547bd388df08198254efc237e3
qinmingyuan <mingyuan0715@foxmail.com>
2025-03-30 22:02:26 +0800

    fix error

--- app/models/roled/ext/user.rb
+++ app/models/roled/ext/user.rb
@@ -4,13 +4,14 @@ module Roled
 
     included do
       include Ext::Base
+      attribute :admin, :boolean, default: false
     end
 
     def visible_roles
       Role.joins(:role_types).where(role_types: { who_type: 'Auth::User' }).visible
     end
 
-    def admin?
+    def old_admin?
       if respond_to?(:account_identities) && (RailsCom.config.default_admin_accounts & account_identities).length > 0
         true
       elsif respond_to?(:identity) && RailsCom.config.default_admin_accounts.include?(identity)
