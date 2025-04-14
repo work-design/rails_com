e03e3aca1fde5bac476eaed80b78f1d8317e3c43
qinmingyuan <mingyuan0715@foxmail.com>
2025-04-14 18:26:10 +0800

    we

--- app/models/com/inner/work_wechat_bot.rb
+++ app/models/com/inner/work_wechat_bot.rb
@@ -2,11 +2,16 @@ module Com
   module Inner::WorkWechatBot
     extend ActiveSupport::Concern
 
-    def send_err_message(err_hash)
+    def send_err_message(title = '队列任务出现错误', err_hash)
       err_hash.each do |key, value|
         add_column key, value
       end
-      _body = body('队列任务出现错误', content)
+      _body = {
+        msgtype: 'markdown',
+        markdown: {
+          content: ["# #{title}", content].join("\n\n")
+        }
+      }
       res = HTTPX.post(hook_url, json: _body)
       res.json
     end
