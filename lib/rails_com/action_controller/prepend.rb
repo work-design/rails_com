3e12d6ec06115ae44f5655e34a437494dda5de89
qinmingyuan <mingyuan0715@foxmail.com>
2025-04-16 21:57:16 +0800

    d

--- lib/rails_com/action_controller/prepend.rb
+++ lib/rails_com/action_controller/prepend.rb
@@ -6,22 +6,30 @@ module RailsCom::ActionController
       # 支持在 views/:controller 目录下以 _:action 开头的子目录进一步分组，会优先查找该目录下文件
       # 支持在 views/:controller 目录下以 _base 开头的通用分组子目录
       pres = ["#{controller_path}/_#{params[:action]}"]
-      bases = ["#{controller_path}/_base"]
-      names = [controller_path]
+      _bases = ["#{controller_path}/_base"]
+      controller_paths = [controller_path]
       namespaces = [params[:namespace]]
 
       super_class = self.class.superclass
-      # 同名 controller, 向上级追溯
       while super_class.action_methods.include?(params[:action])
         pres.append "#{super_class.controller_path}/_#{params[:action]}"
-        bases.append "#{super_class.controller_path}/_base"
-        names.append super_class.controller_path
-        #namespaces.append super_actions.dig(params[:action], :namespace)
 
         super_class = super_class.superclass
       end
 
-      pres.concat bases
+      route_super_class = self.class.superclass
+      super_actions = RailsCom::Routes.controllers[route_super_class.controller_path]
+      # 同名 controller, 向上级追溯
+      while super_actions.is_a?(Hash) && super_actions.key?(params[:action])
+        _bases.append "#{route_super_class.controller_path}/_base"
+        controller_paths.append route_super_class.controller_path
+        namespaces.append super_actions.dig(params[:action], :namespace)
+
+        route_super_class = route_super_class.superclass
+        super_actions = RailsCom::Routes.controllers[route_super_class.controller_path]
+      end
+
+      pres.concat _bases
       # 可以在 controller 中定义 _prefixes 方法
       # super do |pres|
       #   pres + ['xx']
@@ -31,7 +39,7 @@ module RailsCom::ActionController
       end
       pres += super
 
-      r = names.map do |i|
+      r = controller_paths.map do |i|
         olds = i.split('/')
         olds[-1] = 'base'
         olds.join('/')
