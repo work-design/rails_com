4cec169683655c01180bc5d538c1a07ee6acbb5c
qinmingyuan <mingyuan0715@foxmail.com>
2025-04-26 23:03:09 +0800

    curd

--- lib/rails_com/action_dispatch/mapper.rb
+++ lib/rails_com/action_dispatch/mapper.rb
@@ -12,11 +12,6 @@ module RailsCom::ActionDispatch
           post :new
         end
       end
-      if parent_resource.actions.include?(:create)
-        collection do
-          post :create_refresh
-        end
-      end
       member do
         post :actions
         if parent_resource.actions.include?(:show) && parent_resource.actions.include?(:index)
