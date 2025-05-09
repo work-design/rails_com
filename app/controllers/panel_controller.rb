63a3e8641037a0960b708706e8aee745f6ab2065
qinmingyuan <mingyuan0715@foxmail.com>
2025-05-09 18:24:34 +0800

    org

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -1,6 +1,7 @@
 class PanelController < ApplicationController
   include Com::Controller::Panel
   include Roled::Controller::Panel
+  include Org::Controller::Panel if defined? RailsOrg
   before_action :require_member_or_user, :require_role
   after_action :set_flash, only: [:update, :create, :destroy]
 
